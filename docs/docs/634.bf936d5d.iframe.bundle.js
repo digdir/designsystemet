e4db9a071ab3a99d30803fbe90b74eb7cd150bc5
Øyvind Thune <oyvlei@gmail.com>
2022-12-07 12:07:57 +0100

    Changed back to yarn

---
+++ docs/docs/634.bf936d5d.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdigdir_storybook=self.webpackChunkdigdir_storybook||[]).push([[634],{"../node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("../node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
