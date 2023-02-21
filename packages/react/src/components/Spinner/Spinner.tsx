72e0b2b80ea2bf59a364a8ba38c145500322eaef
LAPTOP-S0QU96IH\david <david@framit.no>
2023-02-21 15:18:12 +0100

    PR-feedback

--- packages/react/src/components/Spinner/Spinner.tsx
+++ packages/react/src/components/Spinner/Spinner.tsx
@@ -13,30 +13,18 @@ type Size =
 
 type Variant = 'default' | 'interaction' | 'inverted';
 
-type SpinnerProps = {
-  screenReaderLabel: string;
-  size?: Size;
-  variant?: Variant;
+const sizeMap: Record<Size, number> = {
+  xSmall: 13,
+  small: 20,
+  medium: 27,
+  large: 40,
+  '1xLarge': 44,
+  '2xLarge': 56,
+  '3xLarge': 79,
 };
-export const Spinner = ({
-  screenReaderLabel,
-  size = 'medium',
-  variant = 'default',
-}: SpinnerProps): JSX.Element => {
-  const sizeMap: Record<Size, number> = {
-    xSmall: 13,
-    small: 20,
-    medium: 27,
-    large: 40,
-    '1xLarge': 44,
-    '2xLarge': 56,
-    '3xLarge': 79,
-  };
 
-  const variantMap: Record<
-    Variant,
-    { foreground: string; background: string }
-  > = {
+const variantMap: Record<Variant, { foreground: string; background: string }> =
+  {
     default: {
       foreground: classes.defaultForeground,
       background: classes.defaultBackground,
@@ -51,35 +39,45 @@ export const Spinner = ({
     },
   };
 
+export type SpinnerProps = {
+  title: string;
+  size?: Size;
+  /** Spinner appearance  */
+  variant?: Variant;
+} & React.ComponentPropsWithoutRef<'svg'>;
+
+/**  Spinner component used for indicating busy or indeterminate loading */
+export const Spinner = ({
+  title,
+  size = 'medium',
+  variant = 'default',
+  className = '',
+  ...rest
+}: SpinnerProps): JSX.Element => {
   return (
-    <div
-      data-testid='spinner-container'
-      aria-busy={true}
-      aria-live='polite'
+    <svg
+      className={`${classes.spinner} ${className}`}
+      style={{ width: sizeMap[size], height: sizeMap[size] }}
+      viewBox='0 0 50 50'
+      {...rest}
     >
-      <svg
-        className={classes.spinner}
-        style={{ width: sizeMap[size], height: sizeMap[size] }}
-        viewBox='0 0 50 50'
-      >
-        <title>{screenReaderLabel}</title>
-        <circle
-          className={variantMap[variant].background}
-          cx='25'
-          cy='25'
-          r='20'
-          fill='none'
-          strokeWidth='5'
-        ></circle>
-        <circle
-          className={`${classes.spinnerCircle} ${variantMap[variant].foreground}`}
-          cx='25'
-          cy='25'
-          r='20'
-          fill='none'
-          strokeWidth='5'
-        ></circle>
-      </svg>
-    </div>
+      <title>{title}</title>
+      <circle
+        className={variantMap[variant].background}
+        cx='25'
+        cy='25'
+        r='20'
+        fill='none'
+        strokeWidth='5'
+      ></circle>
+      <circle
+        className={`${classes.spinnerCircle} ${variantMap[variant].foreground}`}
+        cx='25'
+        cy='25'
+        r='20'
+        fill='none'
+        strokeWidth='5'
+      ></circle>
+    </svg>
   );
 };
