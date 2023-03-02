c48a688f0131c4c2f726977298ba4453041c824c
Michael Marszalek <mimarz@gmail.com>
2023-03-02 12:53:08 +0100

    🔧 Updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
   ],
 };
