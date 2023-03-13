d8c58938da78f639b7b9cfe3631c4a5d34ba9220
Michael Marszalek <mimarz@gmail.com>
2023-03-13 11:25:47 +0100

    🐛 Fixed IconSearch in mdx

--- docs-components/IconSearch/IconSearch.tsx
+++ docs-components/IconSearch/IconSearch.tsx
@@ -4,7 +4,7 @@ import { Search } from '@navikt/ds-icons';
 import { differenceInDays } from 'date-fns';
 import cn from 'classnames';
 
-import { Close } from '../../packages/icons/dist/cjs';
+import { Close } from '../../packages/icons';
 import meta from '../../packages/icons/meta.json';
 
 import classes from './IconSearch.module.css';
