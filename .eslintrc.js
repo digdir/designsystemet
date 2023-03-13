2d0930ba9d61ee4ca885fc06b13f1dae2c1ec4bc
Michael Marszalek <mimarz@gmail.com>
2023-03-13 10:41:19 +0100

    testing after moving iconsearch from js package

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
