deeb7405c70301d2de1f21f16a475e49b75f5b42
Øyvind Thune <oyvlei@gmail.com>
2023-03-16 09:42:14 +0100

    🗑 Removed unused imports

--- storefront/layouts/NavigationPageLayout/NavigationPageLayout.tsx
+++ storefront/layouts/NavigationPageLayout/NavigationPageLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Picture } from '@navikt/ds-icons';
 import { useRouter } from 'next/router';
