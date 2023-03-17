da40088230d04b8d497b6035c1d6906944136d7b
Michael Marszalek <mimarz@gmail.com>
2023-03-17 14:11:19 +0100

    ♻️ Replaced `HeadingSize`, `HeadingLevel`

--- packages/react/src/components/Typography/Heading/index.ts
+++ packages/react/src/components/Typography/Heading/index.ts
@@ -1,1 +1,1 @@
-export { default as Heading, HeadingSize, HeadingLevel } from './Heading';
+export { default as Heading } from './Heading';
--- packages/react/src/types/enums.ts
+++ packages/react/src/types/enums.ts
@@ -83,3 +83,20 @@ export enum RadioGroupVariant {
   Vertical = 'vertical',
   Horizontal = 'horizontal',
 }
+
+export enum HeadingSize {
+  XLarge = 'xlarge',
+  Large = 'large',
+  Medium = 'medium',
+  Small = 'small',
+  XSmall = 'xsmall',
+}
+
+export enum HeadingLevel {
+  H1 = 1,
+  H2 = 2,
+  H3 = 3,
+  H4 = 4,
+  H5 = 5,
+  H6 = 6,
+}
