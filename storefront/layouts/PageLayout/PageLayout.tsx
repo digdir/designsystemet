aa2af891165acc3a8513db8fa9f6614b2a0bf19b
Øyvind Thune <oyvlei@gmail.com>
2023-03-17 09:14:24 +0100

    ➕ Added grid to PageLayout

--- storefront/layouts/PageLayout/PageLayout.tsx
+++ storefront/layouts/PageLayout/PageLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 
+import { Container } from '../../components/Container/Container';
 import Header from '../../components/Header/Header';
 import { SidebarMenu } from '../../components/SidebarMenu/SidebarMenu';
 import {
@@ -30,34 +30,24 @@ const PageLayout = ({ content, menu, data }: PageLayoutProps) => {
   return (
     <div>
       <Header />
-      <main className={classes.page}>
-        <Container>
-          <Row>
-            <Col md={2}>
-              <SidebarMenu
-                title={capitalizeString(convertQueryToReadable(menu.title))}
-                menu={menu}
-                activeRouterPath={router.pathname}
-              />
-            </Col>
-            <Col
-              md={8}
-              className={classes.right}
-            >
-              <div className={classes.header}>
-                <h1 className={classes.title}>{data.title}</h1>
-                {data.date && <div className={classes.date}>{data.date}</div>}
-              </div>
 
-              <Row>
-                <Col md={11}>
-                  <div className={classes.content}>{content}</div>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </main>
+      <Container className={classes.page}>
+        <div className={classes.left}>
+          <SidebarMenu
+            title={capitalizeString(convertQueryToReadable(menu.title))}
+            menu={menu}
+            activeRouterPath={router.pathname}
+          />
+        </div>
+        <div className={classes.right}>
+          <div className={classes.header}>
+            <h1 className={classes.title}>{data.title}</h1>
+            {data.date && <div className={classes.date}>{data.date}</div>}
+          </div>
+
+          <div className={classes.content}>{content}</div>
+        </div>
+      </Container>
     </div>
   );
 };
