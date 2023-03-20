f4c55718e2fc07344df5ae9c42af26d76ed9aac8
Michael Marszalek <mimarz@gmail.com>
2023-03-20 14:08:15 +0100

    🔧 tweaks to config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,10 +5,13 @@ module.exports = {
     'stylelint-prettier/recommended',
     'stylelint-config-prettier',
   ],
+  plugins: ['stylelint-prettier', 'stylelint-order'],
+  ignoreFiles: ['dist/**.css'],
   rules: {
     'prettier/prettier': true,
+    'order/properties-alphabetical-order': true,
     'custom-property-pattern': null,
-    // 'custom-property-pattern': 'fds-.+', // this rule needs to be activated once we do new tokens
+    // 'custom-property-pattern': 'fds-.+', // this rule needs to replaced the one above once we have new tokens
     'selector-class-pattern': [
       '^[a-z][a-zA-Z0-9]+$',
       {
