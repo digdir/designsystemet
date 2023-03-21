8cd088ee87ab4dd403307abc4f0ed6d1ce07f37f
Michael Marszalek <mimarz@gmail.com>
2023-03-21 12:13:02 +0100

    🔥 Removed stylelint-order for now

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,20 +2,21 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-css-modules',
-    'stylelint-prettier/recommended',
     'stylelint-config-prettier',
   ],
-  plugins: ['stylelint-prettier', 'stylelint-order'],
+  plugins: ['stylelint-prettier'],
   ignoreFiles: ['dist/**.css'],
   rules: {
     'prettier/prettier': true,
-    'order/properties-alphabetical-order': true,
+    'declaration-block-no-redundant-longhand-properties': null,
     'custom-property-pattern': null,
     // 'custom-property-pattern': 'fds-.+', // this rule needs to replaced the one above once we have new tokens
     'selector-class-pattern': [
       '^[a-z][a-zA-Z0-9]+$',
       {
-        message: 'Expected class name to be camelCase',
+        severity: 'warning',
+        message:
+          'Its recommened to use camelCase for local class names when using CSS modules',
       },
     ],
     'alpha-value-notation': 'number',
