12a8894ead3a5e43f2a63b4324809b125bb349ca
Michael Marszalek <mimarz@gmail.com>
2023-03-28 14:26:03 +0200

    refactor: Remove component tokens from design tokens (#249)
    * style: removed button-specific color tokens
    
    * style: added text-on_danger-subtle
    
    * style: added text styles for use on subtle success and danger
    
    * 🚧 Working on figuring out tokens setup
    
    * style: new spacing tokens
    
    * style: remove core spacing from semantic
    
    * 🚧 WIP transforming
    
    * 🍱 Updated tokens because studio broke
    
    * changed font weights to numerical values
    
    * New tweaks
    
    * Categorized spacing tokens again
    
    * Setting fontsize for style dictionary as recommended
    
    * clean
    
    * rename set
    
    * cleaned up some wording
    
    * ✨ New token build files
    
    * Back to dist for built files
    
    ---------
    
    Co-authored-by: Marianne Røsvik <post@mrosvik.no>

--- packages/tokens/build.ts
+++ packages/tokens/build.ts
@@ -1,66 +1,104 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import { noCase } from 'change-case';
-import StyleDictionaryCore from 'style-dictionary';
-import type { Config } from 'style-dictionary';
+import StyleDictionary from 'style-dictionary';
+import type { Config, TransformedToken } from 'style-dictionary';
 
 type Brands = 'Altinn' | 'Digdir' | 'Tilsynet';
 const brands: Brands[] = ['Digdir', 'Tilsynet', 'Altinn'];
+const prefix = 'fds';
+const basePxFontSize = 16;
+/**
+ * Transforms `level1.level2.another_level` to `level1-level2-another_level`
+ * This maintains hierarchy distinction (i.e. underscore is not a hierarchy level separator)
+ */
+StyleDictionary.registerTransform({
+  name: 'name/cti/hierarchical-kebab',
+  type: 'name',
+  transformer: (token, options) => {
+    return noCase([options?.prefix].concat(token.path).join('-'), {
+      delimiter: '-',
+      stripRegexp: /[^A-Z0-9_]+/gi,
+    });
+  },
+});
 
-const getStyleDictionaryConfig = (brand: Brands): Config => {
+StyleDictionary.registerTransform({
+  name: 'typography/shorthand',
+  type: 'value',
+  transitive: true,
+  matcher: (token) => token.type === 'typography',
+  transformer: (token) => {
+    const { value } = token;
+    return `${value.fontWeight} ${value.fontSize}/${value.lineHeight} '${value.fontFamily}'`;
+  },
+});
+
+/**
+ * Transform fontSizes from px to rem
+ */
+StyleDictionary.registerTransform({
+  name: 'pxToRem',
+  type: 'value',
+  transitive: true,
+  matcher: (token) => ['fontSizes'].includes(token.type),
+  transformer: (token, options) =>
+    options?.basePxFontSize
+      ? token.value / options.basePxFontSize + 'rem'
+      : token.value,
+});
+
+const excludeSource = (token: TransformedToken) =>
+  !token.filePath.includes('Core.json');
+
+const getStyleDictionaryConfig = (
+  brand: Brands,
+  targetFolder = 'dist',
+): Config => {
   const tokensPath = '../../design-tokens';
-  const distFolder = `dist/${brand.toLowerCase()}`;
+  const destinationPath = `${targetFolder}/${brand.toLowerCase()}`;
+
   return {
-    source: [
+    include: [
       `${tokensPath}/Brand/${brand}.json`,
-      `${tokensPath}/Base/**/*.json`,
+      `${tokensPath}/Base/Semantic.json`,
     ],
-    transform: {
-      /**
-       * Transforms `level1.level2.another_level` to `level1-level2-another_level`
-       * This maintains hierarchy distinction (i.e. underscore is not a hierarchy level separator)
-       */
-      'name/cti/hierarchical-kebab': {
-        type: 'name',
-        transformer: (token, options) => {
-          return noCase([options?.prefix].concat(token.path).join('-'), {
-            delimiter: '-',
-            stripRegexp: /[^A-Z0-9_]+/gi,
-          });
-        },
-      },
-    },
+    source: [`${tokensPath}/Base/Core.json`],
     platforms: {
       js: {
+        basePxFontSize,
+        transforms: ['name/cti/camel', 'typography/shorthand', 'pxToRem'],
         transformGroup: 'js',
         files: [
           {
-            destination: `${distFolder}/tokens.cjs.js`,
+            destination: `${destinationPath}/tokens.cjs.js`,
             format: 'javascript/module-flat',
+            filter: excludeSource,
           },
           {
-            destination: `${distFolder}/tokens.esm.js`,
+            destination: `${destinationPath}/tokens.esm.js`,
             format: 'javascript/es6',
+            filter: excludeSource,
           },
           {
-            destination: `${distFolder}/tokens.d.ts`,
+            destination: `${destinationPath}/tokens.d.ts`,
             format: 'typescript/es6-declarations',
+            filter: excludeSource,
           },
         ],
       },
       css: {
-        // `transformGroup: "css"` includes the transforms below, except for `name/cti/hierarchical-kebab`
+        prefix,
+        basePxFontSize,
         transforms: [
-          'attribute/cti',
           'name/cti/hierarchical-kebab',
-          'time/seconds',
-          'content/icon',
-          'size/rem',
-          'color/css',
+          'typography/shorthand',
+          'pxToRem',
         ],
         files: [
           {
-            destination: `${distFolder}/tokens.css`,
+            destination: `${destinationPath}/tokens.css`,
             format: 'css/variables',
+            filter: excludeSource,
           },
         ],
       },
@@ -74,11 +112,11 @@ brands.map((brand) => {
   console.log('\n==============================================');
   console.log(`\nProcessing: ${brand}`);
 
-  const StyleDictionary = StyleDictionaryCore.extend(
-    getStyleDictionaryConfig(brand),
+  const extendedStyleDictionary = StyleDictionary.extend(
+    getStyleDictionaryConfig(brand, 'dist'),
   );
 
-  StyleDictionary.buildAllPlatforms();
+  extendedStyleDictionary.buildAllPlatforms();
 
   console.log('\nEnd processing');
 });
