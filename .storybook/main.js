e316d794d4483a0587ff42bac23022b34a31262c
Michael Marszalek <mimarz@gmail.com>
2023-04-17 09:40:33 +0200

    chore: :arrow_up: Upgrade to Storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,23 @@
 const path = require('node:path');
-
 module.exports = {
   stories: ['../packages/**/*.stories.mdx', '../stories/**/*.stories.mdx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-designs',
-    '@storybook/preset-scss',
-    '@etchteam/storybook-addon-status',
-    {
-      name: 'storybook-css-modules',
-      options: {
-        cssModulesLoaderOptions: {
-          importLoaders: 1,
-          modules: {
-            localIdentName: '[name]_[local]__[hash:base64:5]',
-          },
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-designs', '@storybook/preset-scss', '@etchteam/storybook-addon-status', {
+    name: 'storybook-css-modules',
+    options: {
+      cssModulesLoaderOptions: {
+        importLoaders: 1,
+        modules: {
+          localIdentName: '[name]_[local]__[hash:base64:5]'
+        }
+      }
+    }
+  }, '@storybook/addon-mdx-gfm'],
   staticDirs: ['../assets'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
