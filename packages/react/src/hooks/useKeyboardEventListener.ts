a35f6b9ca5dc2457753a0e6e2a590233178c9ea1
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2023-04-19 12:40:17 +0200

    Update packages/react/src/hooks/useKeyboardEventListener.ts
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

--- packages/react/src/hooks/useKeyboardEventListener.ts
+++ packages/react/src/hooks/useKeyboardEventListener.ts
@@ -8,7 +8,7 @@ type Element = HTMLElement | null | undefined;
  * @param onKeyDown The action to perform when the key is pressed. Accepts the KeyboardEvent as a parameter.
  * @param element The element to add the event listener to. No event listener is added if no element is given.
  */
-export function useKeyboardEventListener<T extends Element = undefined>(
+export function useKeyboardEventListener<T extends Element>(
   key: string,
   onKeyDown: () => void,
   element?: T,
