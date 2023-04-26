2f235fc8d80e26bfbf3297725e0764d7efa037f9
Michael Marszalek <mimarz@gmail.com>
2023-04-26 09:37:50 +0200

    chore: :wrench: Updated storybook config files according to official docs

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,39 +1,43 @@
 import type { Preview } from '@storybook/react';
 import '@altinn/figma-design-tokens/dist/tokens.css';
+
 import './inter.css';
-import './customStyling.css';
 import customTheme from './customTheme';
 
-export const parameters: Preview['parameters'] = {
-  status: {
-    statuses: {
-      new: {
-        background: '#0000ff',
-        color: '#ffffff',
-        description: 'This component is stable and released',
-      },
-      beta: {
-        background: '#6544c5',
-        color: '#ffffff',
-        description: 'This component is stable and released',
+const preview: Preview = {
+  parameters: {
+    status: {
+      statuses: {
+        new: {
+          background: '#0000ff',
+          color: '#ffffff',
+          description: 'This component is stable and released',
+        },
+        beta: {
+          background: '#6544c5',
+          color: '#ffffff',
+          description: 'This component is stable and released',
+        },
       },
     },
-  },
-  layout: 'centered',
-  viewMode: 'docs',
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  docs: {
-    theme: customTheme,
-  },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    layout: 'centered',
+    viewMode: 'docs',
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: customTheme,
     },
-  },
-  options: {
-    storySort: {
-      order: ['Komponentoversikt', 'Kjernekomponenter'],
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    options: {
+      storySort: {
+        order: ['Komponentoversikt', 'Kjernekomponenter'],
+      },
     },
   },
 };
+
+export default preview;
