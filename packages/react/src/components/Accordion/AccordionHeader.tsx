2f0d126ab3e1dafafe01fdc614c07d9654633566
Michael Marszalek <mimarz@gmail.com>
2023-05-15 17:16:52 +0200

    Updated Header types

--- packages/react/src/components/Accordion/AccordionHeader.tsx
+++ packages/react/src/components/Accordion/AccordionHeader.tsx
@@ -1,6 +1,6 @@
 import { Expand } from '@navikt/ds-icons';
 import cn from 'classnames';
-import type { MouseEventHandler } from 'react';
+import type { MouseEventHandler, HTMLAttributes } from 'react';
 import React, { forwardRef, useContext } from 'react';
 
 import { Paragraph, Heading } from '../';
@@ -8,72 +8,56 @@ import { Paragraph, Heading } from '../';
 import classes from './Accordion.module.css';
 import { AccordionItemContext } from './AccordionItem';
 
-export interface AccordionHeaderProps
-  extends React.HTMLAttributes<HTMLHeadingElement> {
-  /**
-   * Heading level
-   */
+export type AccordionHeaderProps = {
+  /** Heading level */
   level?: 1 | 2 | 3 | 4 | 5 | 6;
-  /**
-   * Text inside Accordion.Header
-   */
-  children: React.ReactNode;
-  /**
-   * Handle when clicked on header
-   */
+  /** Handle when clicked on header */
   onHeaderClick?: MouseEventHandler<HTMLButtonElement> | undefined;
-}
+} & HTMLAttributes<HTMLHeadingElement>;
 
-export type AccordionHeaderType = React.ForwardRefExoticComponent<
-  AccordionHeaderProps & React.RefAttributes<HTMLHeadingElement>
->;
+export const AccordionHeader = forwardRef<
+  HTMLHeadingElement,
+  AccordionHeaderProps
+>(({ level = 1, children, className, onHeaderClick, ...rest }, ref) => {
+  const context = useContext(AccordionItemContext);
 
-const AccordionHeader: AccordionHeaderType = forwardRef(
-  ({ level = 1, children, className, onHeaderClick, ...rest }, ref) => {
-    const context = useContext(AccordionItemContext);
-
-    if (context === null) {
-      console.error(
-        '<Accordion.Header> has to be used within an <Accordion.Item>',
-      );
-      return null;
-    }
-
-    const handleClick = (
-      e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    ) => {
-      context.toggleOpen();
-      onHeaderClick && onHeaderClick(e);
-    };
-
-    return (
-      <Heading
-        {...rest}
-        ref={ref}
-        size='xsmall'
-        level={level}
-        className={cn(classes.header, className)}
-      >
-        <button
-          type='button'
-          onClick={handleClick}
-          aria-expanded={context.open}
-          aria-controls={context.contentId}
-        >
-          <Expand
-            aria-hidden
-            className={classes.expandIcon}
-          />
-          <Paragraph
-            as='span'
-            size='small'
-          >
-            {children}
-          </Paragraph>
-        </button>
-      </Heading>
+  if (context === null) {
+    console.error(
+      '<Accordion.Header> has to be used within an <Accordion.Item>',
     );
-  },
-);
+    return null;
+  }
 
-export default AccordionHeader;
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    context.toggleOpen();
+    onHeaderClick && onHeaderClick(e);
+  };
+
+  return (
+    <Heading
+      {...rest}
+      ref={ref}
+      size='xsmall'
+      level={level}
+      className={cn(classes.header, className)}
+    >
+      <button
+        type='button'
+        onClick={handleClick}
+        aria-expanded={context.open}
+        aria-controls={context.contentId}
+      >
+        <Expand
+          aria-hidden
+          className={classes.expandIcon}
+        />
+        <Paragraph
+          as='span'
+          size='small'
+        >
+          {children}
+        </Paragraph>
+      </button>
+    </Heading>
+  );
+});
--- packages/react/src/components/Accordion/index.ts
+++ packages/react/src/components/Accordion/index.ts
@@ -3,7 +3,8 @@ import { Accordion as AccordionParent } from './Accordion';
 import AccordionItem from './AccordionItem';
 import type { AccordionContentProps } from './AccordionContent';
 import { AccordionContent } from './AccordionContent';
-import AccordionHeader from './AccordionHeader';
+import type { AccordionHeaderProps } from './AccordionHeader';
+import { AccordionHeader } from './AccordionHeader';
 
 type AccordionComponent = typeof AccordionParent & {
   Item: typeof AccordionItem;
@@ -17,5 +18,5 @@ Accordion.Header = AccordionHeader;
 Accordion.Content = AccordionContent;
 Accordion.Item = AccordionItem;
 
-export type { AccordionProps, AccordionContentProps };
+export type { AccordionProps, AccordionContentProps, AccordionHeaderProps };
 export { Accordion };
