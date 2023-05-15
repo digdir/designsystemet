dae4e958457b0bdeb03b040d6ab8fea41b094d3d
Michael Marszalek <mimarz@gmail.com>
2023-05-15 12:23:41 +0200

    Export all typography except ErrorMessage

--- packages/react/src/components/Typography/index.ts
+++ packages/react/src/components/Typography/index.ts
@@ -3,4 +3,4 @@ export * from './Heading';
 export * from './Ingress';
 export * from './Detail';
 export * from './Label';
-export * from './ErrorMessage';
+// export * from './ErrorMessage'; // TODO re-export after https://github.com/digdir/designsystem/issues/399
