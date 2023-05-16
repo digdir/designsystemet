141a309fb9d5bc78d961d570f8797492054d043c
Michael Marszalek <mimarz@gmail.com>
2023-05-16 12:11:57 +0200

    Update docs-components/TokenBlock/TokenBlock.tsx
    Co-authored-by: Tomas Engebretsen <tomas.engebretsen@digdir.no>

--- docs-components/TokenBlock/TokenBlock.tsx
+++ docs-components/TokenBlock/TokenBlock.tsx
@@ -8,7 +8,7 @@ export const TokenBlock = () => (
     <div className={classes.desc}>
       <Markdown>
         {
-          ' Husk å importer ny tokens pakke **`@digdir/design-system-tokens/<brand>/tokens.css`** før du tar i bruk denne komponenten.\n\n Importer tokens i henhold til ditt brand; `altinn`, `digdir` eller `tilsynet`.'
+          ' Husk å importere ny tokens-pakke **`@digdir/design-system-tokens/<brand>/tokens.css`** før du tar i bruk denne komponenten.\n\n Importer tokens i henhold til ditt brand; `altinn`, `digdir` eller `tilsynet`.'
         }
       </Markdown>
     </div>
