539271fdf2c4e7994bd61344704f3de80143e1ff
LAPTOP-S0QU96IH\david <david@framit.no>
2023-05-26 08:55:29 +0200

    fix(TextField, TextArea): should append element ids to the aria-descriebd-by

--- packages/react/src/components/TextArea/TextArea.tsx
+++ packages/react/src/components/TextArea/TextArea.tsx
@@ -34,6 +34,7 @@ export const TextArea = forwardRef(
       characterLimit,
       value,
       onChange,
+      'aria-describedby': ariaDescribedBy,
       ...rest
     }: TextAreaProps,
     ref: ForwardedRef<HTMLTextAreaElement>,
@@ -66,7 +67,7 @@ export const TextArea = forwardRef(
               value={value}
               onChange={handleOnChange}
               id={inputId}
-              aria-describedby={describedBy}
+              aria-describedby={`${describedBy || ''} ${ariaDescribedBy || ''}`}
               disabled={disabled}
               readOnly={Boolean(readOnly)}
               className={[
