bc7cd3e44dc3cd7cfe99350f141b18fce03b802a
Michael Marszalek <mimarz@gmail.com>
2023-05-30 15:22:52 +0200

    Added beta and title support in `Information` component

--- docs-components/Information/Information.tsx
+++ docs-components/Information/Information.tsx
@@ -3,23 +3,45 @@ import { Markdown } from '@storybook/blocks';
 
 import classes from './Information.module.css';
 
-type Texts = 'token' | 'development';
+type Texts = 'token' | 'development' | 'beta';
 
-const texts: Record<Texts, string> = {
-  token:
-    ' Husk å importere ny tokens-pakke **`@digdir/design-system-tokens/<brand>/tokens.css`** før du tar i bruk denne komponenten.\n\n Importer tokens i henhold til ditt brand; `altinn`, `digdir` eller `tilsynet`.',
-  development:
-    'Komponenten er under utvikling og dermed ikke tilgjenglige i react pakken `@digdir/design-system-react`',
+const texts: Record<
+  Texts,
+  { description: string; title?: string; colorClass?: string }
+> = {
+  token: {
+    title: 'Tokens',
+    description:
+      'Husk å importere ny tokens-pakke **`@digdir/design-system-tokens/brand/<brand>/tokens.css`** før du tar i bruk denne komponenten.\n\n Importer tokens i henhold til ditt brand; `altinn`, `digdir` eller `tilsynet`.',
+  },
+  development: {
+    title: 'Under utvikling',
+    description:
+      'Komponenten er under utvikling og dermed ikke tilgjenglige i react pakken `@digdir/design-system-react`.',
+  },
+  beta: {
+    title: 'Beta',
+    description:
+      'Komponenten er i beta og kan dermed ha mangelfull funksjonalitet eller være flagget for endring. Dette kan medføre breaking-changes i patch/minor versjon av kodepakker.',
+    colorClass: 'beta',
+  },
 };
 
 type InformationProps = {
   text: Texts;
 };
 
-export const Information = ({ text }: InformationProps) => (
-  <div className={classes.container}>
-    <div className={classes.desc}>
-      <Markdown>{texts[text]}</Markdown>
+export const Information = ({ text }: InformationProps) => {
+  const textData = texts[text];
+
+  const color = textData?.colorClass || '';
+
+  return (
+    <div className={classes.container + ' ' + color}>
+      {textData.title && <div className={classes.title}>{textData.title}</div>}
+      <div className={classes.desc}>
+        <Markdown>{textData.description}</Markdown>
+      </div>
     </div>
-  </div>
-);
+  );
+};
