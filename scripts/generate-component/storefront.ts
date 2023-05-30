f831e5be18b0bf1570d0ecbf002aa9d827e6f4cb
Øyvind Thune <oyvlei@gmail.com>
2023-05-30 19:07:32 +0200

    Moved ts-node to root and removed it from tokens package

--- scripts/generate-component/storefront.ts
+++ scripts/generate-component/storefront.ts
@@ -1,27 +1,30 @@
 const componentName = process.argv[2];
 const componentPath = './';
 import { mkdir, writeFile } from 'fs/promises';
-import { mainContent, exportContent, cssContent } from './fileTemplates.mjs';
+
+import { mainContent, exportContent, cssContent } from './fileTemplates';
 
 (async () => {
   // Component folder
-  mkdir(`${componentPath}/${componentName}`).catch(console.error);
+  await mkdir(`${componentPath}/${componentName}`).catch(console.error);
 
   // Component file
-  writeFile(
+  await writeFile(
     `${componentPath}/${componentName}/${componentName}.tsx`,
     mainContent(componentName),
   );
 
   // Css file
-  writeFile(
+  await writeFile(
     `${componentPath}/${componentName}/${componentName}.module.css`,
     cssContent(componentName),
   );
 
   // Export file
-  writeFile(
+  await writeFile(
     `${componentPath}/${componentName}/index.ts`,
     exportContent(componentName),
   );
-})();
+})().catch((e) => {
+  console.log(e);
+});
