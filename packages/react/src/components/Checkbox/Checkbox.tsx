496e722e86a9a7bdbb953d76c02a64ba0a7870e8
Tomas <tomas.engebretsen@gmail.com>
2023-06-08 14:51:50 +0200

    docs(Checkbox): Switch to new format

--- packages/react/src/components/Checkbox/Checkbox.tsx
+++ packages/react/src/components/Checkbox/Checkbox.tsx
@@ -7,35 +7,73 @@ import { CheckboxRadioTemplate } from '../_CheckboxRadioTemplate';
 import classes from './Checkbox.module.css';
 
 export interface CheckboxProps {
+  /**
+   * An ID that will be applied to the ID attribute of the checkbox element.
+   * It will be automatically generated to a random value if not set.
+   */
   checkboxId?: string;
+
+  /**
+   * Set to `true` to disable the checkbox.
+   * This is currently the only way to set the `checked` mode, as uncontrolled mode is not supported yet.
+   */
   checked?: boolean;
+
+  /** Set to `true` to apply compact styling, making the checkbox smaller. */
   compact?: boolean;
+
+  /** The description of the checkbox. This will appear below the label. */
   description?: ReactNode;
+
+  /** Set to `true` to disable the checkbox. */
   disabled?: boolean;
+
+  /** Set to `true` to apply error styling to the checkbox. */
   error?: boolean;
+
+  /** Set this to make a clickable help icon appear to the right for the label. The content will appear when the user clicks on it. */
   helpText?: ReactNode;
+
+  /** Set to `true` to visually hide the label text, but still make it accessible to tools like screen readers. */
   hideLabel?: boolean;
+
+  /** The label of the checkbox. */
   label?: ReactNode;
+
+  /** The name of the checkbox. */
   name?: string;
+
+  /**
+   * A callback that will be called when the checkbox is toggled.
+   * As uncontrolled mode is not supported yet, this function has to be used to toggle between checked and unchecked state.
+   */
   onChange: ChangeEventHandler<HTMLInputElement>;
+
+  /**
+   * Set to `true` if this checkbox is a part of a preview and does not provide anything to the functionality of the application.
+   * In this mode, the label text will not be clickable unless it is a plain text.
+   * This allows having clickable elements inside the label text.
+   */
   presentation?: boolean;
+
+  /** Set to `true` to apply read-only mode. */
   readOnly?: boolean;
 }
 
 const Checkbox = ({
   checkboxId,
-  checked,
-  compact,
+  checked = false,
+  compact = false,
   description,
-  disabled,
-  error,
+  disabled = false,
+  error = false,
   helpText,
   hideLabel,
   label,
   name,
   onChange,
-  presentation,
-  readOnly,
+  presentation = false,
+  readOnly = false,
 }: CheckboxProps) => (
   <CheckboxRadioTemplate
     checked={checked}
