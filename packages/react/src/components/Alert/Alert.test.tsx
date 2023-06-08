8c2e7a2b7c1f52b25d6f28a0939e5c40f4732eb9
Michael Marszalek <mimarz@gmail.com>
2023-06-08 08:44:28 +0200

    Remove screen.debug

--- packages/react/src/components/Alert/Alert.test.tsx
+++ packages/react/src/components/Alert/Alert.test.tsx
@@ -6,22 +6,18 @@ import { Alert } from './Alert';
 describe('Alert', () => {
   test('has correct icon title when severity="danger"', (): void => {
     render(<Alert severity='danger'>Alert me!</Alert>);
-    screen.debug();
     expect(screen.getByTitle('Feil'));
   });
   test('has correct icon title when severity="warning"', (): void => {
     render(<Alert severity='warning'>Alert me!</Alert>);
-    screen.debug();
     expect(screen.getByTitle('Advarsel'));
   });
   test('has correct icon title when severity="success"', (): void => {
     render(<Alert severity='success'>Alert me!</Alert>);
-    screen.debug();
     expect(screen.getByTitle('Suksess'));
   });
   test('has correct icon title when severity is not defined', (): void => {
     render(<Alert>Alert me!</Alert>);
-    screen.debug();
     expect(screen.getByTitle('Informasjon'));
   });
 });
