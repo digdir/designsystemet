6fa21527e994ad37b2aded12c2e2c21a14219d2f
Michael Marszalek <mimarz@gmail.com>
2023-06-08 09:51:25 +0200

    Added some more tests

--- packages/react/src/components/Alert/Alert.test.tsx
+++ packages/react/src/components/Alert/Alert.test.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
+import { Heading } from '../';
+
 import { Alert } from './Alert';
 
 describe('Alert', () => {
-  test('has correct icon title when severity="danger"', (): void => {
+  test('has correct icon title when severity="danger"', () => {
     render(<Alert severity='danger'>Alert me!</Alert>);
     expect(screen.getByTitle('Feil'));
   });
-  test('has correct icon title when severity="warning"', (): void => {
+  test('has correct icon title when severity="warning"', () => {
     render(<Alert severity='warning'>Alert me!</Alert>);
     expect(screen.getByTitle('Advarsel'));
   });
-  test('has correct icon title when severity="success"', (): void => {
+  test('has correct icon title when severity="success"', () => {
     render(<Alert severity='success'>Alert me!</Alert>);
     expect(screen.getByTitle('Suksess'));
   });
-  test('has correct icon title when severity is not defined', (): void => {
+  test('has correct icon title when severity is not defined', () => {
     render(<Alert>Alert me!</Alert>);
     expect(screen.getByTitle('Informasjon'));
+  });
+  test('should render children, heading level 1', () => {
+    render(
+      <Alert severity='info'>
+        <Heading
+          level={1}
+          size='xsmall'
+        >
+          Alert me!
+        </Heading>
+      </Alert>,
+    );
+    expect(screen.getByRole('heading', { level: 1, name: 'Alert me!' }));
+  });
+  test('passed style should apply or override', () => {
+    const style = { color: '#fff', marginTop: '32px' };
+    render(
+      <Alert
+        data-testid='alert'
+        severity='info'
+        style={style}
+        className='testClass'
+      >
+        <Heading
+          level={1}
+          size='xsmall'
+        >
+          Alert me!
+        </Heading>
+      </Alert>,
+    );
+
+    const alert = screen.getByTestId('alert');
+    expect(alert).toHaveStyle(style);
+    expect(alert).toHaveClass('testClass');
   });
 });
