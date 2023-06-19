1a97e29ac169fd04ca2d74d87738853c7e261790
LAPTOP-S0QU96IH\david <david@framit.no>
2023-06-19 22:45:19 +0200

    feat(Chip.Group): should be possible to set size on the whole group once

--- packages/react/src/components/Chip/Group/Group.tsx
+++ packages/react/src/components/Chip/Group/Group.tsx
@@ -2,20 +2,29 @@ import type { HTMLAttributes } from 'react';
 import React, { forwardRef } from 'react';
 import cn from 'classnames';
 
+import type { ChipProps } from '../Chip';
+
 import classes from './Group.module.css';
 
-type GroupProps = HTMLAttributes<HTMLUListElement>;
+export type GroupProps = {
+  size?: 'xsmall' | 'small';
+} & HTMLAttributes<HTMLUListElement>;
 
 export const Group = forwardRef<HTMLUListElement, GroupProps>(
-  ({ children, ...rest }: GroupProps, ref): JSX.Element => (
+  ({ children, size = 'xsmall', ...rest }: GroupProps, ref): JSX.Element => (
     <ul
       {...rest}
       ref={ref}
-      className={cn(classes.list, rest.className)}
+      className={cn(classes.groupContainer, classes[size], rest.className)}
     >
       {React.Children.toArray(children).map(
         (child, index): JSX.Element => (
-          <li key={`${child.toString()}-${index}`}>{child}</li>
+          <li key={`${child.toString()}-${index}`}>
+            {React.isValidElement(child) &&
+              React.cloneElement(child as React.ReactElement<ChipProps>, {
+                size,
+              })}
+          </li>
         ),
       )}
     </ul>
