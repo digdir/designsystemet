c614e133e92bcb977488fc1a710ef42c32ee6611
LAPTOP-S0QU96IH\david <david@framit.no>
2023-06-19 20:11:35 +0200

    refactor(Chip.Removable): simplified the code

--- packages/react/src/components/Chip/Removable/Removable.tsx
+++ packages/react/src/components/Chip/Removable/Removable.tsx
@@ -1,26 +1,28 @@
-import type { HTMLAttributes } from 'react';
 import React, { forwardRef } from 'react';
+import cn from 'classnames';
 import { XMarkIcon } from '@navikt/aksel-icons';
 
 import { Chip } from '../Chip';
+import type { ChipProps } from '../Chip';
 
 import classes from './Removable.module.css';
 
-type RemovableChipProps = {
-  size?: 'xsmall' | 'small';
-} & HTMLAttributes<HTMLButtonElement>;
+type RemovableChipProps = Omit<ChipProps, 'selected'>;
 
 export const RemovableChip = forwardRef(
   ({ children, ...rest }: RemovableChipProps, ref): JSX.Element => {
     return (
       <Chip
-        ref={ref}
         {...rest}
-        className={classes.removable}
+        ref={ref}
+        className={cn(classes.removable, rest.className)}
       >
         <div className={classes.container}>
-          {children}
-          <div className={classes.xMark}>
+          <div>{children}</div>
+          <div
+            className={classes.xMark}
+            aria-hidden
+          >
             <XMarkIcon className={classes.iconSize} />
           </div>
         </div>
