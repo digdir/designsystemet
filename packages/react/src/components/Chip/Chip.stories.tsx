69029786e6a0c6bee58fa786adeee7c1bf3a1319
LAPTOP-S0QU96IH\david <david@framit.no>
2023-06-20 13:09:00 +0200

    feat(Chip): added more docs

--- packages/react/src/components/Chip/Chip.stories.tsx
+++ packages/react/src/components/Chip/Chip.stories.tsx
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 
-import { Chip } from './Chip';
+import { Chip } from './index';
 
 type Story = StoryObj<typeof Chip>;
 
@@ -22,4 +23,32 @@ export const Preview: Story = {
     selected: false,
     checkmark: false,
   },
+};
+
+export const Default: StoryFn<typeof Chip> = () => {
+  return <Chip>Nynorsk</Chip>;
+};
+
+export const ChipWithCheckmark: StoryFn<typeof Chip> = () => {
+  return (
+    <Chip
+      checkmark
+      selected
+    >
+      Nynorsk
+    </Chip>
+  );
+};
+
+export const Removable: StoryFn<typeof Chip> = () => {
+  return <Chip.Removable aria-label='Slett nynorsk'>Nynorsk</Chip.Removable>;
+};
+
+export const Group: StoryFn<typeof Chip> = () => {
+  return (
+    <Chip.Group>
+      <Chip>Nynorsk</Chip>
+      <Chip>Bokmål</Chip>
+    </Chip.Group>
+  );
 };
