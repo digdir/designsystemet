346b8de5fb07bf240ba7dc1ad5eaeba16edc78fc
Tomas <tomas.engebretsen@gmail.com>
2023-06-22 14:11:01 +0200

    refactor: Export all in components/index.ts

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,72 +1,20 @@
-export { Button } from './Button';
-export type { ButtonProps } from './Button';
-export { Tabs } from './Tabs';
-export type { TabsProps, TabItem } from './Tabs';
-export { FieldSet } from './FieldSet';
-export type { FieldSetProps } from './FieldSet';
-export { Checkbox } from './Checkbox';
-export type { CheckboxProps } from './Checkbox';
-export { CheckboxGroup } from './CheckboxGroup';
-export type { CheckboxGroupProps, CheckboxGroupItem } from './CheckboxGroup';
-export { HelpText } from './HelpText';
-export { Popover } from './Popover';
-export type { PopoverProps } from './Popover';
-export { RadioButton } from './RadioButton';
-export type { RadioButtonProps } from './RadioButton';
-export { RadioGroup } from './RadioGroup';
-export type { RadioGroupProps, RadioGroupItem } from './RadioGroup';
-export { Select } from './Select';
-export type {
-  SelectProps,
-  SingleSelectOption,
-  MultiSelectOption,
-  SingleSelectEvent,
-  MultiSelectEvent,
-} from './Select';
+export * from './Button';
+export * from './Tabs';
+export * from './FieldSet';
+export * from './Checkbox';
+export * from './CheckboxGroup';
+export * from './HelpText';
+export * from './Popover';
+export * from './RadioButton';
+export * from './RadioGroup';
+export * from './Select';
 export * from './Spinner';
-export { TextArea } from './TextArea';
-export type { TextAreaProps } from './TextArea';
-export { TextField } from './TextField';
-export type { TextFieldProps, TextFieldFormatting } from './TextField';
-export {
-  LegacyAccordion,
-  LegacyAccordionContent,
-  LegacyAccordionHeader,
-} from './LegacyAccordion';
-export type {
-  LegacyAccordionClickHandler,
-  LegacyAccordionContentProps,
-  LegacyAccordionHeaderProps,
-  LegacyAccordionProps,
-} from './LegacyAccordion';
+export * from './TextArea';
+export * from './TextField';
+export * from './LegacyAccordion';
 export * from './List';
-export {
-  ResponsiveTable,
-  Table,
-  TableBody,
-  TableCell,
-  TableFooter,
-  TableHeader,
-  TableRow,
-} from './Table';
-export type {
-  ChangeProps,
-  ResponsiveTableConfig,
-  ResponsiveTableProps,
-  SortDirection,
-  SortProps,
-  TableBodyProps,
-  TableCellProps,
-  TableFooterProps,
-  TableHeaderProps,
-  TableProps,
-  TableRowProps,
-} from './Table';
-export { ToggleButtonGroup } from './ToggleButtonGroup';
-export type {
-  ToggleButtonGroupProps,
-  ToggleButtonProps,
-} from './ToggleButtonGroup';
+export * from './Table';
+export * from './ToggleButtonGroup';
 export * from './Typography/';
 export * from './Accordion';
 export * from './NativeSelect';
