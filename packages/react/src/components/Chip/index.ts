3f55208897ff463d3400a2f4a935dc8f251d9e7d
LAPTOP-S0QU96IH\david <david@framit.no>
2023-07-14 13:54:18 +0200

    feat(Chip): better typing of Chip component

--- packages/react/src/components/Chip/index.ts
+++ packages/react/src/components/Chip/index.ts
@@ -1,21 +1,25 @@
-import { ChipBase as ParentChip } from './_ChipBase';
 import { Group } from './Group';
 import type { ChipGroupProps } from './Group';
 import { RemovableChip } from './Removable';
 import type { RemovableChipProps } from './Removable';
-import { ToggleChip, type ToggleChipProps } from './Toggle';
+import { ToggleChip } from './Toggle';
+import type { ToggleChipProps } from './Toggle/';
 
-type ChipComponent = typeof ParentChip & {
+type ChipComponent = {
   Group: typeof Group;
   Removable: typeof RemovableChip;
   Toggle: typeof ToggleChip;
 };
 
-const Chip = ParentChip as ChipComponent;
+const Chip: ChipComponent = {
+  Group: Group,
+  Removable: RemovableChip,
+  Toggle: ToggleChip,
+};
 
-Chip.Group = Group;
-Chip.Removable = RemovableChip;
-Chip.Toggle = ToggleChip;
+Chip.Group.displayName = 'Chip.Group';
+Chip.Removable.displayName = 'Chip.Removable';
+Chip.Toggle.displayName = 'Chip.Toggle';
 
 export type { RemovableChipProps, ToggleChipProps, ChipGroupProps };
 export { Chip };
