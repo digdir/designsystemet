8a96ddb59467bec8874e836cdaed8f9668fcccae
Michael Marszalek <mimarz@gmail.com>
2023-07-24 08:52:14 +0200

    refactor(Chip): :recycle: Improve `Chip.Group` application of childrens sizes
    Use context instead of clonelement for applying sizes to children

--- packages/react/src/components/Chip/Group/Group.tsx
+++ packages/react/src/components/Chip/Group/Group.tsx
@@ -1,14 +1,20 @@
 import type { HTMLAttributes } from 'react';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, createContext } from 'react';
 import cn from 'classnames';
 
 import type { ChipBaseProps } from '../_ChipBase';
 
 import classes from './Group.module.css';
 
+export type ChipGroupContext = {
+  size?: ChipBaseProps['size'];
+};
+
+export const ChipGroupContext = createContext<ChipGroupContext | null>(null);
+
 export type ChipGroupProps = {
   /**
-   * Changes padding, font-sizes and gap between chips.
+   * Changes Chip size and gap between chips.
    */
   size?: 'xsmall' | 'small';
 } & HTMLAttributes<HTMLUListElement>;
@@ -20,15 +26,13 @@ export const Group = forwardRef<HTMLUListElement, ChipGroupProps>(
       ref={ref}
       className={cn(classes.groupContainer, classes[size], rest.className)}
     >
-      {React.Children.toArray(children).map((child, index) =>
-        React.isValidElement(child) ? (
-          <li key={`${child.toString()}-${index}`}>
-            {React.cloneElement(child as React.ReactElement<ChipBaseProps>, {
-              size,
-            })}
-          </li>
-        ) : null,
-      )}
+      <ChipGroupContext.Provider value={{ size }}>
+        {React.Children.toArray(children).map((child, index) =>
+          React.isValidElement(child) ? (
+            <li key={`${child.toString()}-${index}`}>{child}</li>
+          ) : null,
+        )}
+      </ChipGroupContext.Provider>
     </ul>
   ),
 );
