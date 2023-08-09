a696a97e128218bda242819714294332d40d014c
Michael Marszalek <mimarz@gmail.com>
2023-08-09 15:28:16 +0200

    Added a simple page for new Fieldset

---
+++ packages/react/src/components/form/Fieldset/Fieldset.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { Meta, StoryFn } from '@storybook/react';
+
+import { Fieldset } from '.';
+
+type Story = StoryFn<typeof Fieldset>;
+
+export default {
+  title: 'ikke utgitt/Fieldset',
+  component: Fieldset,
+} as Meta;
+
+export const Preview: Story = (args) => <Fieldset {...args}></Fieldset>;
