ac0adcc2992499fd850e7974a54a7e4b19dfdd06
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2023-08-16 08:13:43 +0200

    feat(Link): Create the component (#711)
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

---
+++ packages/react/src/components/Link/Link.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+
+import { Paragraph } from '../Typography';
+
+import { Link } from '.';
+
+type Story = StoryObj<typeof Link>;
+
+export default {
+  title: 'Kjernekomponenter/Link',
+  component: Link,
+  parameters: {
+    status: {
+      type: 'beta',
+      url: 'http://www.url.com/status',
+    },
+  },
+} as Meta;
+
+export const Normal: Story = {
+  args: {
+    children: 'Gå til designsystemet',
+    href: 'https://designsystemet.no/',
+  },
+};
+
+export const Invertert: Story = {
+  args: {
+    children: 'Gå til designsystemet',
+    inverted: true,
+    href: 'https://designsystemet.no/',
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          background: '#333333',
+          padding: '32px',
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export const ITekst: StoryFn<typeof Link> = () => (
+  <Paragraph>
+    Vi bruker komponenter fra{' '}
+    <Link href='https://designsystemet.no/'>et fantastisk designsystem</Link>.
+  </Paragraph>
+);
