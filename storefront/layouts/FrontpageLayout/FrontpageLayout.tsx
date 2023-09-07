0d6dd121b79f2782e2fbe2f2277b0af9a43dc298
Øyvind Thune <oyvlei@gmail.com>
2023-09-07 10:55:13 +0200

    Started cleaning frontpage layout

--- storefront/layouts/FrontpageLayout/FrontpageLayout.tsx
+++ storefront/layouts/FrontpageLayout/FrontpageLayout.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import {
-  Section,
-  NavigationCard,
-  Banner,
-  ImageSection,
-} from '../../components';
+import { Section, NavigationCard, ImageSection } from '../../components';
 import type { NavigationCardProps, ImageSectionProps } from '../../components';
 
-interface FrontpageLayoutProps {
+type FrontpageLayoutProps = {
   Content: React.ReactNode;
   data: FrontpageData;
-}
+};
 
-interface FrontpageData {
+type FrontpageData = {
   title: string;
   description: string;
   navigationCards: {
@@ -22,44 +17,38 @@ interface FrontpageData {
     items: [];
   };
   contributeSection: ImageSectionProps;
-}
+};
 
 const FrontpageLayout = ({ Content, data }: FrontpageLayoutProps) => {
   return (
     <main id='main'>
       {Content}
-      <div className='max-width-container main'>
-        <Banner
-          title={data.title}
-          desc={data.description}
-        />
 
-        <Section title={data.navigationCards.title}>
-          {data.navigationCards.items.map(
-            (item: NavigationCardProps, index: number) => (
-              <NavigationCard
-                key={index}
-                url={item.url}
-                title={item.title}
-                color={item.color}
-                icon={item.icon}
-                description={item.description}
-              />
-            ),
-          )}
-        </Section>
+      <Section title={data.navigationCards.title}>
+        {data.navigationCards.items.map(
+          (item: NavigationCardProps, index: number) => (
+            <NavigationCard
+              key={index}
+              url={item.url}
+              title={item.title}
+              color={item.color}
+              icon={item.icon}
+              description={item.description}
+            />
+          ),
+        )}
+      </Section>
 
-        <ImageSection
-          title={data.contributeSection.title}
-          description={data.contributeSection.description}
-          imgSrc={data.contributeSection.imgSrc}
-          imgAlt={data.contributeSection.imgAlt}
-          headingLevel={data.contributeSection.headingLevel}
-          imgWidth={data.contributeSection.imgWidth}
-          imgHeight={data.contributeSection.imgHeight}
-          buttons={data.contributeSection.buttons}
-        />
-      </div>
+      <ImageSection
+        title={data.contributeSection.title}
+        description={data.contributeSection.description}
+        imgSrc={data.contributeSection.imgSrc}
+        imgAlt={data.contributeSection.imgAlt}
+        headingLevel={data.contributeSection.headingLevel}
+        imgWidth={data.contributeSection.imgWidth}
+        imgHeight={data.contributeSection.imgHeight}
+        buttons={data.contributeSection.buttons}
+      />
     </main>
   );
 };
