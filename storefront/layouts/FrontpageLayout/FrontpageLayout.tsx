3f0062e02014226bbcf09fb580b7e9f9570a5204
Øyvind Thune <oyvlei@gmail.com>
2023-09-07 13:53:09 +0200

    Cleaned frontpageLayout

--- storefront/layouts/FrontpageLayout/FrontpageLayout.tsx
+++ storefront/layouts/FrontpageLayout/FrontpageLayout.tsx
@@ -1,57 +1,11 @@
 import React from 'react';
 
-import { Section, NavigationCard, ImageSection } from '../../components';
-import type { NavigationCardProps, ImageSectionProps } from '../../components';
-
 type FrontpageLayoutProps = {
-  Content: React.ReactNode;
-  data: FrontpageData;
+  content: React.ReactNode;
 };
 
-type FrontpageData = {
-  title: string;
-  description: string;
-  navigationCards: {
-    title: string;
-    description: string;
-    items: [];
-  };
-  contributeSection: ImageSectionProps;
-};
-
-const FrontpageLayout = ({ Content, data }: FrontpageLayoutProps) => {
-  return (
-    <main id='main'>
-      {Content}
-
-      <Section title={data.navigationCards.title}>
-        {data.navigationCards.items.map(
-          (item: NavigationCardProps, index: number) => (
-            <NavigationCard
-              key={index}
-              url={item.url}
-              title={item.title}
-              color={item.color}
-              icon={item.icon}
-              description={item.description}
-            />
-          ),
-        )}
-      </Section>
-
-      <ImageSection
-        title={data.contributeSection.title}
-        description={data.contributeSection.description}
-        imgSrc={data.contributeSection.imgSrc}
-        imgAlt={data.contributeSection.imgAlt}
-        headingLevel={data.contributeSection.headingLevel}
-        imgWidth={data.contributeSection.imgWidth}
-        imgHeight={data.contributeSection.imgHeight}
-        buttons={data.contributeSection.buttons}
-      />
-    </main>
-  );
+const FrontpageLayout = ({ content }: FrontpageLayoutProps) => {
+  return <main id='main'>{content}</main>;
 };
 
 export { FrontpageLayout };
-export type { FrontpageData };
