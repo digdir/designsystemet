145064083a10cb5bd94263b4baac6de199751846
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-09-12 13:55:42 +0200

    set font-family on root

--- storefront/pages/_app.tsx
+++ storefront/pages/_app.tsx
@@ -6,21 +6,18 @@ import '@digdir/design-system-tokens/brand/digdir/tokens.css';
 import '../globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import cn from 'classnames';
 import { Analytics } from '@vercel/analytics/react';
 
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
 import { JumpToMain } from '../components/JumpToMain';
 
-import classes from './_app.module.css';
-
 const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={cn(classes.appRoot, 'root')}>
+      <div className={'root'}>
         <JumpToMain />
         <Header />
         <Component {...pageProps} />
