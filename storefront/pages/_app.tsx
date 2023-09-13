bd819f9a9c4f9df2b3854e42f02ed78e59fc81bd
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-09-13 09:13:31 +0200

    chore(storefront): Update storefront to use the Altinn CDN for Inter font (#809)

--- storefront/pages/_app.tsx
+++ storefront/pages/_app.tsx
@@ -4,29 +4,20 @@ import 'tippy.js/dist/tippy.css';
 import '@altinn/figma-design-tokens/dist/tokens.css';
 import '@digdir/design-system-tokens/brand/digdir/tokens.css';
 import '../globals.css';
-import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import cn from 'classnames';
 import { Analytics } from '@vercel/analytics/react';
 
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
 import { JumpToMain } from '../components/JumpToMain';
 
-const inter = Inter({
-  weight: ['400', '500', '600', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'fallback',
-});
-
 const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={cn(inter.className, 'root')}>
+      <div className={'root'}>
         <JumpToMain />
         <Header />
         <Component {...pageProps} />
