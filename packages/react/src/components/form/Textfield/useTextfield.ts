0c14cfa3c6add379ada2f5e11dd2b6a0de05aa56
Michael Marszalek <mimarz@gmail.com>
2023-09-21 12:34:22 +0200

    feat(Textfield): :sparkles: New `Textfield` component (#816)

---
+++ packages/react/src/components/form/Textfield/useTextfield.ts
@@ -0,0 +1,44 @@
+import type { InputHTMLAttributes } from 'react';
+import { useContext } from 'react';
+
+import type { FormField } from '../useFormField';
+import { useFormField } from '../useFormField';
+import { FieldsetContext } from '../Fieldset';
+
+import type { TextfieldProps } from './Textfield';
+
+type UseTextfield = (props: TextfieldProps) => FormField & {
+  inputProps?: Pick<
+    InputHTMLAttributes<HTMLInputElement>,
+    'readOnly' | 'type' | 'name' | 'required' | 'onClick' | 'onChange'
+  >;
+};
+/** Handles props for `Textfield` in context with `Fieldset` */
+export const useTextfield: UseTextfield = (props) => {
+  const fieldset = useContext(FieldsetContext);
+  const { inputProps, readOnly, ...rest } = useFormField(props, 'textfield');
+
+  return {
+    ...rest,
+    readOnly,
+    size: fieldset?.size ?? props.size,
+    inputProps: {
+      ...inputProps,
+      readOnly,
+      onClick: (e) => {
+        if (readOnly) {
+          e.preventDefault();
+          return;
+        }
+        props?.onClick?.(e);
+      },
+      onChange: (e) => {
+        if (readOnly) {
+          e.preventDefault();
+          return;
+        }
+        props?.onChange?.(e);
+      },
+    },
+  };
+};
