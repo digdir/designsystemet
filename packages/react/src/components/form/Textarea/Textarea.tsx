fd4dbe4661e25ebfbb37174b3a3fa2d1881b74c9
Michael Marszalek <mimarz@gmail.com>
2023-09-22 10:29:08 +0200

    feat(Textarea): :sparkles: New `Textarea` component

---
+++ packages/react/src/components/form/Textarea/Textarea.tsx
@@ -0,0 +1,145 @@
+import type { ReactNode, TextareaHTMLAttributes } from 'react';
+import React, { useState, useId, forwardRef } from 'react';
+import cn from 'classnames';
+import { PadlockLockedFillIcon } from '@navikt/aksel-icons';
+
+import { omit } from '../../../utils';
+import { Label, Paragraph, ErrorMessage } from '../../Typography';
+import type { FormFieldProps } from '../useFormField';
+import type { CharacterLimitProps } from '../CharacterCounter';
+import { CharacterCounter } from '../CharacterCounter';
+
+import { useTextarea } from './useTextarea';
+import classes from './Textarea.module.css';
+import utilityClasses from './../../../utils/utility.module.css';
+
+export type TextareaProps = {
+  /** Label */
+  label?: ReactNode;
+  /** Visually hides `label` and `description` (still available for screen readers)  */
+  hideLabel?: boolean;
+  /** Changes field size and paddings */
+  size?: 'xsmall' | 'small' | 'medium' | 'large';
+
+  /**
+   *  The characterLimit function calculates remaining characters based on `maxCount`
+   *
+   *  Provide a `label` function that takes count as parameter and returns a message.
+   *
+   *  Use `srLabel` to describe `maxCount` for screen readers.
+   *
+   *  Defaults to Norwegian if no labels are provided.
+   */
+  characterLimit?: CharacterLimitProps;
+} & Omit<FormFieldProps, 'size'> &
+  Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'size'>;
+
+/** Textarea field
+ *
+ * @example
+ * ```tsx
+ * <Textarea label="Textarea label">
+ * ```
+ */
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (props, ref) => {
+    const { label, description, style, characterLimit, hideLabel, ...rest } =
+      props;
+
+    const {
+      inputProps,
+      descriptionId,
+      hasError,
+      errorId,
+      size = 'medium',
+      readOnly,
+    } = useTextarea(props);
+
+    const [inputValue, setInputValue] = useState(props.defaultValue);
+    const characterLimitId = `textfield-charactercount-${useId()}`;
+    const hasCharacterLimit = characterLimit != null;
+
+    const describedBy = cn(
+      inputProps['aria-describedby'],
+      hasCharacterLimit && characterLimitId,
+    );
+
+    return (
+      <Paragraph
+        as='div'
+        size={size}
+        style={style}
+        className={cn(
+          classes.formField,
+          inputProps.disabled && classes.disabled,
+          readOnly && classes.readonly,
+          rest.className,
+        )}
+      >
+        {label && (
+          <Label
+            size={size}
+            weight='medium'
+            htmlFor={inputProps.id}
+            className={cn(
+              classes.label,
+              hideLabel && utilityClasses.visuallyHidden,
+            )}
+          >
+            {readOnly && (
+              <PadlockLockedFillIcon
+                aria-hidden
+                className={classes.padlock}
+              />
+            )}
+            <span>{label}</span>
+          </Label>
+        )}
+        {description && (
+          <Paragraph
+            id={descriptionId}
+            as='div'
+            size={size}
+            className={cn(
+              classes.description,
+              hideLabel && utilityClasses.visuallyHidden,
+            )}
+          >
+            {description}
+          </Paragraph>
+        )}
+        <textarea
+          {...omit(['size', 'error', 'errorId'], rest)}
+          {...inputProps}
+          className={cn(
+            classes.textarea,
+            utilityClasses.focusable,
+            classes[size],
+          )}
+          ref={ref}
+          aria-describedby={describedBy}
+          onChange={(e) => {
+            inputProps?.onChange?.(e);
+            setInputValue(e.target.value);
+          }}
+        />
+        {hasCharacterLimit && (
+          <CharacterCounter
+            size={size}
+            value={inputValue ? inputValue.toString() : ''}
+            id={characterLimitId}
+            {...characterLimit}
+          />
+        )}
+        <div
+          className={classes.errorMessage}
+          id={errorId}
+          aria-live='polite'
+          aria-relevant='additions removals'
+        >
+          {hasError && <ErrorMessage size={size}>{props.error}</ErrorMessage>}
+        </div>
+      </Paragraph>
+    );
+  },
+);
---
+++ packages/react/src/components/form/Textarea/useTextarea.ts
@@ -0,0 +1,45 @@
+import type { TextareaHTMLAttributes } from 'react';
+import { useContext } from 'react';
+
+import type { FormField } from '../useFormField';
+import { useFormField } from '../useFormField';
+import { FieldsetContext } from '../Fieldset';
+
+import type { TextareaProps } from './Textarea';
+
+type UseTextarea = (props: TextareaProps) => FormField & {
+  inputProps?: Pick<
+    TextareaHTMLAttributes<HTMLTextAreaElement>,
+    'readOnly' | 'name' | 'required' | 'onClick' | 'onChange'
+  >;
+};
+
+/** Handles props for `Textarea` in context with `Fieldset` */
+export const useTextarea: UseTextarea = (props) => {
+  const fieldset = useContext(FieldsetContext);
+  const { inputProps, readOnly, ...rest } = useFormField(props, 'textarea');
+
+  return {
+    ...rest,
+    readOnly,
+    size: fieldset?.size ?? props.size,
+    inputProps: {
+      ...inputProps,
+      readOnly,
+      onClick: (e) => {
+        if (readOnly) {
+          e.preventDefault();
+          return;
+        }
+        props?.onClick?.(e);
+      },
+      onChange: (e) => {
+        if (readOnly) {
+          e.preventDefault();
+          return;
+        }
+        props?.onChange?.(e);
+      },
+    },
+  };
+};
