bc45c2e50a941fdd49028607fa3d94c4876e4cc0
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-09-28 13:12:09 +0200

    feat(ToggleGroup): ✨ New `ToggleGroup` component (#813)
    Co-authored-by: Michael Marszalek <mimarz@users.noreply.github.com>
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

---
+++ packages/react/src/components/ToggleGroup/ToggleGroupItem/useToggleGroupitem.ts
@@ -0,0 +1,43 @@
+import { useContext, useId } from 'react';
+
+import { ToggleGroupContext } from '../ToggleGroup';
+import type { ButtonProps } from '../../Button';
+
+import type { ToggleGroupItemProps } from './ToggleGroupItem';
+
+type UseToggleGroupItem = (props: ToggleGroupItemProps) => {
+  active: boolean;
+  size?: 'small' | 'medium' | 'large';
+  buttonProps?: Pick<
+    ButtonProps,
+    'id' | 'onClick' | 'role' | 'aria-checked' | 'aria-current' | 'name'
+  >;
+};
+
+/** Handles props for `ToggleGroup.Item` in context with `ToggleGroup` and `RovingTabIndex` */
+export const useToggleGroupItem: UseToggleGroupItem = (
+  props: ToggleGroupItemProps,
+) => {
+  const { ...rest } = props;
+  const toggleGroup = useContext(ToggleGroupContext);
+  const itemValue =
+    props.value ?? (typeof props.children === 'string' ? props.children : '');
+  const active = toggleGroup.value == itemValue;
+  const buttonId = `togglegroup-item-${useId()}`;
+
+  return {
+    ...rest,
+    active: active,
+    size: toggleGroup?.size,
+    buttonProps: {
+      id: buttonId,
+      'aria-checked': active,
+      'aria-current': active,
+      role: 'radio',
+      name: toggleGroup.name,
+      onClick: () => {
+        toggleGroup.onChange?.(itemValue);
+      },
+    },
+  };
+};
