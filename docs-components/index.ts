c57f9c180cbf37e658b79a746395e3a846587dd7
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-10-04 08:31:10 +0200

    use stack component and use as wrapper

--- docs-components/index.ts
+++ docs-components/index.ts
@@ -8,5 +8,4 @@ export {
 export { Changelog } from './Changelog/Changelog';
 export { Stack } from './Stack/Stack';
 export { Do, Dont } from './DoAndDont/DoAndDont';
-export { FlexContainer } from './FlexContainer/FlexContainer';
 export * from './Information/Information';
