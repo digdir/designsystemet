707902abf3b6c3ed809ad4299150272e62579a2a
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-10-04 14:16:16 +0200

    add padding to tab content

--- packages/react/src/components/Tabs/TabContent/TabContent.tsx
+++ packages/react/src/components/Tabs/TabContent/TabContent.tsx
@@ -1,7 +1,10 @@
 import type { HTMLAttributes } from 'react';
 import React, { forwardRef, useContext } from 'react';
+import cn from 'classnames';
 
 import { TabsContext } from '../Tabs';
+
+import classes from './TabContent.module.css';
 
 export type TabContentProps = {
   /** When this value is selected as the current state, render this `TabContent` component*/
@@ -10,14 +13,15 @@ export type TabContentProps = {
 
 export const TabContent = forwardRef<HTMLDivElement, TabContentProps>(
   ({ children, value, ...rest }, ref) => {
-    const tabs = useContext(TabsContext);
-    const active = value == tabs.value;
+    const { value: tabsValue, size = 'medium' } = useContext(TabsContext);
+    const active = value == tabsValue;
 
     return (
       <>
         {active && (
           <div
             {...rest}
+            className={cn(classes[size], classes.tabContent, rest.className)}
             ref={ref}
           >
             {children}
