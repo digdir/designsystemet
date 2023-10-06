d9d5310a42e1d2e6e1479d246ee34e2bbb6ef2fe
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-10-06 14:25:46 +0200

    add forwardRef, props and stories

--- packages/react/src/components/Tooltip/Tooltip.tsx
+++ packages/react/src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import type { HTMLAttributes } from 'react';
+import React, { cloneElement, forwardRef, useMemo, useState } from 'react';
+import cn from 'classnames';
 import type { Placement } from '@floating-ui/react';
 import {
   useFloating,
@@ -12,94 +14,136 @@ import {
   useDismiss,
   useRole,
   useInteractions,
+  useTransitionStyles,
   FloatingArrow,
   FloatingPortal,
 } from '@floating-ui/react';
 
 import styles from './Tooltip.module.css';
+import mergeRefs from './utils';
 
 const ARROW_HEIGHT = 7;
 const GAP = 2;
 
 type TooltipProps = {
+  children: React.ReactElement & React.RefAttributes<HTMLElement>;
   content: string;
   /** Placement of the tooltip on the trigger. Defaults to 'top' */
   placement?: Placement;
   /** Delay in milliseconds before the tooltip appears. Defaults to 0 */
   delay?: number;
-  children: React.ReactNode;
-};
+  open?: boolean;
+  defaultOpen?: boolean;
+} & HTMLAttributes<HTMLDivElement>;
 
-export const Tooltip = ({
-  content,
-  placement = 'top',
-  delay = 0,
-  children,
-}: TooltipProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
+  (
+    {
+      children,
+      content,
+      placement = 'top',
+      delay = 0,
+      open: userOpen,
+      defaultOpen = false,
+      className,
+      ...restHTMLProps
+    },
+    ref,
+  ) => {
+    const [isOpen, setIsOpen] = useState(defaultOpen);
 
-  const arrowRef = React.useRef<SVGSVGElement>(null);
+    const arrowRef = React.useRef<SVGSVGElement>(null);
 
-  const { refs, floatingStyles, context } = useFloating({
-    open: isOpen,
-    onOpenChange: setIsOpen,
-    placement,
-    // Make sure the tooltip stays on the screen
-    whileElementsMounted: autoUpdate,
-    middleware: [
-      offset(ARROW_HEIGHT + GAP),
-      flip({
-        fallbackAxisSideDirection: 'start',
-      }),
-      shift(),
-      arrow({
-        element: arrowRef,
-      }),
-    ],
-  });
+    const { refs, floatingStyles, context } = useFloating({
+      open: userOpen ?? isOpen,
+      onOpenChange: setIsOpen,
+      placement,
+      // Make sure the tooltip stays on the screen
+      whileElementsMounted: autoUpdate,
+      middleware: [
+        offset(ARROW_HEIGHT + GAP),
+        flip({
+          fallbackAxisSideDirection: 'start',
+        }),
+        shift(),
+        arrow({
+          element: arrowRef,
+        }),
+      ],
+    });
 
-  // Event listeners to change the open state
-  const hover = useHover(context, { move: false, delay });
-  const focus = useFocus(context);
-  const dismiss = useDismiss(context);
-  // Role props for screen readers
-  const role = useRole(context, { role: 'tooltip' });
+    const { styles: animationStyles } = useTransitionStyles(context, {
+      initial: {
+        opacity: 0,
+      },
+    });
 
-  // Merge all the interactions into prop getters
-  const { getReferenceProps, getFloatingProps } = useInteractions([
-    hover,
-    focus,
-    dismiss,
-    role,
-  ]);
+    const { getReferenceProps, getFloatingProps } = useInteractions([
+      // Event listeners to change the open state
+      useHover(context, { move: false, delay }),
+      useFocus(context),
+      useDismiss(context),
+      // Role props for screen readers
+      useRole(context, { role: 'tooltip' }),
+    ]);
 
-  return (
-    <>
-      <div
-        ref={refs.setReference}
-        {...getReferenceProps()}
-      >
-        {children}
-      </div>
-      <FloatingPortal>
-        {isOpen && (
-          <>
-            <div
-              className={styles.wrapper}
-              ref={refs.setFloating}
-              style={floatingStyles}
-              {...getFloatingProps()}
-            >
-              {content}
-              <FloatingArrow
-                ref={arrowRef}
-                context={context}
-                fill='#232323'
-              />
-            </div>
-          </>
+    const mergedRef = useMemo(
+      () => mergeRefs([ref, refs.setFloating]),
+      [refs.setFloating, ref],
+    );
+    const childMergedRef = useMemo(
+      () =>
+        mergeRefs([
+          (children as React.ReactElement & React.RefAttributes<HTMLElement>)
+            .ref as React.MutableRefObject<HTMLElement>,
+          refs.setReference,
+        ]),
+      [children, refs.setReference],
+    );
+
+    if (
+      !children ||
+      children?.type === React.Fragment ||
+      (children as unknown) === React.Fragment
+    ) {
+      console.error(
+        '<Tooltip> children needs to be a single ReactElement and not: <React.Fragment/> | <></>',
+      );
+      return null;
+    }
+
+    return (
+      <>
+        {cloneElement(
+          children,
+          getReferenceProps({
+            ref: childMergedRef,
+          }),
         )}
-      </FloatingPortal>
-    </>
-  );
-};
+        <FloatingPortal>
+          {isOpen && (
+            <>
+              <div
+                ref={refs.setFloating}
+                style={{ ...floatingStyles, ...animationStyles }}
+                {...getFloatingProps({
+                  ...restHTMLProps,
+                  className: cn(styles.wrapper, className),
+                  ref: mergedRef,
+                })}
+              >
+                {content}
+                <FloatingArrow
+                  ref={arrowRef}
+                  context={context}
+                  fill='#232323'
+                  height={ARROW_HEIGHT}
+                />
+              </div>
+            </>
+          )}
+        </FloatingPortal>
+      </>
+    );
+  },
+);
---
+++ packages/react/src/components/Tooltip/utils.tsx
@@ -0,0 +1,15 @@
+import type React from 'react';
+// https://github.com/gregberge/react-merge-refs
+export default function mergeRefs<T>(
+  refs: Array<React.MutableRefObject<T> | React.LegacyRef<T>>,
+): React.RefCallback<T> {
+  return (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === 'function') {
+        ref(value);
+      } else if (ref !== null && ref !== undefined) {
+        (ref as React.MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
+}
