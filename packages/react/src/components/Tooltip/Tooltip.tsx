e3b45cd222efa55fa63529ebc29ac99297cc6c21
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-10-06 13:06:51 +0200

    add arrow, placement and delay

--- packages/react/src/components/Tooltip/Tooltip.tsx
+++ packages/react/src/components/Tooltip/Tooltip.tsx
@@ -6,24 +6,39 @@ import {
   offset,
   flip,
   shift,
+  arrow,
   useHover,
   useFocus,
   useDismiss,
   useRole,
   useInteractions,
+  FloatingArrow,
   FloatingPortal,
 } from '@floating-ui/react';
 
 import styles from './Tooltip.module.css';
 
+const ARROW_HEIGHT = 7;
+const GAP = 2;
+
 type TooltipProps = {
   content: string;
+  /** Placement of the tooltip on the trigger. Defaults to 'top' */
   placement?: Placement;
+  /** Delay in milliseconds before the tooltip appears. Defaults to 0 */
+  delay?: number;
   children: React.ReactNode;
 };
 
-export const Tooltip = ({ content, placement, children }: TooltipProps) => {
+export const Tooltip = ({
+  content,
+  placement = 'top',
+  delay = 0,
+  children,
+}: TooltipProps) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const arrowRef = React.useRef<SVGSVGElement>(null);
 
   const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
@@ -32,16 +47,19 @@ export const Tooltip = ({ content, placement, children }: TooltipProps) => {
     // Make sure the tooltip stays on the screen
     whileElementsMounted: autoUpdate,
     middleware: [
-      offset(5),
+      offset(ARROW_HEIGHT + GAP),
       flip({
         fallbackAxisSideDirection: 'start',
       }),
       shift(),
+      arrow({
+        element: arrowRef,
+      }),
     ],
   });
 
   // Event listeners to change the open state
-  const hover = useHover(context, { move: false });
+  const hover = useHover(context, { move: false, delay });
   const focus = useFocus(context);
   const dismiss = useDismiss(context);
   // Role props for screen readers
@@ -65,14 +83,21 @@ export const Tooltip = ({ content, placement, children }: TooltipProps) => {
       </div>
       <FloatingPortal>
         {isOpen && (
-          <div
-            className={styles.wrapper}
-            ref={refs.setFloating}
-            style={floatingStyles}
-            {...getFloatingProps()}
-          >
-            {content}
-          </div>
+          <>
+            <div
+              className={styles.wrapper}
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+            >
+              {content}
+              <FloatingArrow
+                ref={arrowRef}
+                context={context}
+                fill='#232323'
+              />
+            </div>
+          </>
         )}
       </FloatingPortal>
     </>
