e624cdb59f86e5e1410c8b117f5ec32b28537b75
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-10-11 08:42:35 +0200

    update stories of Tooltip

---
+++ packages/react/src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
+import React from 'react';
+
+import { Paragraph, Button } from '../..';
+
+import { Tooltip } from '.';
+
+type Story = StoryObj<typeof Tooltip>;
+
+const defaultChildren = (
+  <Button
+    style={{
+      margin: '3rem',
+    }}
+  >
+    My trigger
+  </Button>
+);
+
+export default {
+  title: 'Felles/Tooltip',
+  component: Tooltip,
+} as Meta;
+
+export const Preview: Story = {
+  args: {
+    content: 'Tooltip text',
+    children: defaultChildren,
+  },
+};
+
+export const Placement: Story = {
+  args: {
+    content: 'Tooltip text',
+    placement: 'bottom',
+    children: defaultChildren,
+  },
+};
+
+export const DefaultOpen: Story = {
+  args: {
+    content: 'Tooltip text',
+    defaultOpen: true,
+    children: defaultChildren,
+  },
+};
+
+export const Complex: StoryFn<typeof Tooltip> = () => {
+  return (
+    <Paragraph>
+      Du kan ha{' '}
+      <Tooltip content='Kan gi bra brukeropplevelse'>
+        <span
+          style={{ fontWeight: 'bold', textDecoration: 'underline dotted' }}
+        >
+          tooltip
+        </span>
+      </Tooltip>{' '}
+      inne i tekst også
+    </Paragraph>
+  );
+};
