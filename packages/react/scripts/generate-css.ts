3a1df9cbdfc19fe2b8818db39ae6bed922685b95
Michael Marszalek <mimarz@gmail.com>
2023-10-11 09:53:07 +0200

    Cleaned up some generator code

--- packages/react/scripts/generate-css.ts
+++ packages/react/scripts/generate-css.ts
@@ -5,6 +5,7 @@ import cssnano from 'cssnano';
 import postcssModules from 'postcss-modules';
 import glob from 'fast-glob';
 import fs from 'fs-extra';
+import * as R from 'ramda';
 
 import { generateScopedName } from './rollup/hash-name';
 
@@ -24,9 +25,9 @@ console.log({
   allCssModules: modules,
 });
 
-function prepareFileName(filePath: string) {
-  return path.basename(filePath).replace('.module.css', '.css');
-}
+// function prepareFileName(filePath: string) {
+//   return path.basename(filePath).replace('.module.css', '.css');
+// }
 
 const outputFolder = path.resolve(__dirname, '../../css');
 fs.ensureDirSync(outputFolder);
@@ -35,32 +36,29 @@ async function createFiles() {
   // group files that are under src/components/{THIS IS THE NAME}
   const components: { [key: string]: string[] } = {};
   modules.forEach((file) => {
-    let componentName = '';
-    if (file.includes('/utilities/')) {
-      componentName = file.split('/src/')[1];
-    } else {
-      componentName = file.split('/components/')[1];
+    let componentName = file.split('/src/')[1];
+
+    if (file.includes('utilities/')) {
+      componentName = componentName.split('utilities/')[1].split('/')[0];
     }
 
     if (componentName.includes('legacy/')) {
       componentName = componentName.split('legacy/')[1].split('/')[0];
-    } else if (componentName.includes('utilities/')) {
-      componentName = componentName.split('utilities/')[1].split('/')[0];
-    } else {
-      // find first uppercase letter
-      const filenameParts = componentName.split('/');
-      const uppercasePart = filenameParts.find(
-        (part) => part[0] === part[0].toUpperCase(),
-      );
-
-      if (!uppercasePart) {
-        throw new Error(
-          `Could not find uppercase part in ${componentName} from ${file}`,
-        );
-      }
-
-      componentName = uppercasePart;
     }
+
+    // find first uppercase letter
+    const filenameParts = componentName.split('/');
+    const uppercasePart = filenameParts.find(
+      (part) => part[0] === part[0].toUpperCase(),
+    );
+
+    if (!uppercasePart) {
+      throw new Error(
+        `Could not find uppercase part in ${componentName} from ${file}`,
+      );
+    }
+
+    componentName = uppercasePart;
 
     if (!components[componentName]) {
       components[componentName] = [];
@@ -109,7 +107,9 @@ async function createFiles() {
           path.join(outputFolder, 'legacy', `${componentName}.css`),
           result.css,
         );
-      } else if (files[0].includes('utilities')) {
+      }
+
+      if (files[0].includes('utilities')) {
         // if utilities folder does not exist, create it
         if (!fs.existsSync(path.join(outputFolder, 'utilities'))) {
           fs.mkdirSync(path.join(outputFolder, 'utilities'));
@@ -140,50 +140,43 @@ async function createFiles() {
         result.css,
       );
     }),
-  ).then(async () => {
-    const utlityFiles = generatedComponents.filter((file) =>
-      file.includes('utilities/'),
+  ).then(() => {
+    const [utilityFiles, componentFiles] = R.partition(
+      R.includes('utilities/'),
+      generatedComponents,
     );
-    await generateConcactenatedUtilities(utlityFiles);
-    await concatIntoGlobal(generatedComponents);
+
+    createUtilities(utilityFiles);
+    createIndex(['utilities.css', ...componentFiles]);
   });
 
   console.log('Done generating css files');
 }
 
-async function generateConcactenatedUtilities(utlityFiles: string[]) {
-  await new Promise<void>((resolve) => {
-    const cssFilesContent = utlityFiles.map(
-      (file) => `@import url('${file}');`,
-    );
-    fs.writeFileSync(
-      path.join(outputFolder, 'utilities.css'),
-      cssFilesContent.join('\n'),
-    );
-    resolve();
-  });
+function createUtilities(utlityFiles: string[]) {
+  const cssFilesContent = utlityFiles.map((file) => `@import url('${file}');`);
+  fs.writeFileSync(
+    path.join(outputFolder, 'utilities.css'),
+    cssFilesContent.join('\n'),
+  );
 }
 
-async function concatIntoGlobal(cssFiles: string[]) {
-  await new Promise<void>((resolve) => {
-    fs.writeJsonSync(
-      path.join(__dirname, '../../css/css-exports.json'),
-      {
-        modules: cssFiles.filter((file) => !file.includes('global.css')),
-        index: prepareFileName('index.css'),
-      },
-      { spaces: 2 },
-    );
+function createIndex(cssFiles: string[]) {
+  // fs.writeJsonSync(
+  //   path.join(__dirname, '../../css/css-exports.json'),
+  //   {
+  //     modules: cssFiles.filter((file) => !file.includes('global.css')),
+  //     index: prepareFileName('index.css'),
+  //   },
+  //   { spaces: 2 },
+  // );
 
-    console.log({ generatedCssFiles: cssFiles });
-    const cssFilesContent = cssFiles.map((file) => `@import url('${file}');`);
-    fs.writeFileSync(
-      path.join(outputFolder, 'index.css'),
-      cssFilesContent.join('\n'),
-    );
-
-    resolve();
-  });
+  console.log({ generatedCssFiles: cssFiles });
+  const cssFilesContent = cssFiles.map((file) => `@import url('${file}');`);
+  fs.writeFileSync(
+    path.join(outputFolder, 'index.css'),
+    cssFilesContent.join('\n'),
+  );
 }
 
 void createFiles();
