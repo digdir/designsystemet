81c69150d8b537e3dabcd83766dd70923f77e298
Tobias Günther <tobiasg@egde.no>
2023-10-12 13:02:07 +0200

    AB#1182 added svelte-kit to gitignore

--- packages/svelte/.svelte-kit/__package__/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Button from './components/Button/Button.svelte';
-import Modal from './components/Modal/Modal.svelte';
-import Select from './components/Select/Select.svelte';
-import Link from './components/Link/Link.svelte';
-import List from './components/List/List.svelte';
-import Alert from './components/Alert/Alert.svelte';
-import Paragraph from './components/Typography/Paragraph/Paragraph.svelte';
-export { Button, Modal, Select, Link, List, Alert, Paragraph };
