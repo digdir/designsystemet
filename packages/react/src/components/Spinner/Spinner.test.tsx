269f1578b7571ab6eb53c52cb8055734abaa14f9
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-10-27 14:35:24 +0200

    feat(Spinner): Update component and docs (#970)
    Co-authored-by: Øyvind Thune <oyvlei@gmail.com>

--- packages/react/src/components/Spinner/Spinner.test.tsx
+++ packages/react/src/components/Spinner/Spinner.test.tsx
@@ -3,7 +3,16 @@ import { render, screen } from '@testing-library/react';
 
 import { Spinner } from './Spinner';
 
-test('should render with title "loading"', (): void => {
-  render(<Spinner title='Loading' />);
-  expect(screen.getByTitle('Loading')).toBeInTheDocument();
+describe('spinner', (): void => {
+  it('should render with default medium size', (): void => {
+    render(<Spinner title='Loading' />);
+    expect(screen.getByTitle('Loading').parentElement).toHaveStyle({
+      width: '40px',
+    });
+  });
+
+  it('should render with title "loading', (): void => {
+    render(<Spinner title='Loading' />);
+    expect(screen.getByTitle('Loading')).toBeInTheDocument();
+  });
 });
