88343474c0b407864b49568f699c8fb665fa63e5
Øyvind Thune <oyvlei@gmail.com>
2023-10-31 11:05:33 +0100

    chore: fix devsite caching issue and update devsite to next 14 (#998)

--- apps/dev/src/app/api/alias/list/route.ts
+++ apps/dev/src/app/api/alias/list/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const requestHeaders: HeadersInit = new Headers();
   requestHeaders.set('Content-Type', 'application/json');
@@ -7,6 +9,8 @@ export async function GET() {
     'https://api.vercel.com/v4/aliases?teamId=' + process.env.VERCEL_TEAM_ID,
     {
       headers: requestHeaders,
+      next: { revalidate: 0 },
+      cache: 'no-store',
     },
   );
   const data = (await res.json()) as Promise<Response>;
