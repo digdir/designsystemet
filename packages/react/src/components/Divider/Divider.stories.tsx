f17350c85acce9f69851aa67ec640786f6716f5a
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-11-06 15:03:22 +0100

    better story

--- packages/react/src/components/Divider/Divider.stories.tsx
+++ packages/react/src/components/Divider/Divider.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Meta, StoryFn } from '@storybook/react';
 
 import { Divider } from './';
+import { Paragraph } from '../Typography';
 
 type Story = StoryFn<typeof Divider>;
 
@@ -11,13 +12,15 @@ export default {
 } as Meta;
 
 export const Preview: Story = (args) => (
-  <div
-    style={{
-      width: '300px',
-    }}
-  >
+  <>
+    <Paragraph>
+      Divider er brukt for å dele opp innhold i mindre deler.
+    </Paragraph>
     <Divider {...args} />
-  </div>
+    <Paragraph>
+      Den kan også brukes for å skille innhold som er relatert til hverandre.
+    </Paragraph>
+  </>
 );
 Preview.args = {
   color: 'default',
