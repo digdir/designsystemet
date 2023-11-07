c7c3acdb2e2d8520e1fedd9961a4c2789ed847d8
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-11-07 13:51:25 +0100

    fix(Divider): Add so it can recieve `HTMLHRElement` props (#1064)

--- packages/react/src/components/Divider/Divider.tsx
+++ packages/react/src/components/Divider/Divider.tsx
@@ -9,8 +9,17 @@ export type DividerProps = {
    * @default 'default'
    */
   color: 'default' | 'strong' | 'subtle';
-};
+} & React.HTMLAttributes<HTMLHRElement>;
 
-export const Divider = ({ color = 'default' }: DividerProps) => {
-  return <hr className={cn(classes.divider, classes[color + 'Divider'])} />;
+export const Divider = ({ color = 'default', ...rest }: DividerProps) => {
+  return (
+    <hr
+      {...rest}
+      className={cn(
+        classes.divider,
+        classes[color + 'Divider'],
+        rest.className,
+      )}
+    />
+  );
 };
