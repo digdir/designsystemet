cf3e0e84d8487892b0b338972fc7158001d50473
Michael Marszalek <mimarz@gmail.com>
2023-11-10 11:56:22 +0100

    Tweaks

--- packages/react/src/components/form/Search/Search.stories.tsx
+++ packages/react/src/components/form/Search/Search.stories.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 import { MagnifyingGlassIcon } from '@navikt/aksel-icons';
 
-import { Button, Paragraph } from '../..';
+import { Button, Paragraph, Heading } from '../..';
 
 import { Search } from '.';
 
@@ -27,7 +27,6 @@ export const Preview: Story = {
 export const FullWidth: Story = {
   args: {
     label: 'Label',
-    variant: 'primary',
   },
   parameters: {
     layout: 'padded',
@@ -51,6 +50,7 @@ export const Controlled: StoryFn<typeof Search> = () => {
           label='Kontroller meg!'
           value={value}
           onChange={(e) => setValue(e.target.value)}
+          onClear={() => setValue('')}
         />
         <Button onClick={() => setValue('Pizza')}>Jeg vil ha Pizza</Button>
       </div>
@@ -71,5 +71,42 @@ export const OnlyIcon: StoryFn<typeof Search> = () => {
       }
       variant='primary'
     />
+  );
+};
+
+export const Form: StoryFn<typeof Search> = () => {
+  const [value, setValue] = useState<string>();
+  const [submittedValue, setSubmittedValue] = useState<string>();
+
+  return (
+    <>
+      <Heading
+        level={3}
+        size='xxsmall'
+        spacing
+      >
+        Submitted value: {submittedValue}
+      </Heading>
+      <form
+        onSubmit={(e) => {
+          // Prevent navigation from Storybook
+          e.preventDefault();
+          setSubmittedValue(value);
+        }}
+      >
+        <Search
+          label='Search after content'
+          clearButtonLabel='Empty'
+          onChange={(e) => setValue(e.target.value)}
+          searchButtonLabel={
+            <MagnifyingGlassIcon
+              fontSize={'1.5em'}
+              title='Search'
+            />
+          }
+          variant='primary'
+        />
+      </form>
+    </>
   );
 };
