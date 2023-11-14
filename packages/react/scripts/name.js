5b175bf6a333df72953ce40ef527fa28c1c90ad8
Michael Marszalek <mimarz@gmail.com>
2023-11-14 14:43:17 +0100

    wip

--- packages/react/scripts/name.js
+++ packages/react/scripts/name.js
@@ -8,6 +8,7 @@ import path from 'path';
 export function generateScopedName(selector, fileName) {
   const componentName = path
     .basename(fileName)
+    .toLowerCase()
     .replace('.module', '')
     .replace('.css', '');
   return `fds-${componentName}-${selector}`;
