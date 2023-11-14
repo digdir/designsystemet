97c93c4b1ddbe9d6252dea7f735cd1e1941028d2
Michael Marszalek <mimarz@gmail.com>
2023-11-14 08:54:13 +0100

    updated imports

--- packages/react/src/components/ToggleGroup/ToggleGroupItem/ToggleGroupItem.tsx
+++ packages/react/src/components/ToggleGroup/ToggleGroupItem/ToggleGroupItem.tsx
@@ -3,7 +3,7 @@ import React, { forwardRef } from 'react';
 import cn from 'classnames';
 
 import { Button } from '../../Button';
-import { RovingTabindexItem } from '../../../utility-components/RovingTabIndex';
+import { RovingTabindexItem } from '../../../utilities/RovingTabIndex';
 
 import classes from './ToggleGroupItem.module.css';
 import { useToggleGroupItem } from './useToggleGroupitem';
