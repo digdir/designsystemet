d3b625a98c023476e9436ef7eff6ed3309642e21
Michael Marszalek <mimarz@gmail.com>
2023-11-14 14:16:51 +0100

    docs and conform

--- packages/react/scripts/name.js
+++ packages/react/scripts/name.js
@@ -1,5 +1,10 @@
 import path from 'path';
-
+/**
+ *  @url https://github.com/madyankin/postcss-modules#generating-scoped-names
+ * @param {string} selector
+ * @param {string} fileName
+ * @returns
+ */
 export function generateScopedName(selector, fileName) {
   const componentName = path
     .basename(fileName)
