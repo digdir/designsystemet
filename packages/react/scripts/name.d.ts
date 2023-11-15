36b65444350413fa50d54032037b6ac9c9638f27
Michael Marszalek <mimarz@gmail.com>
2023-11-15 11:44:05 +0100

    fix concatenated css files

--- packages/react/scripts/name.d.ts
+++ packages/react/scripts/name.d.ts
@@ -1,4 +1,5 @@
 export declare function generateScopedName(
   name: string,
   filename: string,
+  css: string,
 ): string;
--- packages/react/scripts/name.js
+++ packages/react/scripts/name.js
@@ -1,15 +1,13 @@
 import path from 'path';
+
 /**
  *  @url https://github.com/madyankin/postcss-modules#generating-scoped-names
  * @param {string} selector
- * @param {string} fileName
+ * @param {string} fileNames
  * @returns
  */
-export function generateScopedName(selector, fileName) {
-  const componentName = path
-    .basename(fileName)
-    .toLowerCase()
-    .replace('.module', '')
-    .replace('.css', '');
+export function generateScopedName(selector, fileNames) {
+  const componentName = path.basename(fileNames, '.module.css').toLowerCase();
+
   return `fds-${componentName}-${selector}`;
 }
