f26006dd27959bc8240f094acf97f49f41c17552
stianmorsund <stian.morsund@webstep.no>
2023-11-16 21:25:10 +0100

    testse

--- packages/react/src/components/SkipLink/SkipLink.test.tsx
+++ packages/react/src/components/SkipLink/SkipLink.test.tsx
@@ -1,50 +1,42 @@
-import React, { createRef } from 'react';
-import type { RefObject } from 'react';
+import React from 'react';
 import { render as renderRtl, screen } from '@testing-library/react';
 
 import { SkipLink } from './SkipLink';
 import type { SkipLinkProps } from './SkipLink';
 
-// Test data:
-const href = '';
+const href = '#main-content';
 const children = 'Hopp til hovedinnhold';
 const defaultProps: SkipLinkProps = { href, children };
 
 describe('SkipLink', () => {
   it('Renders an anchor element with the given text and href', () => {
     render();
-    const link = screen.getByRole('link');
-    expect(link).toBeInTheDocument();
-    expect(link).toHaveTextContent(children);
-    expect(link).toHaveAttribute('href', href);
+    const skipLink = screen.getByRole('link');
+    expect(skipLink).toBeInTheDocument();
+    expect(skipLink).toHaveTextContent(children);
+    expect(skipLink).toHaveAttribute('href', href);
   });
 
-  // it('Appends given className to the anchor element', () => {
-  //   const className = 'foo';
-  //   render({ className });
-  //   const link = screen.getByRole('link');
-  //   expect(link).toHaveClass('link');
-  //   expect(link).toHaveClass(className);
-  // });
-
-  // it('Sets the ref on the anchor element if given', () => {
-  //   const ref = createRef<HTMLAnchorElement>();
-  //   render({}, ref);
-  //   expect(ref.current).toBe(screen.getByRole('link'));
-  // });
+  it('Appends given className to the anchor element', () => {
+    const className = 'foo';
+    render({ className });
+    const link = screen.getByRole('link');
+    expect(link).toHaveClass('skiplink');
+    expect(link).toHaveClass(className);
+  });
 });
 
-const render = (
-  props: Partial<SkipLinkProps> = {},
-  ref?: RefObject<HTMLAnchorElement>,
-) => {
+const render = (props: Partial<SkipLinkProps> = {}) => {
   const allProps = { ...defaultProps, ...props };
   return renderRtl(
-    <SkipLink
-      {...allProps}
-      ref={ref}
-    >
-      {allProps.children}
-    </SkipLink>,
+    <>
+      <SkipLink {...allProps}>{allProps.children}</SkipLink>
+      <main
+        id={props.href?.replace('#', '')}
+        tabIndex={-1}
+      >
+        Hovedinnhold
+      </main>
+    </>,
   );
 };
