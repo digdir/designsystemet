494b0d251fa240e1abf6bf33e6ebf3986a13a621
Allinox <arnex04@gmail.com>
2023-11-16 12:02:57 +0100

    feat(ResponsiveTable): 🗑️ Change to Legacy

--- packages/react/src/components/Table/index.ts
+++ packages/react/src/components/Table/index.ts
@@ -2,14 +2,14 @@ export type { ChangeProps, SortDirection, SortProps } from './utils';
 export type {
   ResponsiveTableProps,
   ResponsiveTableConfig,
-} from './ResponsiveTable';
+} from '../legacy/LegacyResponsiveTable/ResponsiveTable';
 export type { TableBodyProps } from './TableBody';
 export type { TableCellProps } from './TableCell';
 export type { TableFooterProps } from './TableFooter';
 export type { TableHeaderProps } from './TableHeader';
 export type { TableProps } from './Table';
 export type { TableRowProps } from './TableRow';
-export { ResponsiveTable } from './ResponsiveTable';
+export { ResponsiveTable } from '../legacy/LegacyResponsiveTable/ResponsiveTable';
 export { Table } from './Table';
 export { TableBody } from './TableBody';
 export { TableCell } from './TableCell';
