167a105b613b662d68b4471ef8f934944e63c74f
stianmorsund <stian.morsund@webstep.no>
2023-11-16 21:24:58 +0100

    harcode as anchor

--- packages/react/src/components/SkipLink/SkipLink.tsx
+++ packages/react/src/components/SkipLink/SkipLink.tsx
@@ -1,28 +1,33 @@
 import type { AnchorHTMLAttributes, ReactNode } from 'react';
-import React, { forwardRef } from 'react';
+import React from 'react';
 import cn from 'classnames';
-
-import type { OverridableComponent } from '../../types/OverridableComponent';
 
 import classes from './SkipLink.module.css';
 
 export type SkipLinkProps = {
-  /** The content to display inside the link. */
+  /** The content to display inside the skiplink. */
   children: ReactNode;
 
-  /** Custom class name for the link. This will be appended to the design system class names. */
+  /** Custom class name for the skiplink. This will be appended to the design system class names. */
   className?: string;
 
+  /** Href of an element in the DOM the skiplink should skip to. E.g #main-content */
   href?: string;
 } & AnchorHTMLAttributes<HTMLAnchorElement>;
 
-export const SkipLink: OverridableComponent<SkipLinkProps, HTMLAnchorElement> =
-  forwardRef(({ as: Component = 'a', children, className, ...rest }, ref) => (
-    <Component
+export const SkipLink = ({
+  href,
+  className,
+  children,
+  ...rest
+}: SkipLinkProps): JSX.Element => {
+  return (
+    <a
+      href={href}
       {...rest}
       className={cn(classes.skiplink, className)}
-      ref={ref}
     >
       {children}
-    </Component>
-  ));
+    </a>
+  );
+};
