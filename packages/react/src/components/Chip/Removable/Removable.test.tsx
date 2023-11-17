14458fef0243d7b1b5bf829567230ab5b1ff4b5d
Allinox <arnex04@gmail.com>
2023-11-17 14:21:33 +0100

    Moved tests from ChipBase to Toggle and Removable

--- packages/react/src/components/Chip/Removable/Removable.test.tsx
+++ packages/react/src/components/Chip/Removable/Removable.test.tsx
@@ -2,47 +2,46 @@ import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Chip, type ToggleChipProps } from '..';
+import { Chip, type RemovableChipProps } from '..';
 
 const user = userEvent.setup();
 
-const TestComponent = ({ children, ...rest }: ToggleChipProps): JSX.Element => {
-  const [selected, setSelected] = useState(false);
+const TestComponent = ({
+  children,
+  ...rest
+}: RemovableChipProps): JSX.Element => {
+  const [removed, setRemoved] = useState(false);
 
   return (
-    <Chip.Toggle
-      {...rest}
-      selected={selected}
-      onClick={() => setSelected(!selected)}
-    >
-      {children}
-    </Chip.Toggle>
+    <>
+      {!removed && (
+        <Chip.Removable
+          {...rest}
+          onClick={() => setRemoved(true)}
+        >
+          {children}
+        </Chip.Removable>
+      )}
+    </>
   );
 };
 
-describe('ToggleChip', () => {
+describe('RemovableChip', () => {
   it('should render button as native element', () => {
-    render(<Chip.Toggle>Norwegian</Chip.Toggle>);
+    render(<Chip.Removable>Norwegian</Chip.Removable>);
 
     expect(screen.getByRole('button', { name: 'Norwegian' }));
   });
 
-  it('should be possible to render as selected', () => {
-    render(<Chip.Toggle selected>Norwegian</Chip.Toggle>);
-
-    expect(screen.getByRole('button', { name: 'Norwegian' })).toHaveAttribute(
-      'aria-pressed',
-      'true',
-    );
-  });
-
-  it('should toggle aria-pressed based on user interaction', async () => {
+  it('should be removed based on user interaction', async () => {
     render(<TestComponent>Norwegian</TestComponent>);
     const chip = screen.getByRole('button', { name: 'Norwegian' });
 
-    expect(chip).toHaveAttribute('aria-pressed', 'false');
+    expect(chip);
     await user.click(chip);
-    expect(chip).toHaveAttribute('aria-pressed', 'true');
+    expect(
+      screen.queryByRole('button', { name: 'Norwegian' }),
+    ).not.toBeInTheDocument();
   });
 
   it('rest props should be supported', () => {
