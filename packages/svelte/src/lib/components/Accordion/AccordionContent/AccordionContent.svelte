a8af0e849e81af97b4dcb3fa5b6b0525d5c5e64b
TommyAndre <tommyab@egde.no>
2023-11-22 09:47:17 +0100

    AB#1311 partial fix for goto bug

--- packages/svelte/src/lib/components/Accordion/AccordionContent/AccordionContent.svelte
+++ packages/svelte/src/lib/components/Accordion/AccordionContent/AccordionContent.svelte
@@ -1,31 +1,36 @@
 <script>
-	import { getContext } from 'svelte';
-	import { slide } from 'svelte/transition';
+  import { getContext } from 'svelte';
+  import { slide } from 'svelte/transition';
 
-	let open = null;
+  let open = null;
 
-	$: {
-		try {
-			open = getContext('accordionItem').open;
-		} catch {
-			console.error('<AccordionContent> has to be used within an <AccordionItem>');
-		}
-	}
+  $: {
+    try {
+      open = getContext('accordionItem').open;
+    } catch {
+      console.error(
+        '<AccordionContent> has to be used within an <AccordionItem>',
+      );
+    }
+  }
 </script>
 
 {#if $open}
-	<div class="content" transition:slide>
-		{#if $$slots.content}
-			<slot name="content" />
-		{/if}
-	</div>
+  <div
+    class="content"
+    transition:slide|local
+  >
+    {#if $$slots.content}
+      <slot name="content" />
+    {/if}
+  </div>
 {/if}
 
 <style>
-	.content {
-		padding: var(--fds-spacing-5, 1rem);
-		background-color: #ffffff50;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
+  .content {
+    padding: var(--fds-spacing-5, 1rem);
+    background-color: #ffffff50;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 </style>
