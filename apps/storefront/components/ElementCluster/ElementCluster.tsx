11d22f93968fc861771ca60077bbacd7c0998249
Øyvind Thune <oyvlei@gmail.com>
2023-11-22 11:47:19 +0100

    Stuff

--- apps/storefront/components/ElementCluster/ElementCluster.tsx
+++ apps/storefront/components/ElementCluster/ElementCluster.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import cn from 'classnames';
 
 import classes from './ElementCluser.module.css';
 
-export const ElementCluster = () => {
+type ElementClusterProps = {
+  className?: string;
+};
+
+export const ElementCluster = ({ className }: ElementClusterProps) => {
   return (
-    <div className={classes.cluster}>
+    <div className={cn(classes.cluster, [className])}>
       <div className={classes.first}></div>
       <div className={classes.second}></div>
       <div className={classes.third}></div>
--- apps/storefront/layouts/FrontpageLayout/FrontpageLayout.tsx
+++ apps/storefront/layouts/FrontpageLayout/FrontpageLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './FrontPageLayout.module.css';
 import { Heading } from '@digdir/design-system-react';
-import { NavigationCard } from '@components';
+import { NavigationCard, Container, ElementCluster } from '@components';
 import { PaletteIcon, WrenchIcon, ComponentIcon } from '@navikt/aksel-icons';
 
 type FrontpageLayoutProps = {
@@ -14,36 +14,40 @@ const FrontpageLayout = ({ content }: FrontpageLayoutProps) => {
     <main id='main'>
       <div className={classes.header}>
         <div className={classes.content}>
-          <div className={classes.test}>
-            <Heading size='large'>
-              Designsystemet hjelper deg å lage gode digitale tjenester
-            </Heading>
-          </div>
-          <div className={classes.cards}>
-            <NavigationCard
-              title='For designere'
-              description='Lær hvordan du kommer i gang med designsystemet som designer.'
-              color='blue'
-              url='/grunnleggende/for-designere/kom-i-gang'
-              icon={<PaletteIcon fontSize={36} />}
-            ></NavigationCard>
+          <ElementCluster className={classes.cluster1} />
+          <ElementCluster className={classes.cluster2} />
+          <Container className={classes.container}>
+            <div className={classes.test}>
+              <Heading size='large'>
+                Designsystemet hjelper deg å lage gode digitale tjenester
+              </Heading>
+            </div>
+            <div className={classes.cards}>
+              <NavigationCard
+                title='For designere'
+                description='Lær hvordan du kommer i gang med designsystemet som designer.'
+                color='blue'
+                url='/grunnleggende/for-designere/kom-i-gang'
+                icon={<PaletteIcon fontSize={36} />}
+              ></NavigationCard>
 
-            <NavigationCard
-              title='For utviklere'
-              description='Lær hvordan du kommer i gang med designsystemet som utvikler.'
-              color='yellow'
-              url='/grunnleggende/for-utviklere/kom-i-gang'
-              icon={<WrenchIcon fontSize={36} />}
-            ></NavigationCard>
+              <NavigationCard
+                title='For utviklere'
+                description='Lær hvordan du kommer i gang med designsystemet som utvikler.'
+                color='yellow'
+                url='/grunnleggende/for-utviklere/kom-i-gang'
+                icon={<WrenchIcon fontSize={36} />}
+              ></NavigationCard>
 
-            <NavigationCard
-              title='Komponenter'
-              description='Se oversikten over UI-komponentene som er laget i react.'
-              color='red'
-              url='https://storybook.designsystemet.no'
-              icon={<ComponentIcon fontSize={34} />}
-            ></NavigationCard>
-          </div>
+              <NavigationCard
+                title='Komponenter'
+                description='Se oversikten over UI-komponentene som er laget i react.'
+                color='red'
+                url='https://storybook.designsystemet.no'
+                icon={<ComponentIcon fontSize={34} />}
+              ></NavigationCard>
+            </div>
+          </Container>
         </div>
       </div>
     </main>
