183dc29dc381980df8f5b92276baa9cf13126202
Michael Marszalek <mimarz@gmail.com>
2023-11-27 13:57:13 +0100

    removed `Card.Group`

--- packages/react/src/components/Card/index.ts
+++ packages/react/src/components/Card/index.ts
@@ -1,9 +1,7 @@
 import type { CardContentProps } from './CardContent';
 import type { CardFooterProps } from './CardFooter';
-import type { CardGroupProps } from './CardGroup';
 import type { CardProps } from './Card';
 import { CardHeader, type CardHeaderProps } from './CardHeader';
-import { CardGroup } from './CardGroup';
 import { CardContent } from './CardContent';
 import { CardFooter } from './CardFooter';
 import { Card as CardParent } from './Card';
@@ -13,7 +11,6 @@ type CardComponent = typeof CardParent & {
   Header: typeof CardHeader;
   Content: typeof CardContent;
   Footer: typeof CardFooter;
-  Group: typeof CardGroup;
   Media: typeof CardMedia;
 };
 
@@ -22,13 +19,11 @@ const Card = CardParent as CardComponent;
 Card.Header = CardHeader;
 Card.Content = CardContent;
 Card.Footer = CardFooter;
-Card.Group = CardGroup;
 Card.Media = CardMedia;
 
 Card.Header.displayName = 'Card.Header';
 Card.Content.displayName = 'Card.Content';
 Card.Footer.displayName = 'Card.Footer';
-Card.Group.displayName = 'Card.Group';
 Card.Media.displayName = 'Card.Media';
 
 export type {
@@ -36,7 +31,6 @@ export type {
   CardHeaderProps,
   CardContentProps,
   CardFooterProps,
-  CardGroupProps,
   CardMediaProps,
 };
-export { Card, CardHeader, CardContent, CardFooter, CardGroup };
+export { Card, CardHeader, CardContent, CardFooter };
