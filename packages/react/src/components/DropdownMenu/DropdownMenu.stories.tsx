e6caa5da80f5b7369e6e36d58e3caa71d0f4e269
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-02 14:28:08 +0100

    add story with portal

--- packages/react/src/components/DropdownMenu/DropdownMenu.stories.tsx
+++ packages/react/src/components/DropdownMenu/DropdownMenu.stories.tsx
@@ -1,26 +1,24 @@
-import type { StoryFn, Meta } from '@storybook/react';
+import type { StoryFn, Meta, StoryObj } from '@storybook/react';
 import React, { useEffect, useRef } from 'react';
 import { LinkIcon } from '@navikt/aksel-icons';
 
 import { Button, Divider } from '../..';
 
+import type { DropdownMenuProps } from '.';
 import { DropdownMenu } from '.';
 
-const decorators = [
-  (Story: StoryFn) => (
-    <div style={{ margin: '20rem', marginTop: '0' }}>
-      <Story />
-    </div>
-  ),
-];
+const marginDecorator = (Story: StoryFn) => (
+  <div style={{ margin: '20rem', marginTop: '0' }}>
+    <Story />
+  </div>
+);
 
 export default {
   title: 'Felles/DropdownMenu',
   component: DropdownMenu,
-  decorators,
 } as Meta;
 
-export const Preview: StoryFn<typeof DropdownMenu> = (args) => {
+const PreviewComponent = (args: DropdownMenuProps) => {
   const buttonRef = useRef<HTMLButtonElement | null>(null);
   const [open, setOpen] = React.useState(false);
 
@@ -71,13 +69,7 @@ export const Preview: StoryFn<typeof DropdownMenu> = (args) => {
   );
 };
 
-Preview.args = {
-  placement: 'bottom-end',
-  size: 'medium',
-  open: false,
-};
-
-export const Icons: StoryFn<typeof DropdownMenu> = (args) => {
+const IconsComponent = (args: DropdownMenuProps) => {
   const buttonRef = useRef<HTMLButtonElement | null>(null);
   const [open, setOpen] = React.useState(false);
 
@@ -122,4 +114,84 @@ export const Icons: StoryFn<typeof DropdownMenu> = (args) => {
       </DropdownMenu>
     </>
   );
+};
+
+const InPortalComponent = (args: DropdownMenuProps) => {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const [open, setOpen] = React.useState(false);
+
+  useEffect(() => {
+    setOpen(args.open || false);
+  }, [args.open]);
+
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => setOpen(!open)}
+        aria-expanded={open}
+        aria-haspopup='menu'
+      >
+        Dropdown
+      </Button>
+      <DropdownMenu
+        {...args}
+        anchorEl={buttonRef.current}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <DropdownMenu.Group heading='Links'>
+          <DropdownMenu.Item
+            as='a'
+            href='https://github.com/digdir/designsystem'
+            target='_blank'
+          >
+            Github
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            as='a'
+            href='https://designsystemet.no'
+            target='_blank'
+          >
+            Designsystemet.no
+          </DropdownMenu.Item>
+        </DropdownMenu.Group>
+        <Divider />
+        <DropdownMenu.Group>
+          <DropdownMenu.Item>Button 1</DropdownMenu.Item>
+          <DropdownMenu.Item>Button 2</DropdownMenu.Item>
+          <DropdownMenu.Item disabled>Disabled</DropdownMenu.Item>
+        </DropdownMenu.Group>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export const Preview: StoryObj<typeof DropdownMenu> = {
+  decorators: [marginDecorator],
+  args: {
+    placement: 'bottom-end',
+    size: 'medium',
+    open: false,
+  },
+  render: PreviewComponent,
+};
+
+export const Icons: StoryObj<typeof DropdownMenu> = {
+  decorators: [marginDecorator],
+  render: IconsComponent,
+};
+
+export const InPortal: StoryObj<typeof DropdownMenu> = {
+  decorators: [
+    (Story: StoryFn) => (
+      <div>
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    portal: true,
+  },
+  render: InPortalComponent,
 };
