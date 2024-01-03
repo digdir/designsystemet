f7df8ff52e55063afa2d53b0b98576c31002c20f
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-03 09:56:15 +0100

    change prop doc

--- packages/react/src/components/Box/Box.tsx
+++ packages/react/src/components/Box/Box.tsx
@@ -37,7 +37,7 @@ export type BoxProps = {
    */
   background?: 'default' | 'subtle';
   /**
-   * If true, will morph onto the child element
+   * Change the default rendered element for the one passed as a child, merging their props and behavior.
    * @default false
    */
   asChild?: boolean;
