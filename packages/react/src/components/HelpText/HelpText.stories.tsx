ee01b25dd20a0d5f16558c2459e89a5d116fecb3
Michael Marszalek <mimarz@gmail.com>
2024-01-11 10:34:18 +0100

    Remove padding on story so `portal` can be tested properly

--- packages/react/src/components/HelpText/HelpText.stories.tsx
+++ packages/react/src/components/HelpText/HelpText.stories.tsx
@@ -1,22 +1,12 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react';
-import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { HelpText } from '.';
 
 type Story = StoryObj<typeof HelpText>;
 
-const decorators = [
-  (Story: StoryFn) => (
-    <div style={{ margin: '5rem' }}>
-      <Story />
-    </div>
-  ),
-];
-
 export default {
   title: 'Felles/HelpText',
   component: HelpText,
-  decorators,
 } as Meta;
 
 export const Preview: Story = {
