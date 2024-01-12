8c2ddadefdf8c64abff32aa32b332adfaf6fcff9
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-12 15:03:05 +0100

    feat(Table): ✨ Make the component (#1364)
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

---
+++ packages/react/src/components/Table/TableRow.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cl from 'clsx';
+
+import classes from './Table.module.css';
+
+export type TableRowProps = React.HTMLAttributes<HTMLTableRowElement>;
+
+export const TableRow = React.forwardRef<HTMLTableRowElement, TableRowProps>(
+  ({ className, children, ...rest }, ref) => {
+    return (
+      <tr
+        className={cl(classes.row, className)}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </tr>
+    );
+  },
+);
