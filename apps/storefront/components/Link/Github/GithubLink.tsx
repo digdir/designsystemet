58f64cf336620e6c73e0a3085905117ae204973c
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-16 13:22:46 +0100

    docs: Add "edit in github" link to pages

---
+++ apps/storefront/components/Link/Github/GithubLink.tsx
@@ -0,0 +1,28 @@
+'use client';
+import type { HTMLAttributes } from 'react';
+import React from 'react';
+import Image from 'next/image';
+import { Link } from '@digdir/design-system-react';
+
+type GithubLinkProps = HTMLAttributes<HTMLAnchorElement>;
+
+export const GithubLink = ({ ...rest }: GithubLinkProps) => {
+  return (
+    <Link
+      href='https://github.com/digdir/designsystemet/tree/main/apps/storefront/pages'
+      target='_blank'
+      rel='noopener noreferrer'
+      {...rest}
+    >
+      <Image
+        height={20}
+        width={20}
+        alt=''
+        src='/img/logos/github-logo.svg'
+      />
+      Rediger denne siden på Github
+    </Link>
+  );
+};
+
+export default GithubLink;
