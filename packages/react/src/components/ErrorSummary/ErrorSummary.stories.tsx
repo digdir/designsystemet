fa750344f7874ba48fa505590f5ec77f7b30f3f0
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-17 12:37:34 +0100

    add story with textfields

--- packages/react/src/components/ErrorSummary/ErrorSummary.stories.tsx
+++ packages/react/src/components/ErrorSummary/ErrorSummary.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { Meta, StoryFn } from '@storybook/react';
 
+import { Textfield } from '../form/Textfield';
+
 import { ErrorSummary } from './';
 
 type Story = StoryFn<typeof ErrorSummary>;
@@ -13,13 +15,47 @@ export default {
 export const Preview: Story = (args) => (
   <>
     <ErrorSummary {...args}>
-      <ErrorSummary.Item href='#'>ErrorSummary.Item</ErrorSummary.Item>
-      <ErrorSummary.Item href='#'>ErrorSummary.Item</ErrorSummary.Item>
-      <ErrorSummary.Item href='#'>ErrorSummary.Item</ErrorSummary.Item>
+      <ErrorSummary.Item href='#'>
+        Fødselsdato kan ikke være etter år 2005
+      </ErrorSummary.Item>
+      <ErrorSummary.Item href='#'>
+        Telefonnummer kan kun inneholde siffer
+      </ErrorSummary.Item>
+      <ErrorSummary.Item href='#'>E-post må være gyldig</ErrorSummary.Item>
     </ErrorSummary>
   </>
 );
 Preview.args = {
   size: 'medium',
-  heading: 'ErrorSummary',
+  heading: 'For å gå videre må du rette opp følgende feil:',
 };
+
+export const WithForm: Story = () => (
+  <div
+    style={{
+      display: 'grid',
+      gap: 'var(--fds-spacing-4)',
+    }}
+  >
+    <Textfield
+      label='Fornavn'
+      id='fornavn'
+      error='Fornavn må være minst 2 tegn'
+    />
+
+    <Textfield
+      label='Telefon'
+      id='telefon'
+      error='Telefonnummer kan kun inneholde siffer'
+    />
+
+    <ErrorSummary heading='For å gå videre må du rette opp følgende feil: '>
+      <ErrorSummary.Item href='#fornavn'>
+        Fornavn må være minst 2 tegn
+      </ErrorSummary.Item>
+      <ErrorSummary.Item href='#telefon'>
+        Telefonnummer kan kun inneholde siffer
+      </ErrorSummary.Item>
+    </ErrorSummary>
+  </div>
+);
