093789045b53915d91c9a638ff0c69416bdd98af
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-17 10:24:59 +0100

    add link and style

--- packages/react/src/components/ErrorSummary/ErrorSummaryItem.tsx
+++ packages/react/src/components/ErrorSummary/ErrorSummaryItem.tsx
@@ -2,9 +2,21 @@ import type { HTMLAttributes } from 'react';
 import React from 'react';
 
 import { ListItem } from '../List';
+import type { LinkProps } from '../Link';
+import { Link } from '../Link';
 
-type ErrorSummaryItemProps = HTMLAttributes<HTMLLIElement>;
+type ErrorSummaryItemProps = {
+  href: LinkProps['href'];
+} & HTMLAttributes<HTMLLIElement>;
 
-export const ErrorSummaryItem = ({ ...rest }: ErrorSummaryItemProps) => {
-  return <ListItem {...rest} />;
+export const ErrorSummaryItem = ({
+  href,
+  children,
+  ...rest
+}: ErrorSummaryItemProps) => {
+  return (
+    <ListItem {...rest}>
+      <Link href={href}>{children}</Link>
+    </ListItem>
+  );
 };
