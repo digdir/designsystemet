1a075fe4307aa864d652a465bed6f9ebce309596
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-18 14:34:05 +0100

    change to use router to navigate to file

--- apps/storefront/components/Link/Github/GithubLink.tsx
+++ apps/storefront/components/Link/Github/GithubLink.tsx
@@ -1,14 +1,19 @@
+'use client';
 import type { HTMLAttributes } from 'react';
 import React from 'react';
 import Image from 'next/image';
 import { Link } from '@digdir/design-system-react';
+import { useRouter } from 'next/router';
 
 type GithubLinkProps = HTMLAttributes<HTMLAnchorElement>;
 
 export const GithubLink = ({ ...rest }: GithubLinkProps) => {
+  const router = useRouter();
+  const href = `https://github.com/digdir/designsystemet/tree/main/apps/storefront/pages${router.asPath}.mdx`;
+
   return (
     <Link
-      href='https://github.com/digdir/designsystemet/tree/main/apps/storefront/pages'
+      href={href}
       target='_blank'
       rel='noopener noreferrer'
       {...rest}
