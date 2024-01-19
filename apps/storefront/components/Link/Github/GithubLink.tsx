8f40117a857521540bdf9570649d4ec97039054a
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-01-19 09:49:00 +0100

    cleanup

--- apps/storefront/components/Link/Github/GithubLink.tsx
+++ apps/storefront/components/Link/Github/GithubLink.tsx
@@ -5,11 +5,9 @@ import Image from 'next/image';
 import { Link } from '@digdir/design-system-react';
 import { useRouter } from 'next/router';
 
-type GithubLinkProps = HTMLAttributes<HTMLAnchorElement>;
-
-export const GithubLink = ({ ...rest }: GithubLinkProps) => {
-  const router = useRouter();
-  const href = `https://github.com/digdir/designsystemet/tree/main/apps/storefront/pages${router.asPath}.mdx`;
+export const GithubLink = ({ ...rest }: HTMLAttributes<HTMLAnchorElement>) => {
+  const { asPath } = useRouter();
+  const href = `https://github.com/digdir/designsystemet/tree/main/apps/storefront/pages${asPath}.mdx`;
 
   return (
     <Link
