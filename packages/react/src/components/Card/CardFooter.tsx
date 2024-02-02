03ec67c5a2fb29549be383165bc4347377d2d8d8
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-02-02 09:56:18 +0100

    update all card components

--- packages/react/src/components/Card/CardFooter.tsx
+++ packages/react/src/components/Card/CardFooter.tsx
@@ -1,18 +1,27 @@
 import React, { forwardRef, type HTMLAttributes } from 'react';
 import cl from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
 
 import classes from './Card.module.css';
 
-export type CardFooterProps = HTMLAttributes<HTMLDivElement>;
+export type CardFooterProps = {
+  /**
+   * Change the default rendered element for the one passed as a child, merging their props and behavior.
+   * @default false
+   */
+  asChild?: boolean;
+} & HTMLAttributes<HTMLDivElement>;
 
 export const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>(
-  ({ children, className, ...rest }, ref) => (
-    <div
-      className={cl(classes.footer, className)}
-      ref={ref}
-      {...rest}
-    >
-      {children}
-    </div>
-  ),
+  ({ asChild, className, ...rest }, ref) => {
+    const Component = asChild ? Slot : 'div';
+
+    return (
+      <Component
+        className={cl(classes.footer, className)}
+        ref={ref}
+        {...rest}
+      />
+    );
+  },
 );
--- packages/react/src/components/Card/CardHeader.tsx
+++ packages/react/src/components/Card/CardHeader.tsx
@@ -1,18 +1,27 @@
 import React, { forwardRef, type HTMLAttributes } from 'react';
 import cl from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
 
 import classes from './Card.module.css';
 
-export type CardHeaderProps = HTMLAttributes<HTMLDivElement>;
+export type CardHeaderProps = {
+  /**
+   * Change the default rendered element for the one passed as a child, merging their props and behavior.
+   * @default false
+   */
+  asChild?: boolean;
+} & HTMLAttributes<HTMLDivElement>;
 
 export const CardHeader = forwardRef<HTMLDivElement, CardHeaderProps>(
-  ({ children, className, ...rest }, ref) => (
-    <div
-      className={cl(classes.header, className)}
-      ref={ref}
-      {...rest}
-    >
-      {children}
-    </div>
-  ),
+  ({ asChild, className, ...rest }, ref) => {
+    const Component = asChild ? Slot : 'div';
+
+    return (
+      <Component
+        className={cl(classes.header, className)}
+        ref={ref}
+        {...rest}
+      />
+    );
+  },
 );
