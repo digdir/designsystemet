08f615e8a7fdca1985b881fa9d376cbca1eb518c
Barsnes <tobias.barsnes@digdir.no>
2024-02-07 09:29:22 +0100

    gets asChild from ListItem

--- packages/react/src/components/ErrorSummary/ErrorSummaryItem.tsx
+++ packages/react/src/components/ErrorSummary/ErrorSummaryItem.tsx
@@ -5,11 +5,6 @@ import { Link } from '../Link';
 
 export type ErrorSummaryItemProps = {
   href: LinkProps['href'];
-  /**
-   * Change the default rendered element for the one passed as a child, merging their props and behavior.
-   * @default false
-   */
-  asChild?: boolean;
 } & ListItemProps;
 
 export const ErrorSummaryItem = ({
