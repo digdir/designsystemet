b6b01a6adbf8522dd4b14be198d388ae1a8bc2c2
Barsnes <tobias.barsnes@digdir.no>
2024-02-22 08:51:09 +0100

    don't export Custom

--- packages/react/src/components/form/Combobox/index.ts
+++ packages/react/src/components/form/Combobox/index.ts
@@ -1,23 +1,19 @@
 import { Combobox as ComboboxRoot } from './Combobox';
 import { ComboboxOption } from './Option/Option';
 import { ComboboxEmpty } from './Empty/Empty';
-import { ComboboxCustom } from './Custom/Custom';
 
 type ComboboxComponent = typeof ComboboxRoot & {
   Option: typeof ComboboxOption;
   Empty: typeof ComboboxEmpty;
-  Custom: typeof ComboboxCustom;
 };
 
 const Combobox = ComboboxRoot as ComboboxComponent;
 
 Combobox.Option = ComboboxOption;
 Combobox.Empty = ComboboxEmpty;
-Combobox.Custom = ComboboxCustom;
 
 Combobox.Option.displayName = 'Combobox.Option';
 Combobox.Empty.displayName = 'Combobox.Empty';
-Combobox.Custom.displayName = 'Combobox.Custom';
 
 export {
   Combobox as Combobox,
