e558e0a01df4b698de280f49c7f59e6e52a0f22c
Michael Marszalek <mimarz@gmail.com>
2024-02-27 14:58:06 +0100

    remove unused utils

--- packages/react/src/utilities/objectUtils.ts
+++ packages/react/src/utilities/objectUtils.ts
@@ -1,20 +1,3 @@
-/**
- * Applies a shallow comparison on the properties of the given objects.
- * @param object1 The first object to compare.
- * @param object2 The second object to compare.
- * @returns {boolean} True if the objects have the same properties and the values of those properties are equal.
- */
-export const objectValuesEqual = <T extends Record<string, unknown>>(
-  object1: T,
-  object2: T,
-) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  if (keys1.length !== keys2.length) return false;
-  for (const key of keys1) if (object1[key] !== object2[key]) return false;
-  return true;
-};
-
 // https://github.com/ramda/ramda/blob/master/source/omit.js
 type UnknownRecord = Record<string | number | symbol, unknown>;
 
