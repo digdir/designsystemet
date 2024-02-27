e0d755d7ee16342b568afb27792266af9258dddc
Barsnes <tobias.barsnes@digdir.no>
2024-02-27 08:50:00 +0100

    update docs

--- packages/react/src/components/List/Lists.tsx
+++ packages/react/src/components/List/Lists.tsx
@@ -29,7 +29,7 @@ export const Unordered = forwardRef<HTMLUListElement, ListUnorderedProps>(
       >
         <Component
           className={cl(classes[size], rest.className)}
-          {...(headingId ? { 'aria-describedby': headingId } : {})}
+          {...(headingId ? { 'aria-labelledby': headingId } : {})}
           ref={ref}
           {...rest}
         />
@@ -61,7 +61,7 @@ export const Ordered = forwardRef<HTMLOListElement, ListOrderedProps>(
       >
         <Component
           className={cl(classes[size], rest.className)}
-          {...(headingId ? { 'aria-describedby': headingId } : {})}
+          {...(headingId ? { 'aria-labelledby': headingId } : {})}
           ref={ref}
           {...rest}
         />
