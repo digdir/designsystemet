05555086e16f9991b55cc7454a4fd862825156d6
Øyvind Thune <oyvlei@gmail.com>
2024-03-21 13:07:05 +0100

    feat(Storefront): Add component overview page (#1661)
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>

---
+++ apps/storefront/layouts/ComponentsLayout/ComponentsLayout.tsx
@@ -0,0 +1,34 @@
+import type * as React from 'react';
+
+import { Container } from '@components';
+
+import { Banner } from '../../components/SubPages/Banner/Banner';
+
+import classes from './ComponentsLayout.module.css';
+
+type ComponentsLayoutProps = {
+  content: React.ReactNode;
+  banner: {
+    title: string;
+    ingress?: string;
+    icon: React.ReactNode;
+  };
+};
+
+const ComponentsLayout = ({ content, banner }: ComponentsLayoutProps) => {
+  return (
+    <main
+      id='main'
+      className={classes.page}
+    >
+      <Banner color='blue'>
+        <Banner.Icon>{banner.icon}</Banner.Icon>
+        <Banner.Heading>{banner.title}</Banner.Heading>
+        {banner.ingress && <Banner.Ingress>{banner.ingress}</Banner.Ingress>}
+      </Banner>
+      <Container className={classes.grid}>{content}</Container>
+    </main>
+  );
+};
+
+export { ComponentsLayout };
