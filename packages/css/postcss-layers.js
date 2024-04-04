165b51998276ba7242e9578a4b58f7b7ee536127
Barsnes <tobias.barsnes@digdir.no>
2024-04-04 10:46:51 +0200

    refactor to be super small

--- packages/css/postcss-layers.js
+++ packages/css/postcss-layers.js
@@ -1,24 +1,14 @@
 const plugin = () => {
-  const filters = [];
-
-  filters.push({
-    layerName: 'fds',
-  });
-
   return {
     postcssPlugin: 'postcss-layers',
     Once(root, { AtRule }) {
-      const layerNames = ['fds'];
-
-      for (const layerName of layerNames) {
-        const layer = new AtRule({
-          name: 'layer',
-          params: layerName,
-          nodes: root.nodes,
-        });
-        root.removeAll();
-        root.append(layer);
-      }
+      const layer = new AtRule({
+        name: 'layer',
+        params: 'fds',
+        nodes: root.nodes,
+      });
+      root.removeAll();
+      root.append(layer);
     },
   };
 };
