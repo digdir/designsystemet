83082e17d00353f8e9ba23ee8ba5169901dfc63b
Barsnes <tobias.barsnes@digdir.no>
2024-04-04 09:57:21 +0200

    require right in file

--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -1,11 +1,9 @@
-const postcssLayers = require('./postcss-layers');
-
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('cssnano')({
       preset: 'default',
     }),
-    postcssLayers,
+    require('./postcss-layers'),
   ],
 };
