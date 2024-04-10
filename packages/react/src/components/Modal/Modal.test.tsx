bc6217ff7e777ebc5dc886fb770ee9afe5686ae5
Barsnes <tobias.barsnes@digdir.no>
2024-04-10 14:14:22 +0200

    chore(Modal): Update tests to use `Trigger`, and open

--- packages/react/src/components/Modal/Modal.test.tsx
+++ packages/react/src/components/Modal/Modal.test.tsx
@@ -1,33 +1,27 @@
-import { useRef } from 'react';
 import { act, render as renderRtl, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Button } from '../Button';
-
 import type { ModalRootProps } from './ModalRoot';
 
+import type { ModalDialogProps } from './';
 import { Modal } from './';
 
 const HEADER_TITLE = 'Modal header title';
+const OPEN_MODAL = 'Open Modal';
+const MODAL_ID = 'modal-id';
 
-const Comp = (args: Partial<ModalRootProps>) => {
-  const modalRef = useRef<HTMLDialogElement>(null);
-
-  const openModal = () => {
-    console.log(modalRef.current);
-    modalRef.current?.showModal();
-  };
-
+const Comp = (args: Partial<ModalDialogProps>) => {
   return (
     <>
-      <Button onClick={openModal}>Open Modal</Button>
-      <Modal
-        {...args}
-        open
-        ref={modalRef}
-      >
-        {args.children}
-      </Modal>
+      <Modal.Root>
+        <Modal.Trigger>{OPEN_MODAL}</Modal.Trigger>
+        <Modal.Dialog
+          id={MODAL_ID}
+          open={args.open}
+        >
+          {args.children}
+        </Modal.Dialog>
+      </Modal.Root>
     </>
   );
 };
@@ -44,8 +38,67 @@ const render = async (props: Partial<ModalRootProps> = {}) => {
 };
 
 describe('Modal', () => {
+  beforeAll(() => {
+    HTMLDialogElement.prototype.show = vi.fn(function mock(
+      this: HTMLDialogElement,
+    ) {
+      this.open = true;
+    });
+
+    HTMLDialogElement.prototype.showModal = vi.fn(function mock(
+      this: HTMLDialogElement,
+    ) {
+      this.open = true;
+    });
+
+    HTMLDialogElement.prototype.close = vi.fn(function mock(
+      this: HTMLDialogElement,
+    ) {
+      this.open = false;
+    });
+  });
+
+  it('should open the modal', async () => {
+    await render({
+      children: (
+        <>
+          <Modal.Header>{HEADER_TITLE}</Modal.Header>
+        </>
+      ),
+    });
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+
+    const button = screen.getByRole('button', { name: OPEN_MODAL });
+    await userEvent.click(button);
+
+    expect(screen.queryByRole('dialog')).toBeInTheDocument();
+  });
+
+  it('should open and close the modal', async () => {
+    await render({
+      children: (
+        <>
+          <Modal.Header>{HEADER_TITLE}</Modal.Header>
+        </>
+      ),
+    });
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+
+    const button = screen.getByRole('button', { name: OPEN_MODAL });
+    await userEvent.click(button);
+
+    expect(screen.queryByRole('dialog')).toBeInTheDocument();
+
+    const closeButton = screen.getByRole('button', { name: /close/i });
+    await userEvent.click(closeButton);
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+  });
+
   it('should render the modal', async () => {
-    await render();
+    await render({ open: true });
     expect(screen.getByRole('dialog')).toBeInTheDocument();
   });
 
@@ -56,12 +109,14 @@ describe('Modal', () => {
           <Modal.Header>{HEADER_TITLE}</Modal.Header>
         </>
       ),
+      open: true,
     });
     expect(screen.getByRole('button', { name: /close/i })).toBeInTheDocument();
   });
 
   it('should not render the close button when closeButton is false', async () => {
     await render({
+      open: true,
       children: (
         <>
           <Modal.Header closeButton={false}>{HEADER_TITLE}</Modal.Header>
@@ -75,6 +130,7 @@ describe('Modal', () => {
 
   it('should render the header title', async () => {
     await render({
+      open: true,
       children: (
         <>
           <Modal.Header>{HEADER_TITLE}</Modal.Header>
@@ -87,6 +143,7 @@ describe('Modal', () => {
   it('should render the header subtitle', async () => {
     const headerSubtitle = 'Modal header subtitle';
     await render({
+      open: true,
       children: (
         <>
           <Modal.Header subtitle={headerSubtitle}>{HEADER_TITLE}</Modal.Header>
@@ -98,7 +155,7 @@ describe('Modal', () => {
 
   it('should render the children', async () => {
     const children = 'Modal children';
-    await render({ children });
+    await render({ children, open: true });
     expect(screen.getByText(children)).toBeInTheDocument();
   });
 });
