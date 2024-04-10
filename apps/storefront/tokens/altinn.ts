0e0b35be735fa35b5b319e1b07e13896d077cc9b
Marianne Røsvik <marianne.rosvik@digdir.no>
2024-04-10 10:39:18 +0200

    feat(Ingress): Change line height (#1793)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/storefront/tokens/altinn.ts
+++ apps/storefront/tokens/altinn.ts
@@ -2526,7 +2526,7 @@ export const typography = [
     path: ['typography', 'heading', 'xxsmall'],
   },
   {
-    value: "400 1.875rem/1.6 'Inter'",
+    value: "400 1.875rem/1.7 'Inter'",
     type: 'typography',
     filePath: '../../design-tokens/Base/Semantic.json',
     isSource: false,
@@ -2534,7 +2534,7 @@ export const typography = [
       value: {
         fontFamily: '{fontFamilies.inter}',
         fontWeight: '{fontWeights.regular}',
-        lineHeight: '{lineHeights.600}',
+        lineHeight: '{lineHeights.700}',
         fontSize: '{font-size.f4}',
       },
       type: 'typography',
@@ -2544,7 +2544,7 @@ export const typography = [
     path: ['typography', 'ingress', 'large'],
   },
   {
-    value: "400 1.5rem/1.6 'Inter'",
+    value: "400 1.5rem/1.7 'Inter'",
     type: 'typography',
     filePath: '../../design-tokens/Base/Semantic.json',
     isSource: false,
@@ -2552,7 +2552,7 @@ export const typography = [
       value: {
         fontFamily: '{fontFamilies.inter}',
         fontWeight: '{fontWeights.regular}',
-        lineHeight: '{lineHeights.600}',
+        lineHeight: '{lineHeights.700}',
         fontSize: '{font-size.f3}',
       },
       type: 'typography',
@@ -2562,7 +2562,7 @@ export const typography = [
     path: ['typography', 'ingress', 'medium'],
   },
   {
-    value: "400 1.3125rem/1.6 'Inter'",
+    value: "400 1.3125rem/1.7 'Inter'",
     type: 'typography',
     filePath: '../../design-tokens/Base/Semantic.json',
     isSource: false,
@@ -2570,7 +2570,7 @@ export const typography = [
       value: {
         fontFamily: '{fontFamilies.inter}',
         fontWeight: '{fontWeights.regular}',
-        lineHeight: '{lineHeights.600}',
+        lineHeight: '{lineHeights.700}',
         fontSize: '{font-size.f2}',
       },
       type: 'typography',
@@ -2580,7 +2580,7 @@ export const typography = [
     path: ['typography', 'ingress', 'small'],
   },
   {
-    value: "400 1.125rem/1.6 'Inter'",
+    value: "400 1.125rem/1.7 'Inter'",
     type: 'typography',
     filePath: '../../design-tokens/Base/Semantic.json',
     isSource: false,
@@ -2588,7 +2588,7 @@ export const typography = [
       value: {
         fontFamily: '{fontFamilies.inter}',
         fontWeight: '{fontWeights.regular}',
-        lineHeight: '{lineHeights.600}',
+        lineHeight: '{lineHeights.700}',
         fontSize: '{font-size.f1}',
       },
       type: 'typography',
