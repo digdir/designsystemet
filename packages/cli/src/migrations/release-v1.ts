36721778379fa9f7bcac3b0177b0e7b350f69350
Michael Marszalek <mimarz@gmail.com>
2024-04-22 12:10:51 +0200

    fixed release-v1 migration for tokens

--- packages/cli/src/migrations/release-v1.ts
+++ packages/cli/src/migrations/release-v1.ts
@@ -1,7 +1,6 @@
-import lightDark from './light-dark.js';
 import tokensv2 from './tokens-v2.js';
 import prefixDs from './prefix-ds.js';
 
 export default (glob?: string) => {
-  return Promise.all([lightDark(glob), tokensv2(glob), prefixDs(glob)]);
+  return Promise.all([tokensv2(glob), prefixDs(glob)]);
 };
