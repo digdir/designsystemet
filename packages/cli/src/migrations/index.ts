a8a3a9488cd4c78f907d02b6af0833fdcff22757
Michael Marszalek <mimarz@gmail.com>
2024-04-22 12:10:51 +0200

    test

--- packages/cli/src/migrations/index.ts
+++ packages/cli/src/migrations/index.ts
@@ -1,9 +1,11 @@
 import lightDark from './light-dark.js';
-import tokensv2 from './tokens-v2.js';
+import tokensV2 from './tokens-v2.js';
 import prefixDs from './prefix-ds.js';
+import releaseV1 from './release-v1.js';
 
 export default {
   'tokens-rename-light-dark': lightDark,
-  'tokens-v2': tokensv2,
+  'tokens-v2': tokensV2,
   'css-prefix-ds': prefixDs,
+  'release-v1': releaseV1,
 };
---
+++ packages/cli/src/migrations/release-v1.ts
@@ -0,0 +1,7 @@
+import lightDark from './light-dark.js';
+import tokensv2 from './tokens-v2.js';
+import prefixDs from './prefix-ds.js';
+
+export default (glob?: string) => {
+  void Promise.all([lightDark(glob), tokensv2(glob), prefixDs(glob)]);
+};
