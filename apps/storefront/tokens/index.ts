2223d15a85a6bd80d9a932c2d6575625d3952774
Michael Marszalek <mimarz@gmail.com>
2024-05-15 09:58:38 +0200

    storefront adjust

--- apps/storefront/tokens/index.ts
+++ apps/storefront/tokens/index.ts
@@ -1,4 +1,10 @@
-export * as digdir from './digdir';
-export * as tilsynet from './tilsynet';
-export * as altinn from './altinn';
-export * as brreg from './brreg';
+// export * as digdir from './digdir';
+// export * as tilsynet from './tilsynet';
+// export * as altinn from './altinn';
+// export * as brreg from './brreg';
+
+
+export * as digdir from './digdir/light';
+export * as tilsynet from './uutilsynet//light';
+export * as altinn from './altinn/light';
+export * as brreg from './theme4//light';
