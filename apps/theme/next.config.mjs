2ffa0d14524c1f3877026fc8aac7f72fef8ea03e
Øyvind Thune <oyvlei@gmail.com>
2024-05-16 10:58:43 +0200

    Stuff

--- apps/theme/next.config.mjs
+++ apps/theme/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
