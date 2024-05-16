f92f5dc982e4856cbfc42a7851fd96d83444536c
Michael Marszalek <mimarz@gmail.com>
2024-05-16 10:30:46 +0200

    added new migration starting

--- packages/cli/src/codemods/css-var-codemod.ts
+++ packages/cli/src/codemods/css-var-codemod.ts
@@ -4,13 +4,13 @@ import type { AcceptedPlugin, Plugin } from 'postcss';
 import postcss from 'postcss';
 import glob from 'fast-glob';
 
-export const cssVarCodemod = (tokens: Record<string, string>) => {
+export const cssVarCodemod = (dictionary: Record<string, string>) => {
   const transformPlugin: Plugin = {
-    postcssPlugin: 'Dark/Light tokens to Strong/Subtle tokens',
-    Declaration(decl) {
-      Object.keys(tokens).forEach((key) => {
-        if (decl.value.includes(key)) {
-          decl.value = decl.value.replace(key, tokens[key]);
+    postcssPlugin: 'Replace',
+    Declaration(declaration) {
+      Object.keys(dictionary).forEach((key) => {
+        if (declaration.value.includes(key)) {
+          declaration.value = declaration.value.replace(key, dictionary[key]);
         }
       });
     },
