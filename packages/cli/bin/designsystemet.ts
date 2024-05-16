f906f46db4cd0db7c25b7b2f83af4381dfd4560e
Michael Marszalek <mimarz@gmail.com>
2024-05-16 10:30:20 +0200

    Added new migration

--- packages/cli/bin/designsystemet.ts
+++ packages/cli/bin/designsystemet.ts
@@ -4,19 +4,12 @@ import { Argument, program } from '@commander-js/extra-typings';
 import migrations from './../src/codemods/migrations.js';
 import { run } from './../src/tokens/build.js';
 
-program
-  .name('Designsystemet')
-  .description('CLI for working with Designsystemet');
+program.name('Designsystemet').description('CLI for working with Designsystemet');
 
 program
   .command('tokens')
   .description('run Designsystemet token builder')
-  .option(
-    '-t, --tokens <string>',
-    'Path to "design-tokens"',
-    '../../design-tokens',
-  )
-
+  .option('-t, --tokens <string>', 'Path to "design-tokens"', '../../design-tokens')
   .option('-p, --preview')
   .action((opts) => {
     const tokens = typeof opts.tokens === 'string' ? opts.tokens : '';
@@ -26,11 +19,7 @@ program
 program
   .command('migrate')
   .description('run a Designsystemet migration')
-  .addArgument(
-    new Argument('<migration>', 'Migration to run').choices(
-      Object.keys(migrations),
-    ),
-  )
+  .addArgument(new Argument('<migration>', 'Migration to run').choices(Object.keys(migrations)))
   .action((migrationKey) => {
     const migration = migrations[migrationKey as keyof typeof migrations];
     if (!migration) {
