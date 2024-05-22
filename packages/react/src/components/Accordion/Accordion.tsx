9f4b27dd2a7b64c39597c86d5a3a0065c87248fc
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-05-22 11:46:58 +0200

    chore: update clsx import (#2049)

--- packages/react/src/components/Accordion/Accordion.tsx
+++ packages/react/src/components/Accordion/Accordion.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode, HTMLAttributes } from 'react';
 import { forwardRef } from 'react';
-import cl from 'clsx';
+import cl from 'clsx/lite';
 
 export type AccordionProps = {
   /** Accordion background color */
