b279b5d0277ffe1392de7dcba825d16f3b743da5
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-05-23 15:03:48 +0200

    add codemod for classnames

---
+++ packages/cli/src/codemods/css-class-rename.ts
@@ -0,0 +1,66 @@
+import fs from 'fs';
+
+import * as R from 'ramda';
+import type { AcceptedPlugin, Plugin } from 'postcss';
+import postcss from 'postcss';
+import glob from 'fast-glob';
+
+type CSSCodemodProps = {
+  dictionary: {
+    from: string;
+    to: string;
+  };
+  globPath?: string;
+};
+
+export const cssNameCodemod = ({
+  dictionary,
+  globPath = './**/*.css',
+}: CSSCodemodProps) => {
+  const transformPlugin: Plugin = {
+    postcssPlugin: 'Renames CSS classes',
+    Rule(decl) {
+      if (!decl.selector) return;
+      if (!decl.selector.includes('.fds-')) return;
+
+      const newSelector = decl.selector.replace(
+        new RegExp(dictionary.from, 'g'),
+        dictionary.to,
+      );
+
+      decl.selector = newSelector;
+    },
+  };
+
+  const plugins: AcceptedPlugin[] = [transformPlugin];
+
+  const processor = postcss(plugins);
+
+  const transform = async () => {
+    const files = await glob(globPath, { ignore: ['node_modules/**'] });
+
+    console.log({ files });
+
+    const filePromises = files.map(async (file) => {
+      const contents = fs.readFileSync(file).toString();
+      const result = await processor.process(contents, { from: undefined });
+
+      fs.writeFileSync(file, result.css);
+    });
+
+    await Promise.all(filePromises);
+  };
+
+  // Run the transform.
+  void transform();
+};
+
+console.log('running cssNameCodemod');
+
+void cssNameCodemod({
+  dictionary: {
+    from: '.fds-',
+    to: '.ds-',
+  },
+  globPath: './**/*.css',
+});
