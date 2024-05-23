7351ffcceb1b3aa5a4f5507e2aec54390ba42a04
Persijn kwekkeboom <3340349+poi33@users.noreply.github.com>
2024-05-23 11:37:03 +0200

    chore(storybook): 🪧 Add fieldset preview (#2047)
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

--- packages/react/src/components/form/Fieldset/Fieldset.stories.tsx
+++ packages/react/src/components/form/Fieldset/Fieldset.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryFn } from '@storybook/react';
 
+import { Textfield } from '../Textfield';
+import { Textarea } from '../Textarea';
+
 import { Fieldset } from '.';
 
 type Story = StoryFn<typeof Fieldset>;
@@ -9,4 +12,21 @@ export default {
   component: Fieldset,
 } as Meta;
 
-export const Preview: Story = (args) => <Fieldset {...args}></Fieldset>;
+export const Preview: Story = (args) => (
+  <form>
+    <Fieldset {...args}>
+      <Textfield label='Kort beskrivelse'></Textfield>
+      <Textarea label='Lang beskrivelse'></Textarea>
+    </Fieldset>
+  </form>
+);
+
+Preview.args = {
+  description:
+    'Gi en kort beskrivelse i begge feltene',
+  disabled: false,
+  error: '',
+  legend: 'Skriv inn dine svar',
+  readOnly: false,
+  hideLegend: false,
+};
