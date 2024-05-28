591caa64cad4f4a0898cea3b2bba1770ed1aaada
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-05-28 19:19:14 +0200

    colors on some components

--- packages/react/src/components/Spinner/Spinner.stories.tsx
+++ packages/react/src/components/Spinner/Spinner.stories.tsx
@@ -45,17 +45,6 @@ export const Variants: Story = () => (
       variant='interaction'
       size='xl'
     />
-    <div
-      style={{
-        background: 'var(--ds-semantic-surface-neutral-inverted)',
-      }}
-    >
-      <Spinner
-        title='Henter kaffi'
-        variant='inverted'
-        size='xl'
-      />
-    </div>
   </>
 );
 
