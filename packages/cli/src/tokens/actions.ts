5b31f3cbe352a8de0ff0f1ddde00b818ed6bade4
Michael Marszalek <mimarz@gmail.com>
2024-06-05 09:57:38 +0200

    temp fix for sorted entry files

--- packages/cli/src/tokens/actions.ts
+++ packages/cli/src/tokens/actions.ts
@@ -1,14 +1,18 @@
 import fs from 'fs-extra';
 import glob from 'fast-glob';
 import type { Action } from 'style-dictionary/types';
+import chalk from 'chalk';
+import * as R from 'ramda';
 
 export const makeEntryFile: Action = {
   name: 'make_entryfile',
   do: async function (dictionary, config) {
-    console.log('Creating entry file');
+    console.log(chalk.green('Creating entry file'));
     const { outPath, folderName } = config;
     const files = await glob(`**/*`, { cwd: config.buildPath });
-    const content = files.map((file) => `@import url('./${folderName}/${file}');`).join('\n');
+    const content = R.reverse(R.sortBy(R.includes('light'), files))
+      .map((file) => `@import url('./${folderName}/${file}');`)
+      .join('\n');
     await fs.writeFile(`${outPath}/${folderName}.css`, content);
   },
   undo: async function noOp() {},
