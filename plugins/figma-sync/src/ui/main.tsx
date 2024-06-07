de83dc63a68097d27c1ca9ec6d68efc3a0a5a0a1
Øyvind Thune <oyvlei@gmail.com>
2024-06-07 09:39:01 +0200

    Changed figma plugin to Vite

---
+++ plugins/figma-sync/src/ui/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MemoryRouter } from 'react-router-dom';
+
+async function bootstrap() {
+  const App = (await import('./app')).default;
+
+  const rootElement = document.getElementById('root') as HTMLElement;
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </React.StrictMode>,
+  );
+}
+
+void bootstrap();
---
+++ plugins/figma-sync/src/ui/pages/pageThree/PageThree.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+
+function PageThree() {
+  return <div className='content'>page three</div>;
+}
+
+export default PageThree;
