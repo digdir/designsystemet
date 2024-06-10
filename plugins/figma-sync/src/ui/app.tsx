25ffe3fa80efd8bceb53e5ebf01b47e7744f4cc0
Øyvind Thune <oyvlei@gmail.com>
2024-06-10 15:15:53 +0200

    Updated figma plugin with error handling and fixes

--- plugins/figma-sync/src/ui/app.tsx
+++ plugins/figma-sync/src/ui/app.tsx
@@ -2,16 +2,17 @@ import '@digdir/designsystemet-theme';
 import '@digdir/designsystemet-css';
 
 import './App.css';
-import { NavLink, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import PageOne from './pages/PageOne/PageOne';
 import PageTwo from './pages/PageTwo/PageTwo';
 import PageThree from './pages/pageThree/PageThree';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
-      <div className='links'>
+      {/* <div className='links'>
         <NavLink
           className={({ isActive }) => (isActive ? 'link active' : 'link')}
           to='/'
@@ -30,22 +31,25 @@ function App() {
         >
           Oppdater UI Kit
         </NavLink>
-      </div>
+      </div> */}
 
-      <Routes>
-        <Route
-          path='/'
-          element={<PageOne />}
-        />
-        <Route
-          path='/pageTwo'
-          element={<PageTwo />}
-        />
-        <Route
-          path='/pageThree'
-          element={<PageThree />}
-        />
-      </Routes>
+      <div className='content'>
+        <Routes>
+          <Route
+            path='/'
+            element={<PageOne />}
+          />
+          <Route
+            path='/pageTwo'
+            element={<PageTwo />}
+          />
+          <Route
+            path='/pageThree'
+            element={<PageThree />}
+          />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
