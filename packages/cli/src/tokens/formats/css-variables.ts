1263fa7ac05ab6e78055001c0cc92a50947efc81
Michael Marszalek <mimarz@gmail.com>
2024-06-11 22:36:35 +0200

    updated tokens builder

--- packages/cli/src/tokens/formats/css-variables.ts
+++ packages/cli/src/tokens/formats/css-variables.ts
@@ -1,12 +1,16 @@
+import * as R from 'ramda';
+import type { TransformedToken } from 'style-dictionary';
 import type { Format } from 'style-dictionary/types';
-import { fileHeader, createPropertyFormatter } from 'style-dictionary/utils';
+import { fileHeader, createPropertyFormatter, usesReferences } from 'style-dictionary/utils';
+
+const calculatedVariable = R.pipe(R.split(/:(.*?);/g), (split) => `${split[0]}: calc(${R.trim(split[1])});`);
 
 export const cssVariables: Format = {
   name: 'ds/css-variables',
   format: async function ({ dictionary, file, options, platform }) {
     const { allTokens } = dictionary;
-    const { outputReferences } = options;
-    const { selector } = platform;
+    const { outputReferences, usesDtcg } = options;
+    const { selector, baseTypes } = platform;
 
     const header = await fileHeader({ file });
 
@@ -16,7 +20,24 @@ export const cssVariables: Format = {
       format: 'css',
     });
 
-    const formattedVariables = allTokens.map(format);
+    const formatTokens = R.map((token: TransformedToken) => {
+      const originalValue = (usesDtcg ? token.original.$value : token.original.value) as string;
+      const type = (usesDtcg ? token.$type : token.type) || '';
+
+      if (
+        usesReferences(originalValue) &&
+        typeof outputReferences === 'function' &&
+        outputReferences?.(token, { dictionary })
+      ) {
+        if ((baseTypes as string[]).includes(type)) {
+          return calculatedVariable(format(token));
+        }
+      }
+
+      return format(token);
+    });
+
+    const formattedVariables = formatTokens(allTokens);
 
     return header + `${selector} {\n${formattedVariables.join('\n')}\n}\n`;
   },
