af72c7dd5502c4dc190eec0fb8e8ebbdc708d1ab
Barsnes <tobias.barsnes@digdir.no>
2024-06-11 08:45:14 +0200

    move accordion files

--- packages/react/src/components/Accordion/AccordionHeader.test.tsx
+++ packages/react/src/components/Accordion/AccordionHeader.test.tsx
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Accordion } from '..';
-
-import type { AccordionHeaderProps } from './AccordionHeader';
+import type { AccordionHeaderProps } from '.';
+import { Accordion } from '.';
 
 const user = userEvent.setup();
 
