bbc79f96452c83e66b1d6cedb829dd70f636b402
barsnes <tobias.barsnes@digdir.no>
2024-06-25 10:45:02 +0200

    add `useSynchronizedAnimation` to storybook, and export

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,4 +1,4 @@
 export { useMediaQuery } from './useMediaQuery';
 export { usePrevious } from './usePrevious';
-export { useSynchronizedAnimation } from './useSynchronizedAnimation';
+export { useSynchronizedAnimation } from './useSynchronizedAnimation/useSynchronizedAnimation';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
