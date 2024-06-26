405abf4a9226c036770a0bb32c61455465d90658
barsnes <tobias.barsnes@digdir.no>
2024-06-26 14:39:39 +0200

    export `RovingTabindex`, use it in themebuilder

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,9 @@
 export * from './components';
 export { useSynchronizedAnimation, useIsomorphicLayoutEffect } from './hooks';
+export {
+  RovingTabindexRoot,
+  useRovingTabindex,
+  RovingTabindexItem,
+  getNextFocusableValue,
+  getPrevFocusableValue,
+} from './utilities';
