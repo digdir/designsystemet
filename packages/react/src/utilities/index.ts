2628238740b25aaa76c2dc501b750063e62d8025
barsnes <tobias.barsnes@digdir.no>
2024-06-26 14:39:39 +0200

    export `RovingTabindex`, use it in themebuilder

--- packages/react/src/utilities/index.ts
+++ packages/react/src/utilities/index.ts
@@ -1,1 +1,8 @@
 export * from './objectUtils';
+export {
+  RovingTabindexRoot,
+  useRovingTabindex,
+  RovingTabindexItem,
+  getNextFocusableValue,
+  getPrevFocusableValue,
+} from './RovingTabIndex';
