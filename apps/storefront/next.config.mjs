5a3232856e6de1e2b3cb9ae04edd2b6abff46925
barsnes <tobias.barsnes@digdir.no>
2024-07-01 13:13:08 +0200

    fix tables on storefront

--- apps/storefront/next.config.mjs
+++ apps/storefront/next.config.mjs
@@ -2,51 +2,10 @@ import createMDX from '@next/mdx';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 import { s } from 'hastscript';
 
 const nextConfig = {
-  /* webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            providerImportSource: '@mdx-js/react',
-            remarkPlugins: [remarkFrontmatter, remarkGfm],
-            rehypePlugins: [
-              rehypeHighlight,
-              rehypeSlug,
-              [
-                rehypeAutolinkHeadings,
-                {
-                  content() {
-                    return [
-                      s(
-                        'svg',
-                        {
-                          xmlns: 'http://www.w3.org/2000/svg',
-                          viewbox: '0 0 16 16',
-                        },
-                        [
-                          s('path', {
-                            d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z',
-                          }),
-                        ],
-                      ),
-                    ];
-                  },
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    });
-
-    return config;
-  }, */
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'tsx', 'ts'],
   eslint: {
@@ -63,7 +22,7 @@ const nextConfig = {
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [],
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [
       rehypeHighlight,
       rehypeSlug,
