547dff6df7c3ee73baaf558e0c06e7e966cbed5d
barsnes <tobias.barsnes@digdir.no>
2024-07-04 12:40:56 +0200

    fix contrast in storefront

--- apps/storefront/components/Tokens/TokenColor/TokenColor.tsx
+++ apps/storefront/components/Tokens/TokenColor/TokenColor.tsx
@@ -2,6 +2,7 @@ import type { TransformedToken } from 'style-dictionary';
 import { ColorModal } from '@digdir/components';
 import { useRef } from 'react';
 import cl from 'clsx/lite';
+import { getColorNameFromNumber } from '@digdir/designsystemet/color';
 
 import type { ColorNumber } from '../../../../../packages/cli/src/colors/types';
 
@@ -11,25 +12,33 @@ interface TokenColorProps {
   token: TransformedToken;
 }
 
+/* The original.value is something like "{global.yellow.1}", and we need to get the weight between the last . and } */
+function getColorWeight(value: string): ColorNumber | undefined {
+  const firstSplit = value.split('.').pop()?.replace('}', '');
+
+  if (firstSplit?.includes('contrast-')) {
+    if (firstSplit.includes('contrast-1')) {
+      return 14;
+    }
+    return 15;
+  }
+
+  const parsed = parseInt(firstSplit as string);
+
+  return isNaN(parsed) ? undefined : (parsed as ColorNumber);
+}
+
 const TokenColor = ({ value, token }: TokenColorProps) => {
   const colorModalRef = useRef<HTMLDialogElement>(null);
 
-  /* The original.value is something like "{global.yellow.1}", and we need to get the weight between the last . and } */
-  const selectedColor =
-    /* @ts-expect-error #2532 */
-    (token.original.value as string)
-      .split('.')
-      .pop()
-      .replace('}', '')
-      .replace('contrast-', '') || '';
-
-  const Element = parseInt(selectedColor) ? 'button' : 'div';
+  const weight = getColorWeight(token.original.value as string);
+  const Element = weight ? 'button' : 'div';
 
   return (
     <>
-      {parseInt(selectedColor) ? (
+      {weight ? (
         <ColorModal
-          weight={parseInt(selectedColor) as ColorNumber}
+          weight={weight}
           hex={value}
           namespace={token.path[1]}
           colorModalRef={colorModalRef}
@@ -39,9 +48,8 @@ const TokenColor = ({ value, token }: TokenColorProps) => {
         <Element
           style={{ backgroundColor: value }}
           className={cl(classes.color, 'ds-focus')}
-          onClick={() =>
-            parseInt(selectedColor) && colorModalRef.current?.showModal()
-          }
+          onClick={() => weight && colorModalRef.current?.showModal()}
+          name={`Se mer om ${token.path[1]} ${typeof weight === 'number' && getColorNameFromNumber(weight)}`}
         ></Element>
       </div>
     </>
