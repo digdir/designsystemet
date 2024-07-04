9d0232ea7b629619349c132533f151756a38ab36
barsnes <tobias.barsnes@digdir.no>
2024-07-04 12:06:46 +0200

    remove consoles

--- apps/storefront/components/Tokens/TokenColor/TokenColor.tsx
+++ apps/storefront/components/Tokens/TokenColor/TokenColor.tsx
@@ -3,6 +3,8 @@ import { ColorModal } from '@digdir/components';
 import { useRef } from 'react';
 import cl from 'clsx/lite';
 
+import type { ColorNumber } from '../../../../../packages/cli/src/colors/types';
+
 import classes from './TokenColor.module.css';
 interface TokenColorProps {
   value: string;
@@ -13,22 +15,21 @@ const TokenColor = ({ value, token }: TokenColorProps) => {
   const colorModalRef = useRef<HTMLDialogElement>(null);
 
   /* The original.value is something like "{global.yellow.1}", and we need to get the weight between the last . and } */
-  /* @ts-expect-error #2532 */
-  const selectedColor = (token.original.value as string)
-    .split('.')
-    .pop()
-    .replace('}', '')
-    .replace('contrast-', '') as string | undefined;
+  const selectedColor =
+    /* @ts-expect-error #2532 */
+    (token.original.value as string)
+      .split('.')
+      .pop()
+      .replace('}', '')
+      .replace('contrast-', '') || '';
 
   const Element = parseInt(selectedColor) ? 'button' : 'div';
-  console.log(token);
-  console.log(token.original.value, selectedColor, parseInt(selectedColor));
 
   return (
     <>
       {parseInt(selectedColor) ? (
         <ColorModal
-          weight={parseInt(selectedColor)}
+          weight={parseInt(selectedColor) as ColorNumber}
           hex={value}
           namespace={token.path[1]}
           colorModalRef={colorModalRef}
