2702d773efd24dfb7f27bd7c5892564ddd1e33cd
Michael Marszalek <mimarz@gmail.com>
2024-07-29 13:32:06 +0200

    cleaned up storybook app

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: { 'process.env': {} },
   resolve: {
     alias: {
       '@doc-components': path.resolve(
