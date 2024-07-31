2855d1d61f46600c398fd750fe2fbc66153063c0
Michael Marszalek <mimarz@gmail.com>
2024-07-31 14:30:45 +0200

    more checks tweaks

--- packages/react/src/components/Card/Card.stories.tsx
+++ packages/react/src/components/Card/Card.stories.tsx
@@ -59,7 +59,7 @@ export const LinkCard: Story = (args) => (
         rel='noopener noreferrer'
       >
         <Card.Media>
-          <img src={cat5} />
+          <img src={cat5} alt='' />
         </Card.Media>
         <Card.Header>
           <Heading size='sm' level={2}>
