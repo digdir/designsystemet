f044266a82cc4fe2b5203eb99e4a0e92885598c8
Michael Marszalek <mimarz@gmail.com>
2024-07-31 12:40:45 +0200

    remove unused docs-components Table

--- apps/storybook/docs-components/index.ts
+++ apps/storybook/docs-components/index.ts
@@ -1,9 +1,3 @@
-export { Table } from './Table/Table';
-export {
-  TableCellColor,
-  TableCellTag,
-  TableCellSpace,
-} from './Table/TableCells/TableCells';
 export { Stack } from './Stack/Stack';
 export { Do, Dont } from './DoAndDont/DoAndDont';
 export { LinkHeading } from './LinkHeading/LinkHeading';
