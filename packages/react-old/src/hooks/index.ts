3a48cd80478eda30857289ed76a8eca2c2bfd949
Michael Marszalek <mimarz@gmail.com>
2024-08-05 11:40:24 +0200

    chore: :fire: Remove react-old (`@digdir/design-system-react`) (#2200)
    Remove deprecated package as its interfering with changesets, upgrades
    and builds now.
    
    Made a separate branch
    [react-old](https://github.com/digdir/designsystemet/tree/react-old) to
    keep archive.

--- packages/react-old/src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-export { useEventListener } from './useEventListener';
-export { useKeyboardEventListener } from './useKeyboardEventListener';
-export { useMediaQuery } from './useMediaQuery';
-export { usePrevious } from './usePrevious';
-export { useUpdate } from './useUpdate';
-export { useSynchronizedAnimation } from './useSynchronizedAnimation';
-export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
