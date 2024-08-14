3a3617b1fcfe9b915b48a28420dfdfbba923c14d
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-08-14 09:02:38 +0200

    feat(Badge): ✨ New component (#2220)
    resolves #2211

---
+++ packages/react/src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,180 @@
+import type { Meta, StoryFn } from '@storybook/react';
+
+import {
+  ChatIcon,
+  EnvelopeClosedFillIcon,
+  HeartFillIcon,
+  InboxIcon,
+  PencilIcon,
+  VideoFillIcon,
+  VideoIcon,
+} from '@navikt/aksel-icons';
+import { Button } from '../Button';
+import { Tabs } from '../Tabs';
+import { Badge } from './Badge';
+
+type Story = StoryFn<typeof Badge>;
+
+const meta: Meta<typeof Badge> = {
+  title: 'Komponenter/Badge',
+  component: Badge,
+};
+
+export default meta;
+
+export const Preview: Story = (args) => <Badge {...args}></Badge>;
+
+Preview.args = {
+  size: 'md',
+  count: 10,
+  maxCount: 9,
+  color: 'accent',
+};
+
+export const Floating: Story = (args) => (
+  <div
+    style={{
+      display: 'flex',
+      gap: 'var(--ds-spacing-6)',
+    }}
+  >
+    <Badge color='accent' size='md' placement='top-right'>
+      <EnvelopeClosedFillIcon fontSize='2rem' />
+    </Badge>
+    <Badge color='accent' size='md' placement='top-left'>
+      <EnvelopeClosedFillIcon fontSize='2rem' />
+    </Badge>
+    <Badge color='accent' size='md' placement='bottom-right'>
+      <EnvelopeClosedFillIcon fontSize='2rem' />
+    </Badge>
+    <Badge color='accent' size='md' placement='bottom-left'>
+      <EnvelopeClosedFillIcon fontSize='2rem' />
+    </Badge>
+    <Badge color='accent' size='md' placement='top-right' overlap='circle'>
+      <div
+        style={{
+          width: '2rem',
+          height: '2rem',
+          borderRadius: '50%',
+          backgroundColor: 'var(--ds-color-brand2-base-default)',
+        }}
+      />
+    </Badge>
+    <Badge color='accent' size='md' placement='top-left' overlap='circle'>
+      <div
+        style={{
+          width: '2rem',
+          height: '2rem',
+          borderRadius: '50%',
+          backgroundColor: 'var(--ds-color-brand2-base-default)',
+        }}
+      />
+    </Badge>
+    <Badge color='accent' size='md' placement='bottom-right' overlap='circle'>
+      <div
+        style={{
+          width: '2rem',
+          height: '2rem',
+          borderRadius: '50%',
+          backgroundColor: 'var(--ds-color-brand2-base-default)',
+        }}
+      />
+    </Badge>
+    <Badge color='accent' size='md' placement='bottom-left' overlap='circle'>
+      <div
+        style={{
+          width: '2rem',
+          height: '2rem',
+          borderRadius: '50%',
+          backgroundColor: 'var(--ds-color-brand2-base-default)',
+        }}
+      />
+    </Badge>
+  </div>
+);
+
+export const CustomPlacement: Story = (args) => (
+  <div
+    style={{
+      display: 'flex',
+      gap: 'var(--ds-spacing-6)',
+    }}
+  >
+    <Badge
+      color='accent'
+      size='md'
+      placement='top-right'
+      style={{
+        top: '16%',
+        right: '10%',
+      }}
+    >
+      <EnvelopeClosedFillIcon fontSize='2rem' />
+    </Badge>
+  </div>
+);
+
+export const Status: Story = (args) => (
+  <div
+    style={{
+      display: 'flex',
+      gap: 'var(--ds-spacing-4)',
+    }}
+  >
+    <Badge color='danger' size='sm'>
+      <VideoFillIcon fontSize='2rem' />
+    </Badge>
+    <Badge color='danger' size='md'>
+      <VideoFillIcon fontSize='2rem' />
+    </Badge>
+    <Badge color='danger' size='lg'>
+      <VideoFillIcon fontSize='2rem' />
+    </Badge>
+  </div>
+);
+
+export const InTabs: Story = (args) => (
+  <Tabs.Root defaultValue='value1' size='md'>
+    <Tabs.List>
+      <Tabs.Tab value='value1'>
+        <HeartFillIcon fontSize='1.75rem' />
+        Favoritter
+        <Badge count={64} maxCount={10} color='neutral' />
+      </Tabs.Tab>
+      <Tabs.Tab value='value2'>Tab 2</Tabs.Tab>
+      <Tabs.Tab value='value3'>
+        <PencilIcon fontSize='1.75rem' />
+        Nylige
+        <Badge count={2} color='neutral' />
+      </Tabs.Tab>
+    </Tabs.List>
+    <Tabs.Content value='value1'>content 1</Tabs.Content>
+    <Tabs.Content value='value2'>content 2</Tabs.Content>
+    <Tabs.Content value='value3'>content 3</Tabs.Content>
+  </Tabs.Root>
+);
+
+export const InButton: Story = (args) => (
+  <div
+    style={{
+      display: 'flex',
+      gap: 'var(--ds-spacing-4)',
+    }}
+  >
+    <Button icon variant='tertiary'>
+      <Badge color='danger' count={1000} maxCount={99} size='sm'>
+        <InboxIcon fontSize='1.75rem' />
+      </Badge>
+    </Button>
+    <Button icon variant='tertiary'>
+      <Badge color='danger' count={10} size='sm'>
+        <ChatIcon fontSize='1.75rem' />
+      </Badge>
+    </Button>
+    <Button icon variant='tertiary'>
+      <Badge color='danger' size='sm'>
+        <VideoIcon fontSize='1.75rem' />
+      </Badge>
+    </Button>
+  </div>
+);
