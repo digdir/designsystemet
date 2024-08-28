d009c7f5902ff68291aaee433406096fee8ccaa8
barsnes <tobias.barsnes@digdir.no>
2024-08-28 14:26:15 +0200

    focus, color

--- apps/storefront/components/VersionBanner/VersionBanner.tsx
+++ apps/storefront/components/VersionBanner/VersionBanner.tsx
@@ -1,9 +1,13 @@
+import cl from 'clsx/lite';
 import React from 'react';
 import classes from './VersionBanner.module.css';
 
 function VersionBanner() {
   return (
-    <a href='https://v0.designsystemet.no' className={classes.banner}>
+    <a
+      href='https://v0.designsystemet.no'
+      className={cl(classes.banner, 'ds-focus')}
+    >
       Trykk her for dokumentasjon av v0.63.
     </a>
   );
