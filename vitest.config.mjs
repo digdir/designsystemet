1caae9282971353d911cbef9bab03564f8f281a9
Eirik Backer <eirik.backer@gmail.com>
2024-08-30 09:44:56 +0200

    chore: add test coverage (#2317)
    - Add support for test coverage so we easily can detect code that never
    have run during our rests
    - Using vitest with v8
    - Open the generated file `coverage/index.html` in a browser to explore
    
    ---------
    
    Co-authored-by: barsnes <tobias.barsnes@digdir.no>

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -6,6 +6,17 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./test/vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['html', 'json-summary', 'json'],
+      reportOnFailure: true,
+      include: ['packages/react/src/**/*.{ts,tsx}'],
+      exclude: [
+        '**/*.{stories,e2e,docs,test}.{ts,tsx}',
+        '**/index.ts',
+        '**/types/**',
+      ],
+    },
     css: {
       modules: {
         classNameStrategy: 'non-scoped',
