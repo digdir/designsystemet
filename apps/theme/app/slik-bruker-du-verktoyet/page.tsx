344bb8d7a1b62ee6d63b30a6cc0b3d560b2588e2
Øyvind Thune <oyvlei@gmail.com>
2024-08-30 12:37:31 +0200

    Stuff

--- apps/theme/app/slik-bruker-du-verktoyet/page.tsx
+++ apps/theme/app/slik-bruker-du-verktoyet/page.tsx
@@ -36,46 +36,102 @@ export default function Home() {
             </Alert>
 
             <Heading level={2} size='md'>
-              Valg av farger og feilmeldinger
+              Valg av farger
             </Heading>
             <Paragraph>
-              Temabyggeren lar deg velge 5 farger: Accent, Neutral, Brand1,
-              Brand2 og Brand3. Disse fargekodene vil bli til Base Default i
-              fargeskalaene. Det er derfor enkelte ting man må passe på når man
-              velger en farge for at det skal fungere optimalt i systemet.
+              Når du skal velge en farge i verktøyet er det enkelte ting det er
+              viktig å være klar over. Det gjelder spesielt brukten av{' '}
+              <code>Base</code> fargene.
             </Paragraph>
 
             <Heading level={3} size='sm'>
-              Den valgte fargen har mindre enn 3:1 kontrast mot bakgrunnsfargene
+              Accent fargen
             </Heading>
+            <Paragraph>
+              Accent fargen blir brukt som farge for å lyse opp viktige
+              elementer på en side. Det er derfor viktig at denne fargen er
+              sterk nok for å skille seg tydelig ut fra bakgrunnen.
+              Designsystemet anbefaler at fargen man velger har minst 4.5:1
+              kontrast mot bakgrunnsfargene for å både støtte WCAG sitt{' '}
+              <a href='https://www.uutilsynet.no/wcag-standarden/1411-kontrast-ikke-tekstlig-innhold-niva-aa/145'>
+                1.4.11 Kontrast for ikke-tekstlig innhold{' '}
+              </a>{' '}
+              krav og gjøre kontrastfargen som blir generert hvit.
+            </Paragraph>
+
+            <Heading level={3} size='sm'>
+              Neutral fargen
+            </Heading>
+            <Paragraph>
+              Vi anbefaler minst 4.5:1 kontrast mot bakgrunnsfargene for neutral
+              fargen også.{' '}
+            </Paragraph>
+
+            <Heading level={3} size='sm'>
+              Brand fargene
+            </Heading>
+            <Paragraph>
+              Base fargene for brand fargene blir ikke brukt på viktige
+              elementer på komponentene som Designsystemet leverer. Det er
+              derfor litt mer fleksibelt hvilken farge man velger. Om man ønsker
+              å bruke disse base fargene på viktige elementer på siden så må man
+              være klar over 3:1 og 4.5:1 kontrastkravene til WCAG for AA.
+            </Paragraph>
+
+            <Heading level={2} size='md'>
+              Feilmeldinger og ting å være obs på
+            </Heading>
+            <Paragraph>
+              Temabyggeren lar deg velge 5 farger for å bygge et tema: Accent,
+              Neutral, Brand 1, Brand 2 og Brand 3. Disse fargekodene vil bli
+              til <code>Base Default</code> i fargeskalaene. Det er derfor
+              enkelte ting man må passe på når man velger en farge for at det
+              skal fungere optimalt i systemet.
+            </Paragraph>
+
+            <Heading level={3} size='sm'>
+              Fargen har mindre enn 3:1 kontrast mot bakgrunnsfargene
+            </Heading>
+
+            <img src='img/test.png' alt='' />
+
             <Paragraph>
               Om du velger en farge som har mindre enn 3:1 kontrast mot
-              bakgrunnsfargene så vil du få ein advarsel om dette i verktøyet.
-              Det er spesielt viktig at Accent og Neutral fargen har mer
-              kontrast enn dette fordi disse fargene blir brukt på for eksempel
-              Button og Checkbox.
+              bakgrunnsfargene så vil du få en advarsel om dette i verktøyet.
+              Det er spesielt viktig at <code>Accent</code> og{' '}
+              <code>Neutral</code> fargene er innenfor denne kontrastgrensen
+              fordi <code>Base</code> fargene blir brukt på for eksempel{' '}
+              <code>Button</code> og <code>Checkbox</code> komponentene. Mindre
+              kontrast enn dette bryter WCAG kravet:{' '}
+              <a href='https://www.uutilsynet.no/wcag-standarden/1411-kontrast-ikke-tekstlig-innhold-niva-aa/145'>
+                1.4.11 Kontrast for ikke-tekstlig innhold{' '}
+              </a>
+              . For Brand fargene er dette mindre viktig siden det ikke brukes{' '}
+              <code>Base</code> farger på viktige elementer på siden. Det er
+              viktig å være klar over dette om man velger å bruke{' '}
+              <code>Base</code> fargene til å lage sine egne ting.
             </Paragraph>
-
             <Heading level={3} size='sm'>
-              Neutral farge
+              Kontrast-fargen fungerer ikke på alle base fargene
             </Heading>
-            <Paragraph>ff</Paragraph>
 
-            <Heading level={3} size='sm'>
-              Brand farger
-            </Heading>
-            <Paragraph>ff</Paragraph>
+            <img src='img/test2.png' alt='' />
 
-            <Heading level={2} size='md'>
-              Border radius
-            </Heading>
-            <Paragraph>ff</Paragraph>
-
-            <Heading level={2} size='md'>
-              WCAG kontrastmodus
-            </Heading>
-            <Paragraph>ff</Paragraph>
-            <img src='img/1080p.jpg' alt='' />
+            <Paragraph>
+              Verktøyet genererer 2 kontrast farger ({' '}
+              <code>Contrast-default</code> og <code>Contrast-subtle</code> )
+              basert på fargen man har valgt i verktøyet. En lys base farge vil
+              gjøre kontrastfargene mørke og omvendt.{' '}
+              <code>Contrast-default</code> fargen skal fungere som tekstfarge
+              oppå alle 3 base farger man velger med en kontrast på minst 4.5:1
+              for å støtte WCAG sitt AA krav. I eksempel 1 i bildet over klarer
+              ikke verktøyet å finne en hvit eller svart kontrast-farge som
+              fungerer på alle 3 base fargene. Verktøyet velger da ut en
+              kontrastfarge som fungerer på <code>Base Default</code> og
+              prioriterer <code>Base Hover</code> og <code>Base Active</code>{' '}
+              mindre. Feilmeldingen i verktøyet vil gi informasjon om hvilke
+              base farger kontrastfargen støtter.
+            </Paragraph>
           </div>
         </Container>
       </main>
