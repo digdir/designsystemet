ab6514225de10d9cceb2d4ea01edb67d512df8df
Eirik Backer <eirik.backer@gmail.com>
2024-09-13 15:10:02 +0200

    fix: postcss composes directive (#2410)
    - Closes #2385
    - Using same syntax as postcss `composes`, but custom implementation for
    `postcss`

--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -1,6 +1,10 @@
+const postcss = require('postcss');
+const fs = require('node:fs');
+
 module.exports = {
   plugins: [
     require('postcss-import'),
+    postcssComposes(),
     require('postcss-nesting'),
     require('cssnano')({
       preset: 'default',
@@ -8,3 +12,30 @@ module.exports = {
     require('autoprefixer'),
   ],
 };
+
+function postcssComposes() {
+  return {
+    postcssPlugin: '@composes', // Allows `@composes classname from './file.css'` directive
+    AtRule: {
+      composes: async (rule) => {
+        const cache = {};
+        const sanitizedParams = rule.params.replace(/["']/g, '').trim();
+        const [selector, from] = sanitizedParams.split(/\s+from\s+/);
+
+        if (!cache[from])
+          cache[from] = await postcss([]).process(fs.readFileSync(from), {
+            from,
+          });
+
+        cache[from].root.walkRules((from) => {
+          if (from.selector.startsWith(`.${selector}`))
+            rule.replaceWith(
+              from.clone({
+                selector: from.selector.replace(`.${selector}`, '&'),
+              }),
+            );
+        });
+      },
+    },
+  };
+}
