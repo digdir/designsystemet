2e7c7d3c8981ba18f5a8f39362406186c6eca724
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-09-16 14:01:05 +0200

    build(postcss): fix composes plugin folder check (#2421)

--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -1,5 +1,6 @@
 const postcss = require('postcss');
 const fs = require('node:fs');
+const path = require('node:path');
 
 module.exports = {
   plugins: [
@@ -17,21 +18,28 @@ function postcssComposes() {
   return {
     postcssPlugin: '@composes', // Allows `@composes classname from './file.css'` directive
     AtRule: {
-      composes: async (rule) => {
+      composes: async (rule, { AtRule }) => {
         const cache = {};
         const sanitizedParams = rule.params.replace(/["']/g, '').trim();
         const [selector, from] = sanitizedParams.split(/\s+from\s+/);
+        const resolvedFrom = path.resolve(
+          path.dirname(rule.source.input.file),
+          from,
+        );
 
-        if (!cache[from])
-          cache[from] = await postcss([]).process(fs.readFileSync(from), {
-            from,
-          });
+        if (!cache[resolvedFrom])
+          cache[resolvedFrom] = await postcss([]).process(
+            fs.readFileSync(resolvedFrom),
+            {
+              from: resolvedFrom,
+            },
+          );
 
-        cache[from].root.walkRules((from) => {
-          if (from.selector.startsWith(`.${selector}`))
+        cache[resolvedFrom].root.walkRules((fromRule) => {
+          if (fromRule.selector.startsWith(`.${selector}`))
             rule.replaceWith(
-              from.clone({
-                selector: from.selector.replace(`.${selector}`, '&'),
+              fromRule.clone({
+                selector: fromRule.selector.replace(`.${selector}`, '&'),
               }),
             );
         });
