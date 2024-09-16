89cdaf3d8b3d7fed06e3ba4e504ebcb6f0479bb4
Eirik Backer <eirik.backer@gmail.com>
2024-09-16 13:58:25 +0200

    fix(Popover): popovertarget react-docgen (#2414)
    - Fixes so `popovertarget` does not pop up in all Storybook props lists
    - Documents `@types/react-dom` extension and compability
    
    ---------
    
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -2,6 +2,7 @@ import { dirname, join, resolve } from 'node:path';
 import { env } from 'node:process';
 
 import type { StorybookConfig } from '@storybook/react-vite';
+import type { PropItem } from 'react-docgen-typescript';
 
 const config: StorybookConfig = {
   typescript: {
@@ -20,6 +21,14 @@ const config: StorybookConfig = {
       include: [resolve(__dirname, '../../../packages/react/**/**.tsx')], // <- This is the important line.
       shouldExtractLiteralValuesFromEnum: true,
       shouldRemoveUndefinedFromOptional: true,
+      propFilter: ({ name, declarations = [] }: PropItem) => {
+        /* Default from `react-docgen-typescript` we have to add back when using a custom prop filter */
+        const hasDescription = declarations.some(
+          ({ fileName }) => !fileName.includes('node_modules'),
+        );
+
+        return hasDescription && name !== 'popovertarget'; // Skip popovertarget @types/react-dom patch
+      },
     },
   },
   stories: [
