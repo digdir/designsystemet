59d04565e05e8ee8b72570ea5d75a06de7c1100f
Michael Marszalek <mimarz@gmail.com>
2024-09-16 14:49:08 +0200

    feat(cli): :sparkles: Add option for defining theme name (#2374)
    resolves #2334
    
    - Added new option to `--theme` to cli for defining theme name.
      - Will add new theme or overwrite existing theme.
    - Added new script in cli (`update:template`) for updating template
    files from our "master" `design-tokens` in root.
    - Added textfield in theme-builder tokens modal for defining theme name.
    - Not a fan of having code, but commented our template generation for
    `$metadata` and `$themes` as this is an internal improvement that can be
    done later.
    - Some updates to components used in theme-builder to get desired user
    interface.

--- packages/cli/bin/designsystemet.ts
+++ packages/cli/bin/designsystemet.ts
@@ -14,15 +14,16 @@ program.name('Designsystemet').description('CLI for working with Designsystemet'
 
 function makeTokenCommands() {
   const tokenCmd = createCommand('tokens');
+  const DEFAULT_TOKENSDIR = './design-tokens';
 
   tokenCmd
     .command('build')
     .description('Build Designsystemet tokens')
-    .option('-t, --tokens <string>', `Path to ${chalk.blue('design-tokens')}`, './design-tokens')
+    .option('-t, --tokens <string>', `Path to ${chalk.blue('design-tokens')}`, DEFAULT_TOKENSDIR)
     .option('-o, --out <string>', `Output directory for built ${chalk.blue('design-tokens')}`, './dist/tokens')
     .option('-p, --preview', 'Generate preview token.ts files', false)
     .action((opts) => {
-      const tokens = typeof opts.tokens === 'string' ? opts.tokens : './design-tokens';
+      const tokens = typeof opts.tokens === 'string' ? opts.tokens : DEFAULT_TOKENSDIR;
       const out = typeof opts.out === 'string' ? opts.out : './dist/tokens';
       const preview = opts.preview;
       console.log(`Bulding tokens in ${chalk.green(tokens)}`);
@@ -32,20 +33,21 @@ function makeTokenCommands() {
   tokenCmd
     .command('create')
     .description('Create Designsystemet tokens')
-    .option('-w, --write [string]', `Output directory for created ${chalk.blue('design-tokens')}`)
-    .option('-a, --accent <number>', `Accent hex color`)
-    .option('-n, --neutral <number>', `Neutral hex color`)
-    .option('-b1, --brand1 <number>', `Brand1 hex color`)
-    .option('-b2, --brand2 <number>', `Brand2 hex color`)
-    .option('-b3, --brand3 <number>', `Brand3 hex color`)
-    .option('-f, --font-family <string>', `Font family`)
+    .requiredOption('-a, --accent <number>', `Accent hex color`)
+    .requiredOption('-n, --neutral <number>', `Neutral hex color`)
+    .requiredOption('-b1, --brand1 <number>', `Brand1 hex color`)
+    .requiredOption('-b2, --brand2 <number>', `Brand2 hex color`)
+    .requiredOption('-b3, --brand3 <number>', `Brand3 hex color`)
+    .option('-w, --write [string]', `Output directory for created ${chalk.blue('design-tokens')}`, DEFAULT_TOKENSDIR)
+    .option('-f, --font-family <string>', `Font family`, 'Inter')
+    .option('--theme <string>', `Theme name`, 'theme')
     .action(async (opts) => {
-      // const out = typeof opts.out === 'string' ? opts.out : './dist/tokens';
-      console.log(`Creating tokens with options ${chalk.green(JSON.stringify(opts))}`);
-      const family = typeof opts.fontFamily === 'string' ? opts.fontFamily : 'Inter';
-      const write = typeof opts.write === 'boolean' ? './design-tokens' : opts.write;
+      const { theme, fontFamily } = opts;
+      console.log(`Creating tokens with options ${chalk.green(JSON.stringify(opts, null, 2))}`);
+      const write = typeof opts.write === 'boolean' ? DEFAULT_TOKENSDIR : opts.write;
 
       const props = {
+        themeName: theme,
         colors: {
           accent: convertToHex(opts.accent),
           neutral: convertToHex(opts.neutral),
@@ -54,14 +56,14 @@ function makeTokenCommands() {
           brand3: convertToHex(opts.brand3),
         },
         typography: {
-          fontFamily: family,
+          fontFamily: fontFamily,
         },
       };
 
       const tokens = createTokens(props);
 
       if (write) {
-        await writeTokens(write, tokens);
+        await writeTokens({ writeDir: write, tokens, themeName: theme });
       }
 
       return Promise.resolve();
--- packages/cli/src/tokens/write.ts
+++ packages/cli/src/tokens/write.ts
@@ -1,5 +1,7 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import type { ThemeObject } from '@tokens-studio/types';
+import chalk from 'chalk';
 import * as R from 'ramda';
 import type { ColorMode } from '../colors/types.js';
 import type { Collection, File, Tokens, TokensSet, TypographyModes } from './types.js';
@@ -7,12 +9,15 @@ import { generateMetadataJson } from './write/generate$metadata.js';
 import { generateThemesJson } from './write/generate$themes.js';
 
 const DIRNAME: string = import.meta.dirname || __dirname;
-const DEFAULT_FILES_PATH = path.join(DIRNAME, '../../init/template/default-files/design-tokens/');
+const DEFAULT_FILES_PATH = path.join(DIRNAME, './design-tokens/default');
+const TEMPLATE_FILES_PATH = path.join(DIRNAME, './design-tokens/template/');
+
+export const stringify = (data: unknown) => JSON.stringify(data, null, 2);
 
 const generateColorModeFile = (folder: ColorMode, name: Collection, tokens: TokensSet, outPath: string): File => {
   const path = `${outPath}/primitives/modes/colors/${folder}`;
   return {
-    data: JSON.stringify(tokens, null, 2),
+    data: stringify(tokens),
     path,
     filePath: `${path}/${name}.json`,
   };
@@ -26,45 +31,77 @@ const generateTypographyFile = (
 ): File => {
   const path = `${outPath}/primitives/modes/typography/${folder}`;
   return {
-    data: JSON.stringify(tokens, null, 2),
+    data: stringify(tokens),
     path,
     filePath: `${path}/${name}.json`,
   };
 };
 
-export const writeTokens = async (writeDir: string, tokens: Tokens) => {
+type WriteTokensOptions = {
+  writeDir: string;
+  tokens: Tokens;
+  themeName: string;
+};
+
+export const writeTokens = async (options: WriteTokensOptions) => {
+  const { writeDir, tokens, themeName } = options;
   const targetDir = path.resolve(process.cwd(), String(writeDir));
+  const $themesPath = path.join(targetDir, '$themes.json');
+  const $metadataPath = path.join(targetDir, '$metadata.json');
+  let themes = [themeName];
+
   await fs.mkdir(targetDir, { recursive: true });
 
-  // Generate metadata and themes json for Token Studio and build script
-  console.log('Generating metadata and themes files');
-  const $theme = generateThemesJson(['light', 'dark', 'contrast'], ['theme']);
-  const $metadata = generateMetadataJson(['light', 'dark', 'contrast'], ['theme']);
+  try {
+    // Update with existing themes
+    const $themes = await fs.readFile($themesPath, 'utf-8');
+    const themeObjects = (JSON.parse($themes) as ThemeObject[]) || [];
+    themes = R.pipe(
+      R.filter((obj: ThemeObject) => R.toLower(obj.group || '') === 'theme'),
+      R.map(R.prop('name')),
+      R.append(themeName),
+      R.uniq,
+    )(themeObjects) as string[];
+  } catch (error) {}
 
-  await fs.writeFile(path.join(targetDir, '$themes.json'), JSON.stringify($theme, null, 2));
-  await fs.writeFile(path.join(targetDir, '$metadata.json'), JSON.stringify($metadata, null, 2));
+  console.log(`Themes: ${chalk.blue(themes.join(', '))}`);
 
-  console.log(`Copying default files to ${targetDir}`);
+  // Create metadata and themes json for Token Studio and build script
+  const $theme = generateThemesJson(['light', 'dark', 'contrast'], themes);
+  const $metadata = generateMetadataJson(['light', 'dark', 'contrast'], themes);
+
+  await fs.writeFile($themesPath, stringify($theme));
+  await fs.writeFile($metadataPath, stringify($metadata));
+
+  // Copy default files
   await fs.cp(DEFAULT_FILES_PATH, targetDir, {
     recursive: true,
   });
 
+  // Create themes file
+  await fs.mkdir(path.join(targetDir, 'themes'), { recursive: true });
+  const themeTemplate = await fs.readFile(path.join(TEMPLATE_FILES_PATH, `themes/theme.json`), 'utf-8');
+  await fs.writeFile(path.join(targetDir, `themes/${themeName}.json`), themeTemplate.replaceAll('<theme>', themeName));
+
   const files: File[] = [
-    generateColorModeFile('light', 'theme', tokens.colors.light.theme, targetDir),
+    generateColorModeFile('light', themeName, tokens.colors.light[themeName], targetDir),
     generateColorModeFile('light', 'global', tokens.colors.light.global, targetDir),
-    generateColorModeFile('dark', 'theme', tokens.colors.dark.theme, targetDir),
+    generateColorModeFile('dark', themeName, tokens.colors.dark[themeName], targetDir),
     generateColorModeFile('dark', 'global', tokens.colors.dark.global, targetDir),
-    generateColorModeFile('contrast', 'theme', tokens.colors.contrast.theme, targetDir),
+    generateColorModeFile('contrast', themeName, tokens.colors.contrast[themeName], targetDir),
     generateColorModeFile('contrast', 'global', tokens.colors.contrast.global, targetDir),
-    generateTypographyFile('primary', 'theme', tokens.typography.primary, targetDir),
-    generateTypographyFile('secondary', 'theme', tokens.typography.primary, targetDir),
+    generateTypographyFile('primary', themeName, tokens.typography.primary, targetDir),
+    generateTypographyFile('secondary', themeName, tokens.typography.primary, targetDir),
   ];
 
   for (const file of files) {
     const dirPath = path.resolve(file.path);
     const filePath = path.resolve(file.filePath);
-    console.log(`Writing file ${filePath}`);
     await fs.mkdir(dirPath, { recursive: true });
     await fs.writeFile(filePath, file.data, { encoding: 'utf-8' });
   }
+
+  console.log(
+    `Finished creating Designsystem design tokens in ${chalk.green(writeDir)} for theme ${chalk.blue(themeName)}`,
+  );
 };
