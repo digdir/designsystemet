bfe57f521747e4fd1907113256832020023233f0
Michael Marszalek <mimarz@gmail.com>
2024-09-17 08:59:32 +0200

    test theme preview

--- apps/theme/app/page.tsx
+++ apps/theme/app/page.tsx
@@ -267,7 +267,7 @@ export default function Home() {
       <main className={classes.main}>
         <Container>
           <Heading size='md' className={classes.title}>
-            Sett opp fargetema
+            Sett opp fargetema X
           </Heading>
           <ThemeToolbar
             accentError={accentError}
