32fdbd4ea8161c0a69ffabb53d3a0dc9b07de2ff
Eirik Backer <eirik.backer@gmail.com>
2024-09-18 09:55:22 +0200

    fix(Breadcrumbs): css and api alignment (#2428)
    Part of https://github.com/digdir/designsystemet/issues/2295 and
    https://github.com/digdir/designsystemet/issues/2221
    Moves back link into `nav` as this make sense also from a11y perspective
    after discussion with NRK
    
    ---------
    
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>

--- packages/react/src/components/Breadcrumbs/BreadcrumbsItem.tsx
+++ packages/react/src/components/Breadcrumbs/BreadcrumbsItem.tsx
@@ -1,12 +1,9 @@
-import cl from 'clsx/lite';
 import { type HTMLAttributes, forwardRef } from 'react';
 
 export type BreadcrumbsItemProps = HTMLAttributes<HTMLLIElement>;
 
 export const BreadcrumbsItem = forwardRef<HTMLLIElement, BreadcrumbsItemProps>(
-  ({ className, ...rest }, ref) => (
-    <li ref={ref} className={cl('ds-breadcrumbs__item', className)} {...rest} />
-  ),
+  function BreadcrumbsItem({ className, ...rest }, ref) {
+    return <li ref={ref} {...rest} />;
+  },
 );
-
-BreadcrumbsItem.displayName = 'BreadcrumbsItem';
