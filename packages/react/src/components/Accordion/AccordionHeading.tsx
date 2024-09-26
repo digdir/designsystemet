523c50e5688e80dbfd3391f322ea30d231168a6e
barsnes <tobias.barsnes@digdir.no>
2024-09-26 08:40:27 +0200

    accordion

--- packages/react/src/components/Accordion/AccordionHeading.tsx
+++ packages/react/src/components/Accordion/AccordionHeading.tsx
@@ -15,18 +15,7 @@ export type AccordionHeadingProps = {
  * <AccordionHeader>Header</AccordionHeader>
  */
 export const AccordionHeading = forwardRef<HTMLElement, AccordionHeadingProps>(
-  function AccordionHeading({ className, ...rest }, ref) {
-    return (
-      <Paragraph asChild size='sm'>
-        <u-summary
-          class={cl(
-            'ds-focus ds-paragraph ds-paragraph--sm ds-line-height--md', // Very TMP fix awaiting CSS modules
-            className,
-          )}
-          ref={ref}
-          {...rest}
-        />
-      </Paragraph>
-    );
+  function AccordionHeading(rest, ref) {
+    return <u-summary ref={ref} {...rest} />;
   },
 );
