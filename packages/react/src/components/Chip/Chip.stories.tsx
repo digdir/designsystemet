ed5566d11ad0dd352e9712f0d23d222122223e49
eirikbacker <eirik.backer@mattilsynet.no>
2024-09-26 13:53:14 +0200

    chore: remove redundant story code

--- packages/react/src/components/Chip/Chip.stories.tsx
+++ packages/react/src/components/Chip/Chip.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 
 import { Chip } from '.';
 
--- packages/react/src/components/Divider/Divider.stories.tsx
+++ packages/react/src/components/Divider/Divider.stories.tsx
@@ -22,6 +22,3 @@ export const Preview: Story = (args) => (
     </Paragraph>
   </>
 );
-Preview.args = {
-  color: 'default',
-};
