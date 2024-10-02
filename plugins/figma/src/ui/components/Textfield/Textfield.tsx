c4bbc41ff3c5c77088b8177699af9531bf23c8f5
Øyvind Thune <oyvlei@gmail.com>
2024-10-02 12:34:14 +0200

    Stuff

--- plugins/figma/src/ui/components/Textfield/Textfield.tsx
+++ plugins/figma/src/ui/components/Textfield/Textfield.tsx
@@ -1,11 +1,10 @@
 import { Textfield as DSTextfield } from '@digdir/designsystemet-react';
 
-import classes from './Textfield.module.css';
 type TextfieldProps = {
   label: string;
   size?: 'sm' | 'md' | 'lg';
 };
 
 export const Textfield = ({ ...rest }: TextfieldProps) => {
-  return <DSTextfield {...rest} className={classes.input} />;
+  return <DSTextfield {...rest} />;
 };
