b4f8aba2133a476587a5d2517c0e848713051b0b
Eirik Backer <eirik.backer@gmail.com>
2024-10-08 13:25:23 +0200

    feat(Skeleton): text wrapping (#2569)
    - Fixes [#2518](https://github.com/digdir/designsystemet/issues/2518)
    - Discussed with design ✅

--- packages/react/src/components/loaders/Skeleton/Skeleton.stories.tsx
+++ packages/react/src/components/loaders/Skeleton/Skeleton.stories.tsx
@@ -9,6 +9,14 @@ type Story = StoryObj<typeof Skeleton>;
 export default {
   title: 'Komponenter/Loaders/Skeleton',
   component: Skeleton,
+  parameters: {
+    a11y: {
+      config: {
+        // Disable a11y empty heading rule as we intentionally set aria-hidden="true" on the Skeleton component inside Headings
+        rules: [{ id: 'empty-heading', selector: ':has(.ds-skeleton)' }],
+      },
+    },
+  },
 } as Meta;
 
 export const Preview: Story = {
@@ -29,7 +37,7 @@ export const Components: StoryFn<typeof Text> = () => {
     >
       <Skeleton variant='circle' width='50px' height='50px' />
       <Skeleton variant='rectangle' width='100px' height='50px' />
-      <Skeleton variant='text' width='50px' height='16px' />
+      <Skeleton variant='text' width='10' />
     </div>
   );
 };
@@ -51,13 +59,11 @@ export const UsageExample: StoryFn<typeof Skeleton> = () => {
         }}
       >
         <Skeleton variant='circle' width='30px' height='30px' />
-        <Heading asChild size='md'>
+        <Heading size='md'>
           <Skeleton variant='text'>En medium tittel</Skeleton>
         </Heading>
       </div>
-      <Skeleton variant='text' />
-      <Skeleton variant='text' />
-      <Skeleton variant='text' width='80%' />
+      <Skeleton variant='text' width='140' />
     </div>
   );
 };
@@ -79,16 +85,16 @@ export const Children: StoryFn<typeof Skeleton> = () => {
 export const As: StoryFn<typeof Skeleton> = () => {
   return (
     <>
-      <Heading size='lg' asChild>
+      <Heading size='lg'>
         <Skeleton variant='text'>Her er en heading</Skeleton>
       </Heading>
-      <Paragraph asChild>
+      <Paragraph>
         <Skeleton variant='text'>
           Her er en paragraf-komponent som blir rendret som en Skeleton
           variant="text".
         </Skeleton>
       </Paragraph>
-      <Paragraph asChild>
+      <Paragraph>
         <Skeleton variant='text'>
           Se hvordan Skeleton da overskriver stylingen til det enkelte
           elementet.
@@ -98,27 +104,21 @@ export const As: StoryFn<typeof Skeleton> = () => {
   );
 };
 
-export const TextExample: StoryFn<typeof Text> = () => {
-  return (
-    <>
-      <div style={{ display: 'flex', gap: '20px' }}>
-        <div style={{ width: '140px' }}>
-          <Heading size='md'>Heading</Heading>
-          <Paragraph size='sm'>
-            Her er en paragraf som går over flere linjer
-          </Paragraph>
-        </div>
-        <div style={{ width: '140px' }}>
-          <Heading size='md' asChild>
-            <Skeleton variant='text'>Heading</Skeleton>
-          </Heading>
-          <Paragraph size='sm'>
-            <Skeleton variant='text' />
-            <Skeleton variant='text' />
-            <Skeleton variant='text' width={80} />
-          </Paragraph>
-        </div>
-      </div>
-    </>
-  );
-};
+export const TextExample: StoryFn<typeof Text> = () => (
+  <div style={{ display: 'flex', gap: '20px', maxWidth: 300 }}>
+    <div>
+      <Heading size='md'>Heading</Heading>
+      <Paragraph size='sm'>
+        Her er en paragraf som går over flere linjer
+      </Paragraph>
+    </div>
+    <div>
+      <Heading size='md'>
+        <Skeleton variant='text'>Heading</Skeleton>
+      </Heading>
+      <Paragraph size='sm'>
+        <Skeleton variant='text' width={40} />
+      </Paragraph>
+    </div>
+  </div>
+);
