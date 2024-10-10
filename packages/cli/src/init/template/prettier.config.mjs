10e75b8ee05adbbf6da572fc906d663cca367976
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-10-10 14:07:12 +0200

    chore: update biome, remove prettier and stylelint (#2594)
    resolves #2411

--- packages/cli/src/init/template/prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * NOTE: This config exists to ensure files in the template dir are
- * formatted using the default prettier rules
- * @see https://prettier.io/docs/en/configuration.html
- * @type {import("prettier").Config}
- */
-const config = {};
-
-export default config;
