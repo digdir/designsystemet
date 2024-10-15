db678d4df66d0d51dcb34f1d01037649fbefa19f
Michael Marszalek <mimarz@gmail.com>
2024-10-15 13:24:11 +0200

    chore: :fire: Remove `init` CLI command (#2616)
    - Removed outdated `init` command in CLI. Replaced by `tokens create`
    - Removed unused `create-tokens` package.

--- packages/create-tokens/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: false,
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-};
--- packages/create-tokens/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { require } from 'tsx/cjs/api';
-
-require('./src/create-tokens.ts', import.meta.url);
