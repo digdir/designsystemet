24988654eccfc43b07fcc0c148267fe3c2d5b1fe
eirikbacker <eirik.backer@mattilsynet.no>
2024-10-15 16:41:24 +0200

    fix: support changing attributes

--- packages/react/src/components/form/Field/fieldA11Y.ts
+++ packages/react/src/components/form/Field/fieldA11Y.ts
@@ -1,40 +1,54 @@
 export const fieldA11Y = (fieldElement: HTMLElement | null) => {
   if (!fieldElement) return;
 
+  const elements = new Map<Element, string | null>();
   const uuid = `:${Math.round(Date.now() + Math.random() * 100).toString(36)}`;
-  const elements = fieldElement.getElementsByTagName("*"); // Speed up by using a live HTMLCollection instead of traversing MutationRecords
-  const connected = new Set<Element>();
   let input: Element | null = null;
 
-  const observer = createOptimizedMutationObserver(() => {
-    // Find elements
-    for (const el of elements) {
-      if ("validity" in el) input = el;
-      else if (el instanceof HTMLLabelElement || el.hasAttribute("data-field"))
-        connected.add(el);
+  const process = (mutations: Partial<MutationRecord>[]) => {
+    const changed: Node[] = [];
+    const removed: Node[] = [];
+
+    // Merge MutationRecords
+    for (const mutation of mutations) {
+      if (mutation.attributeName) changed.push(mutation.target ?? fieldElement);
+      changed.push(...(mutation.addedNodes || []));
+      removed.push(...(mutation.removedNodes || []));
     }
 
-    // Setup attributes
-    const describedby: string[] = [];
-    if (!fieldElement.contains(input)) input = null; // Reset if input is removed
-    if (input && !input.id) input.id = uuid; // Ensure input always has ID
-    for (const el of connected) {
-      if (!input || !fieldElement.contains(el)) {
-        connected.delete(el);
-        el.removeAttribute("for");
-        el.removeAttribute("id");
-      } else if (el instanceof HTMLLabelElement) el.htmlFor = input.id;
-      else {
-        el.id = `${input.id}:${el.getAttribute("data-field")}`;
-        describedby[el.hasAttribute("data-error") ? "unshift" : "push"](el.id); // Errors to the front
+    // Register elements
+    for (const el of changed)
+      if (isElement(el)) {
+        if (isLabel(el)) elements.set(el, el.htmlFor);
+        else if ("validity" in el) input = el;
+        else if (el.hasAttribute("data-field")) elements.set(el, el.id);
       }
+
+    // Reset removed elements
+    for (const el of removed)
+      if (isElement(el) && elements.has(el)) {
+        const attr = isLabel(el) ? "for" : "id";
+        setAttr(el, attr, elements.get(el));
+        elements.delete(el);
+      }
+
+    // Connect elements
+    const inputId = input?.id || uuid;
+    const describedby: string[] = [];
+    for (const [el, value] of elements) {
+      const attr = isLabel(el) ? "for" : "id";
+      const type = el.getAttribute("data-field");
+
+      if (!value) setAttr(el, attr, type ? `${inputId}:${type}` : inputId); // Ensure we have a value
+      if (type) describedby[type === "validation" ? "unshift" : "push"](el.id); // Validations to the front
     }
 
     // Set aria-describedby
-    if (!describedby.length) input?.removeAttribute("aria-describedby");
-    else input?.setAttribute("aria-describedby", describedby.join(" "));
-  });
+    setAttr(input, "id", inputId);
+    setAttr(input, "aria-describedby", describedby.join(" "));
+  };
 
+  const observer = createOptimizedMutationObserver(process);
   observer.observe(fieldElement, {
     attributeFilter: ["id", "for", "aria-describedby"],
     attributes: true,
@@ -42,22 +56,30 @@ export const fieldA11Y = (fieldElement: HTMLElement | null) => {
     subtree: true,
   });
 
+  process([{ addedNodes: fieldElement.querySelectorAll("*") }]); // Initial setup
+  observer.takeRecords(); // Clear initial setup queue
   return () => observer.disconnect();
 };
 
+// Utilities
+const isElement = (node: Node) => node instanceof Element;
+const isLabel = (node: Node) => node instanceof HTMLLabelElement;
+const setAttr = (el: Element | null, name: string, value?: string | null) =>
+  value ? el?.setAttribute(name, value) : el?.removeAttribute(name);
+
 // Speed up MutationObserver by debouncing, clearing internal queue after changes and only running when page is visible
-const createOptimizedMutationObserver = (callback: () => void) => {
-  let queue: ReturnType<typeof requestAnimationFrame> = 0;
-  const observer = new MutationObserver(() => {
-    cancelAnimationFrame(queue);
-    queue = requestAnimationFrame(process);
+const createOptimizedMutationObserver = (callback: MutationCallback) => {
+  const queue: MutationRecord[] = [];
+  const observer = new MutationObserver((mutations) => {
+    if (!queue.length) requestAnimationFrame(process);
+    queue.push(...mutations);
   });
 
   const process = () => {
-    callback();
+    callback(queue, observer);
+    queue.length = 0; // Reset queue
     observer.takeRecords(); // Clear queue due to DOM changes in callback
   };
 
-  requestAnimationFrame(process); // Initial setup
   return observer;
 };
