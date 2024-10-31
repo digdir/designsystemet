86fe8906697ead115b80980175c38cdfced166db
Michael Marszalek <mimarz@gmail.com>
2024-10-31 15:16:04 +0100

    refactor: move `Input.Affix` to `Field` (#2714)
    resolves #2640 
    
    - Did not create a changeset as this is unreleased/undocumented feature
    that has just been shifted around now.
    - Moved and renamed `Affix` & `AffixWrapper` from `Input` to `Field`
    - Added a story with example using other field types (input, textarea,
    select)
    - Created separate issue for further work on missing features #2715

--- packages/react/src/components/form/Input/index.ts
+++ packages/react/src/components/form/Input/index.ts
@@ -1,14 +1,2 @@
-import { Input as InputComp } from './Input';
-import { InputAffix, InputAffixWrapper } from './InputAffix';
-
-const Input = Object.assign(InputComp, {
-  Affix: InputAffix,
-  AffixWrapper: InputAffixWrapper,
-});
-
-Input.Affix.displayName = 'Input.Affix';
-Input.AffixWrapper.displayName = 'Input.AffixWrapper';
-
+export { Input } from './Input';
 export type { InputProps } from './Input';
-export type { InputAffixProps, InputAffixWrapperProps } from './InputAffix';
-export { Input, InputAffix, InputAffixWrapper };
