c419b291e5d3b36910bd3c8db67a2fc2c30b2941
Michael Marszalek <mimarz@gmail.com>
2024-10-31 13:34:05 +0100

    name

--- packages/react/src/components/form/Field/FieldAffix.tsx
+++ packages/react/src/components/form/Field/FieldAffix.tsx
@@ -9,13 +9,14 @@ export type FieldAffixWrapperProps = Omit<
 export const FieldAffixWrapper = forwardRef<
   HTMLDivElement,
   FieldAffixWrapperProps
->(function InputAddons({ className, ...rest }, ref) {
+>(function FieldAffixWrapper({ className, ...rest }, ref) {
   return (
     <div className={cl('ds-field-affix', className)} ref={ref} {...rest} />
   );
 });
 
 export type FieldAffixProps = Omit<HTMLAttributes<HTMLDivElement>, 'prefix'>;
+
 export const FieldAffix = forwardRef<HTMLSpanElement, FieldAffixProps>(
   function FieldAffix(rest, ref) {
     return <span aria-hidden='true' ref={ref} {...rest} />;
