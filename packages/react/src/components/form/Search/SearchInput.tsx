10ace871c35785090781e001adfe644f63292a45
barsnes <tobias.barsnes@digdir.no>
2024-11-04 13:47:12 +0100

    dont require aria label, but till in docs

--- packages/react/src/components/form/Search/SearchInput.tsx
+++ packages/react/src/components/form/Search/SearchInput.tsx
@@ -4,11 +4,7 @@ import { Input } from '../Input';
 export type SearchInputProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
   'readOnly' | 'type'
-> &
-  (
-    | { 'aria-label': string; 'aria-labelledby'?: never }
-    | { 'aria-label'?: never; 'aria-labelledby': string }
-  );
+>;
 
 export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
   function SearchInput({ ...rest }, ref) {
