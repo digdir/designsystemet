2bf964d368ef106f716070aba8d5011c1c23f964
Øyvind Thune <oyvlei@gmail.com>
2024-11-11 14:07:27 +0100

    Added list grid

--- apps/theme/components/ColorPreview/ColorPreview.tsx
+++ apps/theme/components/ColorPreview/ColorPreview.tsx
@@ -8,12 +8,20 @@ import {
   Checkbox,
   Heading,
   Paragraph,
+  Switch,
+  ToggleGroup,
 } from '@digdir/designsystemet-react';
+import cl from 'clsx/lite';
+import { useState } from 'react';
 import { useThemeStore } from '../../store';
+import listClasses from './Card2.module.css';
 import classes from './ColorPreview.module.css';
+
+type ViewType = 'list' | 'grid';
 
 export const ColorPreview = () => {
   const colors = useThemeStore((state) => state.colors);
+  const [view, setView] = useState<ViewType>('list');
 
   type CardProps = {
     color: {
@@ -51,12 +59,19 @@ export const ColorPreview = () => {
   };
 
   const Card = ({ color }: CardProps) => {
+    if (view === 'list') {
+      return <Card2 color={color} />;
+    }
+    return <Card1 color={color} />;
+  };
+
+  const Card1 = ({ color }: CardProps) => {
     return (
       <div style={setStyle(color.colors.light)} className={classes.card}>
         <Heading className={classes.title} data-size='2xs'>
           {color.name}
         </Heading>
-        <Paragraph data-size='sm'>
+        <Paragraph data-size='sm' className={classes.desc}>
           Livet er for kort til å være grått. Fyll deg selv og dine dager med
           farger.
         </Paragraph>
@@ -78,6 +93,35 @@ export const ColorPreview = () => {
       </div>
     );
   };
+  const Card2 = ({ color }: CardProps) => {
+    return (
+      <div
+        style={setStyle(color.colors.light)}
+        className={cl(classes.card, listClasses.card)}
+      >
+        <div className={listClasses.text}>
+          <Heading className={listClasses.title} data-size='2xs'>
+            {color.name}
+          </Heading>
+          <Paragraph className={classes.desc} data-size='sm'>
+            Farger gjør livet mer fargerikt
+          </Paragraph>
+        </div>
+        <div className={listClasses.checkGroup}>
+          <Checkbox data-size='sm' label='Checkbox' value='value' checked />
+          <Switch description='' position='left' size='sm' checked>
+            Switch
+          </Switch>
+        </div>
+        <div className={classes.btnGroup}>
+          <Button data-size='sm'>Opprett</Button>
+          <Button data-size='sm' variant='secondary'>
+            Avbryt
+          </Button>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className={classes.container}>
       <div className={classes.left}>
@@ -86,8 +130,20 @@ export const ColorPreview = () => {
           TODO: Litt dokumentasjon om fargesystemet, kontraster og bruk av
           farger
         </Paragraph>
+        <div className={classes.bottom}>
+          <div className={classes.label}>Visning:</div>
+          <ToggleGroup
+            data-size='sm'
+            defaultValue='list'
+            name='toggle-group-nuts'
+            onChange={(value) => setView(value as ViewType)}
+          >
+            <ToggleGroup.Item value='list'>Liste</ToggleGroup.Item>
+            <ToggleGroup.Item value='grid'>Grid</ToggleGroup.Item>
+          </ToggleGroup>
+        </div>
       </div>
-      <div className={classes.right}>
+      <div className={cl(classes.right, view === 'grid' && classes.grid)}>
         {colors.main.map((color, index) => (
           <Card key={index} color={color} />
         ))}
