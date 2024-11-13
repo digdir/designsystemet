01cb08ec8cd134c1884811978e09477baddda062
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-11-13 12:08:16 +0100

    feat(useRadioGroup + useCheckboxGroup): add hooks (#2739)
    This is a fork from #2712, and will be implemented further here.
    
    ---------
    
    Co-authored-by: eirikbacker <eirik.backer@mattilsynet.no>
    Co-authored-by: Eirik Backer <eirik.backer@gmail.com>
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

---
+++ packages/react/src/utilities/hooks/useCheckboxGroup/useCheckboxGroup.ts
@@ -0,0 +1,216 @@
+import { useMergeRefs } from '@floating-ui/react';
+import { useEffect, useId, useRef, useState } from 'react';
+import type { ChangeEvent, ReactNode } from 'react';
+
+export type UseCheckboxGroupProps = {
+  /**
+   * Disables all checkboxes in the group.
+   */
+  disabled?: boolean;
+  /**
+   * Error message for the group.
+   * If set, all checkboxes will have `aria-invalid` set to `true`.
+   */
+  error?: ReactNode;
+  /**
+   * Name of the group.
+   * If not set, a random id will be generated.
+   */
+  name?: string;
+  /**
+   * Makes all checkboxes in the group read-only.
+   * If set, all checkboxes will have `aria-readonly` set to `true`.
+   */
+  readOnly?: boolean;
+  /**
+   * Initial value of the group
+   * @default []
+   */
+  value?: string[];
+  /**
+   * Makes all checkboxes in the group required.
+   * If set, all checkboxes will have `required` set to `true`.
+   */
+  required?: boolean;
+  /**
+   * Callback that is called when the value of the group changes.
+   * @param nextValue string[]
+   * @param currentValue string[]
+   * @returns void
+   */
+  onChange?: (nextValue: string[], currentValue: string[]) => void;
+};
+
+/**
+ * Get anything that is set on a checkbox, but
+ * remove anything that comes from the group itself.
+ */
+export type GetCheckboxProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  | 'prefix'
+  | 'role'
+  | 'type'
+  | 'size'
+  | 'aria-label'
+  | 'aria-labelledby'
+  | 'label'
+  | 'checked'
+  | 'value'
+> & {
+  /** Enables indeterminate handling for this `Checkbox` and `CheckboxGroup` */
+  allowIndeterminate?: boolean;
+  ref?: React.RefObject<HTMLInputElement>;
+  value?: string;
+};
+
+const toggleIndeterminate = (
+  getIndeterminateInputs: () => HTMLInputElement[],
+  getInputs: (checked: boolean) => HTMLInputElement[],
+) => {
+  const inputs = getIndeterminateInputs();
+  const checked = !!getInputs(true).length;
+  const unchecked = !!getInputs(false).length;
+
+  for (const input of inputs) {
+    input.indeterminate = unchecked && checked;
+    input.checked = !unchecked && checked;
+  }
+};
+
+export function useCheckboxGroup(props?: UseCheckboxGroupProps) {
+  const {
+    error,
+    name: groupName,
+    onChange,
+    value = [],
+    disabled,
+    readOnly,
+    required,
+  } = props || {};
+
+  const [groupValue, setGroupValue] = useState(value);
+  const namedId = useId();
+  const errorId = useId();
+  const checkboxRefs = useRef<Set<HTMLInputElement>>(new Set());
+  const indeterminateRefs = useRef<Set<HTMLInputElement>>(new Set());
+
+  const getInputs = (checked: boolean) =>
+    Array.from(checkboxRefs.current.values()).filter(
+      (input) => input.checked === checked,
+    );
+
+  const getIndeterminateInputs = () =>
+    Array.from(indeterminateRefs.current.values());
+
+  return {
+    /**
+     * Current value of the group.
+     */
+    value: groupValue,
+    /**
+     * Set the value of the group.
+     *
+     * @param value string[]
+     * @returns void
+     */
+    setValue: setGroupValue,
+    /**
+     * Props to send to the `Checkbox` component.
+     * Accepts value or object
+     * @example
+     * <Checkbox {...getCheckboxProps('value')} />
+     *
+     * @example allow indeterminate
+     * <Checkbox {...getCheckboxProps({ value: 'all', allowIndeterminate: true })} />
+     */
+    getCheckboxProps: (propsOrValue?: string | GetCheckboxProps) => {
+      const props =
+        typeof propsOrValue === 'string'
+          ? { value: propsOrValue }
+          : propsOrValue || {};
+
+      const {
+        allowIndeterminate = false,
+        ref = undefined,
+        value = '',
+        ...rest
+      } = props;
+
+      const inputRef = useRef<HTMLInputElement>(null);
+      const mergedRefs = useMergeRefs([ref, inputRef]);
+
+      const handleChange = () => {
+        const nextGroupValue = Array.from(
+          getInputs(true),
+          ({ value }) => value,
+        );
+        setGroupValue(nextGroupValue);
+        onChange?.(nextGroupValue, groupValue);
+      };
+
+      const indeterminateChange = () => {
+        if (!inputRef.current) return;
+        const checked = !!inputRef.current.checked;
+        for (const input of getInputs(!checked)) {
+          /* We use click to send both event and change checked state */
+          input.click();
+        }
+      };
+
+      useEffect(() => {
+        if (!allowIndeterminate) return;
+
+        toggleIndeterminate(getIndeterminateInputs, getInputs);
+      }, [groupValue]);
+
+      useEffect(() => {
+        if (!inputRef.current) return;
+
+        const input = inputRef.current;
+        const refs = allowIndeterminate ? indeterminateRefs : checkboxRefs;
+        refs.current.add(input);
+
+        if (getIndeterminateInputs().length)
+          toggleIndeterminate(getIndeterminateInputs, getInputs);
+
+        return () => {
+          refs.current.delete(input);
+        };
+      }, [value]);
+
+      return {
+        /* Spread anything the user has set first */
+        ...rest,
+        /* Concat ours with the user prop */
+        'aria-describedby':
+          `${error ? errorId : ''} ${rest['aria-describedby'] || ''}`.trim() ||
+          undefined,
+        'aria-invalid': !!error || rest['aria-invalid'],
+        checked: allowIndeterminate ? undefined : groupValue.includes(value),
+        name: rest.name || groupName || namedId,
+        onChange: (e: ChangeEvent<HTMLInputElement>) => {
+          rest.onChange?.(e);
+          if (e.defaultPrevented) return;
+          allowIndeterminate && indeterminateChange();
+          handleChange();
+        },
+        ref: mergedRefs,
+        value,
+        disabled: disabled || rest.disabled,
+        readOnly: readOnly || rest.readOnly,
+        required: required || rest.required,
+      };
+    },
+    /**
+     * Props to send to the `ValidationMessage` component.
+     *
+     * @example
+     * <ValidationMessage {...validationMessageProps} />
+     */
+    validationMessageProps: {
+      children: error,
+      hidden: !error,
+      id: errorId,
+    },
+  };
+}
---
+++ packages/react/src/utilities/hooks/useRadioGroup/useRadioGroup.test.tsx
@@ -0,0 +1,149 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react';
+
+import {
+  Fieldset,
+  Radio,
+  ValidationMessage,
+  useRadioGroup,
+} from '../../../components';
+import type { GetRadioProps, UseRadioGroupProps } from './useRadioGroup';
+
+const RadioGroup = (args: UseRadioGroupProps) => {
+  const { getRadioProps, validationMessageProps } = useRadioGroup(args);
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Legend</Fieldset.Legend>
+      <Radio label='Test 1' {...getRadioProps('test1')} />
+      <Radio label='Test 2' {...getRadioProps('test2')} />
+      <Radio label='Test 3' {...getRadioProps('test3')} />
+      <ValidationMessage {...validationMessageProps} />
+    </Fieldset>
+  );
+};
+
+const RadioGroupRadio = (args: GetRadioProps) => {
+  const { getRadioProps, validationMessageProps } = useRadioGroup();
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Legend</Fieldset.Legend>
+      <Radio label='Test 1' {...getRadioProps(args)} />
+      <Radio label='Test 2' {...getRadioProps('test2')} />
+      <ValidationMessage {...validationMessageProps} />
+    </Fieldset>
+  );
+};
+
+describe('RadioGroup', () => {
+  test('has generated name for Radio children', () => {
+    render(<RadioGroup />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    expect(radio1).toHaveAttribute('name');
+    expect(radio2).toHaveAttribute('name');
+    expect(radio3).toHaveAttribute('name');
+  });
+  test('has passed name to Radio children', (): void => {
+    render(<RadioGroup name='my-name' />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    expect(radio1).toHaveAttribute('name', 'my-name');
+    expect(radio2).toHaveAttribute('name', 'my-name');
+    expect(radio3).toHaveAttribute('name', 'my-name');
+  });
+  test('has passed disabled to Radio children', (): void => {
+    render(<RadioGroup disabled />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    expect(radio1).toHaveAttribute('disabled');
+    expect(radio2).toHaveAttribute('disabled');
+    expect(radio3).toHaveAttribute('disabled');
+  });
+  test('has passed readOnly to Radio children', (): void => {
+    render(<RadioGroup readOnly />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    expect(radio1).toHaveAttribute('readonly');
+    expect(radio2).toHaveAttribute('readonly');
+    expect(radio3).toHaveAttribute('readonly');
+  });
+  test('has passed required to Radio children', (): void => {
+    render(<RadioGroup required />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    expect(radio1).toHaveAttribute('required');
+    expect(radio2).toHaveAttribute('required');
+    expect(radio3).toHaveAttribute('required');
+  });
+  test('has passed aria-invalid to Radio children', (): void => {
+    render(<RadioGroup error='message' />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    const error = screen.getByText('message');
+    expect(radio1).toHaveAttribute('aria-invalid', 'true');
+    expect(radio2).toHaveAttribute('aria-invalid', 'true');
+    expect(radio3).toHaveAttribute('aria-invalid', 'true');
+    expect(error).toBeVisible();
+  });
+  test('has correct Radio checked when value is used', () => {
+    render(<RadioGroup value='test1' />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+    expect(radio1).toBeChecked();
+    expect(radio2).not.toBeChecked();
+    expect(radio3).not.toBeChecked();
+  });
+  test('has passed clicked Radio element to onChange', async () => {
+    const user = userEvent.setup();
+    const onChangeMock = vi.fn();
+
+    render(<RadioGroup onChange={onChangeMock} />);
+
+    const radio1 = screen.getByLabelText('Test 1');
+    const radio2 = screen.getByLabelText('Test 2');
+    const radio3 = screen.getByLabelText('Test 3');
+
+    await act(async () => await user.click(radio1));
+    expect(onChangeMock).toHaveBeenCalledWith('test1', '');
+    expect(radio1).toBeChecked();
+    expect(radio2).not.toBeChecked();
+    expect(radio3).not.toBeChecked();
+  });
+
+  test('correctly merges passed props with generated props', async () => {
+    const user = userEvent.setup();
+    const onChangeMock = vi.fn();
+    const customAriaDescribedBy = 'custom aria-describedby';
+
+    render(
+      <RadioGroupRadio
+        value='test1'
+        onChange={onChangeMock}
+        aria-describedby={customAriaDescribedBy}
+      />,
+    );
+
+    const radio1 = screen.getByLabelText('Test 1');
+    await act(async () => await user.click(radio1));
+
+    expect(onChangeMock).toHaveBeenCalledOnce();
+    expect(radio1).toBeChecked();
+
+    expect(radio1).toHaveAttribute('aria-describedby', customAriaDescribedBy);
+  });
+});
