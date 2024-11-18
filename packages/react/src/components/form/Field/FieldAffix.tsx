3d3f4a8acb716c3c361dda6b727ac117eefe3810
eirikbacker <eirik.backer@mattilsynet.no>
2024-11-18 13:52:57 +0100

    fix(Field.Affix): make more robust

--- packages/react/src/components/form/Field/FieldAffix.tsx
+++ packages/react/src/components/form/Field/FieldAffix.tsx
@@ -2,19 +2,15 @@ import cl from 'clsx/lite';
 import type { HTMLAttributes } from 'react';
 import { forwardRef } from 'react';
 
-export type FieldAffixWrapperProps = Omit<
-  HTMLAttributes<HTMLDivElement>,
-  'prefix'
->;
+export type FieldAffixesProps = Omit<HTMLAttributes<HTMLDivElement>, 'prefix'>;
 
-export const FieldAffixWrapper = forwardRef<
-  HTMLDivElement,
-  FieldAffixWrapperProps
->(function FieldAffixWrapper({ className, ...rest }, ref) {
-  return (
-    <div className={cl('ds-field-affix', className)} ref={ref} {...rest} />
-  );
-});
+export const FieldAffixes = forwardRef<HTMLDivElement, FieldAffixesProps>(
+  function FieldAffixes({ className, ...rest }, ref) {
+    return (
+      <div className={cl('ds-field-affixes', className)} ref={ref} {...rest} />
+    );
+  },
+);
 
 export type FieldAffixProps = Omit<HTMLAttributes<HTMLDivElement>, 'prefix'>;
 
