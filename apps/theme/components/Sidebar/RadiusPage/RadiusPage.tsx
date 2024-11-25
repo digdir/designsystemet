45486550f1fa7cf485626cfb56ca7b7aec2ed564
Øyvind Thune <oyvlei@gmail.com>
2024-11-25 12:48:52 +0100

    Update RadiusPage.tsx

--- apps/theme/components/Sidebar/RadiusPage/RadiusPage.tsx
+++ apps/theme/components/Sidebar/RadiusPage/RadiusPage.tsx
@@ -1,8 +1,6 @@
 import { Button, Heading, Paragraph } from '@digdir/designsystemet-react';
 import { ChevronLeftIcon, SparklesIcon } from '@navikt/aksel-icons';
 import NextLink from 'next/link';
-import { useState } from 'react';
-import { useThemeStore } from '../../../store';
 import { BorderRadiusInput } from '../../BorderRadiusInput/BorderRadiusInput';
 import classes from './RadiusPage.module.css';
 
@@ -12,9 +10,6 @@ type RadiusPageProps = {
 };
 
 export const RadiusPage = ({ onPrevClick, onNextClick }: RadiusPageProps) => {
-  const borderRadius = useThemeStore((state) => state.borderRadius);
-  const setBorderRadius = useThemeStore((state) => state.setBorderRadius);
-  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div>
       <Heading data-size='xs'>Velg border radius</Heading>
@@ -22,9 +17,9 @@ export const RadiusPage = ({ onPrevClick, onNextClick }: RadiusPageProps) => {
         Logbook a sitting success parents' girl in it however, greater, full
         with he that pleasures up attention to hardly to power definitely hardly
       </Paragraph>
-      {/* BORDER RADIUS */}
 
-      <BorderRadiusInput onChange={(radius) => setBorderRadius(radius)} />
+      {/* BORDER RADIUS */}
+      <BorderRadiusInput />
 
       <div className={classes.bottom}>
         <div className={classes.btnGroup}>
