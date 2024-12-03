685522359f7fa057c38f34b14ae30385fbbcfeb5
eirikbacker <eirik.backer@mattilsynet.no>
2024-12-03 13:25:38 +0100

    fix(ValidationMessage): use data-color instead of data-error

--- packages/react/src/components/ValidationMessage/ValidationMessage.tsx
+++ packages/react/src/components/ValidationMessage/ValidationMessage.tsx
@@ -8,8 +8,11 @@ import type { MergeRight } from '../../utilities';
 export type ValidationMessageProps = MergeRight<
   Omit<DefaultProps, 'data-color'> & HTMLAttributes<HTMLParagraphElement>,
   {
-    /** Toggle error color */
-    error?: boolean;
+    /**
+     * Sets color and icon.
+     * @default 'danger'
+     */
+    'data-color'?: 'danger' | 'success';
     /**
      * Change the default rendered element for the one passed as a child, merging their props and behavior.
      * @default false
@@ -22,16 +25,12 @@ export type ValidationMessageProps = MergeRight<
 export const ValidationMessage = forwardRef<
   HTMLParagraphElement,
   ValidationMessageProps
->(function ValidationMessage(
-  { className, asChild, error = true, ...rest },
-  ref,
-) {
+>(function ValidationMessage({ className, asChild, ...rest }, ref) {
   const Component = asChild ? Slot : 'div';
 
   return (
     <Component
       className={cl('ds-validation-message', className)}
-      data-error={error || undefined}
       data-field='validation'
       ref={ref}
       {...rest}
