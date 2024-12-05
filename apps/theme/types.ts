d1d02ca964957aaddb26970a0ca5124a85a93c11
Øyvind Thune <oyvlei@gmail.com>
2024-12-05 15:12:00 +0100

    Fix fix fix

--- apps/theme/types.ts
+++ apps/theme/types.ts
@@ -1,4 +1,4 @@
-import type { CssColor } from '@adobe/leonardo-contrast-colors';
+import type { CssColor } from '@/packages/cli/dist/src';
 
 export type modeType = 'light' | 'dark' | 'contrast';
 export type ThemeColors = 'accent' | 'neutral' | 'brand1' | 'brand2' | 'brand3';
