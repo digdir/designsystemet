730922c374f5157e90b01d91dbb37c54555ebf2a
Barsnes <tobias.barsnes@digdir.no>
2024-12-09 13:24:24 +0100

    add neutral

--- packages/react/src/components/ValidationMessage/ValidationMessage.tsx
+++ packages/react/src/components/ValidationMessage/ValidationMessage.tsx
@@ -13,7 +13,7 @@ export type ValidationMessageProps = MergeRight<
      * Sets color and icon.
      * @default 'danger'
      */
-    'data-color'?: SeverityColors;
+    'data-color'?: SeverityColors | 'neutral';
     /**
      * Change the default rendered element for the one passed as a child, merging their props and behavior.
      * @default false
