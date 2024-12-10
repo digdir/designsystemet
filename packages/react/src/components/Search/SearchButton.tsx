586aea12c440b6061605ffca4697492e329bdb55
Tobias Barsnes <tobias.barsnes@digdir.no>
2024-12-10 15:41:53 +0100

    chore: flatten components folder (#2874)
    Flattens `form` and `loaders` folders
    
    ---------
    
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

---
+++ packages/react/src/components/Search/SearchButton.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from 'react';
+
+import type { MergeRight } from '../../utilities';
+import { Button, type ButtonProps } from '../Button';
+
+export type SearchButtonProps = MergeRight<
+  ButtonProps,
+  {
+    variant?: 'primary' | 'secondary';
+    /**
+     * Children of the button
+     * @default 'Søk'
+     */
+    children?: React.ReactNode;
+  }
+>;
+
+export const SearchButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  function SearchButton({ children = 'Søk', ...rest }, ref) {
+    return (
+      <Button ref={ref} type='submit' {...rest}>
+        {children}
+      </Button>
+    );
+  },
+);
