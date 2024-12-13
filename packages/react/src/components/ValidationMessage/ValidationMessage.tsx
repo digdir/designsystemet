b81a259f9337dcc42f4b4f8d429bdb0a5e22c765
Michael Marszalek <mimarz@gmail.com>
2024-12-13 14:08:13 +0100

    remove neutral color on validationmessage

--- packages/react/src/components/ValidationMessage/ValidationMessage.tsx
+++ packages/react/src/components/ValidationMessage/ValidationMessage.tsx
@@ -13,7 +13,7 @@ export type ValidationMessageProps = MergeRight<
      * Sets color and icon.
      * @default 'danger'
      */
-    'data-color'?: SeverityColors | 'neutral';
+    'data-color'?: SeverityColors;
     /**
      * Change the default rendered element for the one passed as a child, merging their props and behavior.
      * @default false
