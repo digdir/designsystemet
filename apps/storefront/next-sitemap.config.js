e347e2ef61425ff2c2ac8766244b6457955df563
Simon Archer Dreyer <simonnline@gmail.com>
2024-12-17 11:55:42 +0100

    feat(seo): add sitemap.xml and robots.txt to storefron using next-sitemap

---
+++ apps/storefront/next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://designsystemet.no',
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+      policies: [
+        { userAgent: '*', allow: '/' },
+      ],
+    },
+  };
