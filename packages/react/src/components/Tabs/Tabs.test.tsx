9fd9cfa805ea0ef933753aad85f17217b31dfd83
Barsnes <tobias.barsnes@digdir.no>
2025-01-03 15:39:22 +0100

    rewrite test to new format

--- packages/react/src/components/Tabs/Tabs.test.tsx
+++ packages/react/src/components/Tabs/Tabs.test.tsx
@@ -1,27 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { act, render as renderRtl, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Tabs } from '.';
+import { Tabs, type TabsProps } from '.';
 
-const user = userEvent.setup();
+const render = async (props: Partial<TabsProps> = {}) => {
+  /* Flush microtasks */
+  await act(async () => {});
+  const user = userEvent.setup();
 
-describe('Tabs', () => {
-  /* TODO: Fix this failing test */
-  /* it('can navigate tabs with keyboard', async () => {
-    render(
-      <Tabs>
+  return {
+    user,
+    ...renderRtl(
+      <Tabs {...props}>
         <Tabs.List>
           <Tabs.Tab data-testid='tab1'>Tab 1</Tabs.Tab>
           <Tabs.Tab data-testid='tab2'>Tab 2</Tabs.Tab>
         </Tabs.List>
-        <Tabs.Panel>content 1</Tabs.Panel>
-        <Tabs.Panel>content 2</Tabs.Panel>
+        <Tabs.Panel data-testid='panel1'>content 1</Tabs.Panel>
+        <Tabs.Panel data-testid='panel2'>content 2</Tabs.Panel>
       </Tabs>,
-    );
+    ),
+  };
+};
+
+describe('Tabs', () => {
+  /* TODO: Fix this failing test */
+  /* it('can navigate tabs with keyboard', async () => {
+    const { user } = await render();
 
     const tab1 = screen.getByTestId('tab1');
     const tab2 = screen.getByTestId('tab2');
-    await user.tab();
+    await act(async () => await user.tab());
     expect(tab1).toHaveFocus();
     await act(async () => await user.keyboard('[ArrowRight]'));
     expect(tab2).toHaveFocus();
@@ -30,16 +39,7 @@ describe('Tabs', () => {
   }); */
 
   it('renders content based on value', async () => {
-    render(
-      <Tabs defaultValue='value1'>
-        <Tabs.List>
-          <Tabs.Tab>Tab 1</Tabs.Tab>
-          <Tabs.Tab>Tab 2</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel>content 1</Tabs.Panel>
-        <Tabs.Panel>content 2</Tabs.Panel>
-      </Tabs>,
-    );
+    const { user } = await render();
 
     expect(screen.queryByText('content 1')).toBeVisible();
     expect(screen.queryByText('content 2')).toHaveAttribute('hidden');
@@ -49,14 +49,7 @@ describe('Tabs', () => {
   });
 
   it('item renders with correct aria attributes', async () => {
-    render(
-      <Tabs selectedIndex={0}>
-        <Tabs.List>
-          <Tabs.Tab>Tab 1</Tabs.Tab>
-          <Tabs.Tab>Tab 2</Tabs.Tab>
-        </Tabs.List>
-      </Tabs>,
-    );
+    const { user } = await render();
 
     const tab = screen.getByRole('tab', { name: 'Tab 2' });
     expect(tab).toHaveAttribute('aria-selected', 'false');
@@ -64,32 +57,11 @@ describe('Tabs', () => {
     expect(tab).toHaveAttribute('aria-selected', 'true');
   });
 
-  it('renders ReactNodes as children when TabsPanels value is selected', () => {
-    render(
-      <Tabs selectedIndex={0}>
-        <Tabs.List>
-          <Tabs.Tab>Tab 1</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel>
-          <div>content 1</div>
-        </Tabs.Panel>
-      </Tabs>,
-    );
-
-    const content = screen.queryByText('content 1');
-    expect(content).toBeInTheDocument();
-  });
-
   it('calls onChange when tab is clicked', async () => {
     const onChange = vi.fn();
-    render(
-      <Tabs onChange={onChange}>
-        <Tabs.List>
-          <Tabs.Tab>Tab 1</Tabs.Tab>
-          <Tabs.Tab>Tab 2</Tabs.Tab>
-        </Tabs.List>
-      </Tabs>,
-    );
+    const { user } = await render({
+      onChange,
+    });
 
     await user.click(screen.getByRole('tab', { name: 'Tab 2' }));
     expect(onChange).toHaveBeenCalledWith(1);
