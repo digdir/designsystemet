08fed133ee7ab1b1284920639c763f9626b0db84
Barsnes <tobias.barsnes@digdir.no>
2025-01-08 08:55:31 +0100

    fix: export utilities

--- packages/react/src/utilities/hooks/index.ts
+++ packages/react/src/utilities/hooks/index.ts
@@ -1,10 +1,13 @@
+export { useCheckboxGroup } from './useCheckboxGroup/useCheckboxGroup';
 export { useDebounceCallback } from './useDebounceCallback/useDebounceCallback';
+export { useFormField } from './useFormField/useFormField';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect/useIsomorphicLayoutEffect';
 export { useMediaQuery } from './useMediaQuery/useMediaQuery';
-export { useSynchronizedAnimation } from './useSynchronizedAnimation/useSynchronizedAnimation';
 export { usePrevious } from './usePrevious/usePrevious';
 export { useRadioGroup } from './useRadioGroup/useRadioGroup';
-export { useCheckboxGroup } from './useCheckboxGroup/useCheckboxGroup';
+export { useSynchronizedAnimation } from './useSynchronizedAnimation/useSynchronizedAnimation';
 
 export type { UseRadioGroupProps } from './useRadioGroup/useRadioGroup';
 export type { UseCheckboxGroupProps } from './useCheckboxGroup/useCheckboxGroup';
+export type { FormFieldProps } from './useFormField/useFormField';
+export type { FormField } from './useFormField/useFormField';
