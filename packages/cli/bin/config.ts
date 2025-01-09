c0ad22845cd07cf2530afc6d0525a98f40ab70fe
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-01-09 14:27:26 +0100

    feat(cli): add JSON schema for config file

--- packages/cli/bin/config.ts
+++ packages/cli/bin/config.ts
@@ -13,24 +13,52 @@ export function mapPathToOptionName(path: (string | number)[]) {
   return option;
 }
 
-const themeSchema = z.object({
-  colors: z.object({
-    main: z.record(z.string().transform(convertToHex)),
-    support: z.record(z.string().transform(convertToHex)),
-    neutral: z.string().transform(convertToHex),
-  }),
-  typography: z.object({
-    fontFamily: z.string(),
-  }),
-  borderRadius: z.number(),
-});
+const hexPatterns = [
+  // Hex colors: #000, #0000, #000000, #00000000
+  `#[0-9a-fA-F]{3}`,
+  `#[0-9a-fA-F]{4}`,
+  `#[0-9a-fA-F]{6}`,
+  `#[0-9a-fA-F]{8}`,
+];
+
+export const colorRegex = new RegExp(`^${hexPatterns.join('|')}$`);
+
+const colorSchema = z
+  .string({ description: 'A hex color, which is used for creating a color scale' })
+  .regex(colorRegex)
+  .transform(convertToHex);
+const colorCategorySchema = z.record(colorSchema, { description: 'One or more color definitions' });
+
+const themeSchema = z.object(
+  {
+    colors: z.object(
+      {
+        main: colorCategorySchema,
+        support: colorCategorySchema,
+        neutral: colorSchema,
+      },
+      { description: 'Defines the colors for this theme' },
+    ),
+    typography: z.object(
+      {
+        fontFamily: z.string({ description: 'Sets the font-family for this theme' }),
+      },
+      { description: 'Defines the typography for a given theme' },
+    ),
+    borderRadius: z.number({ description: 'Defines the border-radius for this theme' }),
+  },
+  { description: 'An object defining a theme. The property name holding the object becomes the theme name.' },
+);
 
 /**
  * This defines the structure of the JSON config file
  */
 export const configFileSchema = z.object({
-  outDir: z.string().optional(),
-  themes: z.record(themeSchema),
+  outDir: z.string({ description: 'Path to the output directory for the created design tokens' }).optional(),
+  themes: z.record(themeSchema, {
+    description:
+      'An object with one or more themes. Each property defines a theme, and the property name is used as the theme name.',
+  }),
 });
 
 /**
