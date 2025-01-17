fff1a12b9013dd64f9107661da3f613392a6aa6b
Michael Marszalek <mimarz@gmail.com>
2025-01-17 12:01:09 +0100

    fix: :bug: Use correct popover api attributes based on React version
    Fixes the errors flagged by nextjs of "invalid" react props

--- packages/react/src/components/Popover/PopoverTrigger.tsx
+++ packages/react/src/components/Popover/PopoverTrigger.tsx
@@ -1,5 +1,5 @@
 import { Slot } from '@radix-ui/react-slot';
-import { type HTMLAttributes, forwardRef, useContext } from 'react';
+import { type HTMLAttributes, forwardRef, useContext, version } from 'react';
 import type { DefaultProps } from '../../types';
 import { Button, type ButtonProps } from '../Button/Button';
 import { Context } from './PopoverTriggerContext';
@@ -21,16 +21,19 @@ export const PopoverTrigger = forwardRef<
   const { popoverId } = useContext(Context);
   const Component = asChild ? Slot : inline ? 'button' : Button;
 
-  return (
-    <Component
-      ref={ref}
-      popovertarget={popoverId}
-      {...(inline
+  console.log('version', version);
+
+  const popoverProps = Object.assign(
+    {
+      [version.startsWith('19') ? 'popoverTarget' : 'popovertarget']: popoverId,
+      ...(inline
         ? {
             'data-popover': 'inline',
           }
-        : {})}
-      {...rest}
-    />
+        : {}),
+    },
+    rest,
   );
+
+  return <Component ref={ref} {...popoverProps} />;
 });
