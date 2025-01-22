b698a36eb34a345125658feee3354b8780391e15
Tobias Barsnes <tobias.barsnes@digdir.no>
2025-01-22 13:35:02 +0100

    feat(ErrorSummary): split `.Item` into two components, add `asChild` support (#3040)

--- packages/react/src/components/ErrorSummary/ErrorSummaryItem.tsx
+++ packages/react/src/components/ErrorSummary/ErrorSummaryItem.tsx
@@ -1,42 +1,11 @@
 import { forwardRef } from 'react';
-import type { LinkProps } from '../Link';
-import { Link } from '../Link';
-import type { ListItemProps } from '../List';
-import { List } from '../List';
+import { ListItem, type ListItemProps } from '../List';
 
-type RequiredHref = {
-  href: LinkProps['href'];
-  /**
-   * Change the default rendered element for the one passed as a child, merging their props and behavior.
-   * @default false
-   */
-  asChild?: false;
-};
-
-type OptionalHref = {
-  href?: LinkProps['href'];
-  /**
-   * Change the default rendered element for the one passed as a child, merging their props and behavior.
-   * @default false
-   */
-  asChild: true;
-};
-
-export type ErrorSummaryItemProps = (RequiredHref | OptionalHref) &
-  Omit<ListItemProps, 'asChild' | 'ref'>;
+export type ErrorSummaryItemProps = ListItemProps;
 
 export const ErrorSummaryItem = forwardRef<
   HTMLLIElement,
   ErrorSummaryItemProps
->(function ErrorSummaryItem(
-  { href, asChild, children, ...rest }: ErrorSummaryItemProps,
-  ref,
-) {
-  return (
-    <List.Item {...rest} ref={ref}>
-      <Link href={href} asChild={asChild}>
-        {children}
-      </Link>
-    </List.Item>
-  );
+>(function ErrorSummaryItem({ ...rest }: ErrorSummaryItemProps, ref) {
+  return <ListItem ref={ref} {...rest} />;
 });
