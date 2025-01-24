4417f96152c2b29d3628e3a9b45f1790ce30c7d1
Barsnes <tobias.barsnes@digdir.no>
2025-01-24 09:49:49 +0100

    re-export stories

--- packages/react/src/components/Input/Input.forced-colors.stories.tsx
+++ packages/react/src/components/Input/Input.forced-colors.stories.tsx
@@ -9,3 +9,9 @@ export default {
     },
   },
 };
+
+export const Preview = stories.Preview;
+export const Text = stories.Text;
+export const Checkbox = stories.Checkbox;
+export const Switch = stories.Switch;
+export const Radio = stories.Radio;
