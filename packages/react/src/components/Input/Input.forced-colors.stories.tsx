30000138a5b53b829e635c8057918968299df7c2
Barsnes <tobias.barsnes@digdir.no>
2025-01-24 09:42:09 +0100

    forces colors new name

--- packages/react/src/components/Input/Input.forced-colors.stories.tsx
+++ packages/react/src/components/Input/Input.forced-colors.stories.tsx
@@ -2,6 +2,7 @@ import * as stories from './Input.stories';
 
 export default {
   ...stories,
+  title: 'Komponenter/Input/Forced Colors',
   parameters: {
     chromatic: {
       forcedColors: 'active',
