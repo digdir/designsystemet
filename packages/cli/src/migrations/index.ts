1527fa46ff5cbb1e2592b410c38a39cb68c66b77
Michael Marszalek <mimarz@gmail.com>
2025-01-27 09:37:54 +0100

    renamed migration command

--- packages/cli/src/migrations/index.ts
+++ packages/cli/src/migrations/index.ts
@@ -1,9 +1,9 @@
 import betaToV1 from './beta-to-v1.js';
-import colorTokensRename from './color-tokens-rename.js';
+import colorRenameNext48 from './color-rename-next48.js';
 import reactBetaToV1 from './react-beta-to-v1.js';
 
 export default {
   'css-beta-to-v1': betaToV1,
   'react-beta-to-v1': reactBetaToV1,
-  'color-tokens-rename': colorTokensRename,
+  'color-rename-next48': colorRenameNext48,
 };
