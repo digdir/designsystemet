9c93fe3b3357979fabac4a476a8cd2db2f538859
Michael Marszalek <mimarz@gmail.com>
2025-01-27 10:23:07 +0100

    move to separate release

--- .changeset/hot-chairs-drop.md
+++ .changeset/hot-chairs-drop.md
@@ -2,4 +2,4 @@
 "@digdir/designsystemet": patch
 ---
 
-Added migration for color tokens renaming. Run `designsystemet migrate css-renames-next47-to-next48 --glob "./**/*.css"`
+Added migration for color tokens renaming. Run `designsystemet migrate css-renames-next48-to-next49 --glob "./**/*.css"`
--- packages/cli/src/migrations/index.ts
+++ packages/cli/src/migrations/index.ts
@@ -1,9 +1,9 @@
 import betaToV1 from './beta-to-v1.js';
-import colorRenameNext48 from './color-rename-next48.js';
+import colorRenameNext49 from './color-rename-next49.js';
 import reactBetaToV1 from './react-beta-to-v1.js';
 
 export default {
   'css-beta-to-v1': betaToV1,
   'react-beta-to-v1': reactBetaToV1,
-  'css-renames-next47-to-next48': colorRenameNext48,
+  'css-renames-next48-to-next49': colorRenameNext49,
 };
