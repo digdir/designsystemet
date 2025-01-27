eed1962ff133d25362e84ade1a6b72b6e4e15c33
Michael Marszalek <mimarz@gmail.com>
2025-01-27 09:40:06 +0100

    change migration name again

--- packages/cli/src/migrations/index.ts
+++ packages/cli/src/migrations/index.ts
@@ -5,5 +5,5 @@ import reactBetaToV1 from './react-beta-to-v1.js';
 export default {
   'css-beta-to-v1': betaToV1,
   'react-beta-to-v1': reactBetaToV1,
-  'color-rename-next48': colorRenameNext48,
+  'css-renames-next47-to-next48': colorRenameNext48,
 };
