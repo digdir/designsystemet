391bbb5c4d0003eecf8e04789d73e8736c9ca72c
Michael Marszalek <mimarz@gmail.com>
2025-01-27 08:09:30 +0100

    export migration

--- packages/cli/src/migrations/index.ts
+++ packages/cli/src/migrations/index.ts
@@ -1,7 +1,9 @@
 import betaToV1 from './beta-to-v1.js';
+import colorTokensRename from './color-tokens-rename.js';
 import reactBetaToV1 from './react-beta-to-v1.js';
 
 export default {
   'css-beta-to-v1': betaToV1,
   'react-beta-to-v1': reactBetaToV1,
+  'color-tokens-rename': colorTokensRename,
 };
