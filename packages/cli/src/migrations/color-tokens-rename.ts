4402b5590f24bcc4fb0c81b739f23d7fbde7aed9
Michael Marszalek <mimarz@gmail.com>
2025-01-27 08:59:24 +0100

    update replacer

--- packages/cli/src/migrations/color-tokens-rename.ts
+++ packages/cli/src/migrations/color-tokens-rename.ts
@@ -1,23 +1,29 @@
+import * as R from 'ramda';
 import { cssVarRename } from './codemods/css/plugins.js';
 import { runCssCodemod } from './codemods/css/run.js';
 
 /**
- * Replaces placeholders %color in the given templates with the provided colors.
+ * Replaces placeholders [color] in the given templates with the provided colors.
  *
  * @param {string} oldTemplate - The template string containing the placeholder to be replaced.
  * @param {string} newTemplate - The template string containing the placeholder to be replaced.
  * @param {string[]} colors - An array of color strings to replace the placeholder in the templates.
  * @returns {Record<string, string>} An object mapping the old template with replaced colors to the new template with replaced colors.
  */
-const templateReplace = (oldTemplate: string, newTemplate: string, colors: string[]): Record<string, string> => {
-  return colors.reduce(
+const replace = (
+  oldTemplate: string,
+  newTemplate: string,
+  colors: string[],
+  placeholder = '[color]',
+): Record<string, string> =>
+  R.reduce(
     (acc: Record<string, string>, color) => {
-      acc[oldTemplate.replace('%color', color)] = newTemplate.replace('%color', color);
+      acc[oldTemplate.replace(placeholder, color)] = newTemplate.replace(placeholder, color);
       return acc;
     },
     {} as Record<string, string>,
+    colors,
   );
-};
 
 const colors = ['neutral', 'accent', 'brand1', 'brand2', 'brand3', 'danger', 'warning', 'success', 'info'];
 
@@ -29,15 +35,15 @@ export default (glob?: string) =>
       cssVarRename({
         // Background
         '--ds-color-background-subtle': '--ds-color-background-tinted',
-        ...templateReplace('--ds-color-%color-background-subtle', '--ds-color-%color-background-tinted', colors),
+        ...replace('--ds-color-[color]-background-subtle', '--ds-color-[color]-background-tinted', colors),
         // Surface
         '--ds-color-surface-default': '--ds-color-surface-tinted',
-        ...templateReplace('--ds-color-%color-surface-default', '--ds-color-%color-surface-tinted', colors),
+        ...replace('--ds-color-[color]-surface-default', '--ds-color-[color]-surface-tinted', colors),
         // Contrast
         '--ds-color-contrast-default': '--ds-color-base-contrast-default',
         '--ds-color-contrast-subtle': '--ds-color-base-contrast-subtle',
-        ...templateReplace('--ds-color-%color-contrast-default', '--ds-color-%color-base-contrast-default', colors),
-        ...templateReplace('--ds-color-%color-contrast-subtle', '--ds-color-%color-base-contrast-subtle', colors),
+        ...replace('--ds-color-[color]-contrast-default', '--ds-color-[color]-base-contrast-default', colors),
+        ...replace('--ds-color-[color]-contrast-subtle', '--ds-color-[color]-base-contrast-subtle', colors),
       }),
     ],
   });
