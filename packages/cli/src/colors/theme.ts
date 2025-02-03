3743e6eee90bf1c90e1267f9c9af4b1037a8ac70
Michael Marszalek <mimarz@gmail.com>
2025-02-03 16:04:45 +0100

    new orange base color

--- packages/cli/src/colors/theme.ts
+++ packages/cli/src/colors/theme.ts
@@ -8,7 +8,7 @@ import { getColorInfoFromPosition, getLightnessFromHex, getLuminanceFromLightnes
 export const baseColors: Record<GlobalColors, CssColor> = {
   blue: '#0A71C0',
   green: '#068718',
-  orange: '#B8581D',
+  orange: '#EA9B1B',
   purple: '#663299',
   red: '#C01B1B',
 };
