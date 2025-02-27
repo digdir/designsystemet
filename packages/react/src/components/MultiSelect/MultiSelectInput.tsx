f07b1d6df00c8b9c2f0de6374f89263902a33b75
Barsnes <tobias.barsnes@digdir.no>
2025-02-27 12:18:20 +0100

    fix selected option not being shown

--- packages/react/src/components/MultiSelect/MultiSelectInput.tsx
+++ packages/react/src/components/MultiSelect/MultiSelectInput.tsx
@@ -1,6 +1,6 @@
 import { useMergeRefs } from '@floating-ui/react';
-import { getDatalistValue, isDatalistClick } from '@u-elements/u-datalist';
-import { forwardRef, useCallback, useContext, useEffect } from 'react';
+import { isDatalistClick } from '@u-elements/u-datalist';
+import { forwardRef, useContext, useEffect } from 'react';
 import { Input, type InputProps } from '../Input';
 import { MultiSelectContext } from './MultiSelect';
 
@@ -23,16 +23,9 @@ export const MultiSelectInput = forwardRef<
 >(function MultiSelectList({ value, onInput, ...rest }, ref) {
   const { listId, inputRef, handleFilter } = useContext(MultiSelectContext);
   const mergedRefs = useMergeRefs([inputRef, ref]);
-  const updateSelected = useCallback(() => {
-    const { list, value } = inputRef?.current || {};
-    for (const option of list?.options || []) {
-      option.selected = getDatalistValue(option) === value;
-    }
-  }, []);
 
   // Update also if controlled value
   useEffect(() => {
-    updateSelected();
     handleFilter?.(inputRef?.current);
   }, [value]);
 
@@ -43,7 +36,6 @@ export const MultiSelectInput = forwardRef<
       value={value}
       onInput={(event) => {
         onInput?.(event); // Should run first
-        updateSelected();
 
         if (!isDatalistClick(event.nativeEvent as InputEvent))
           handleFilter?.(inputRef?.current);
