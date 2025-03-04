a6c579f29f85411ec828b12718f627bb774a0abd
Michael Marszalek <mimarz@gmail.com>
2025-03-04 12:53:42 +0100

    load env from file

--- apps/theme/next.config.mjs
+++ apps/theme/next.config.mjs
@@ -1,3 +1,10 @@
+import path from 'node:path';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), 'env.local'),
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
