93e13041c0799e152d0b2c48dbb1a077bddb67b1
Tobias Barsnes <tobiasbarsnes@gmail.com>
2025-03-06 08:10:26 +0100

    docs(apps): create new `CodeBlock`

---
+++ apps/_components/src/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { Button } from '@digdir/designsystemet-react';
+import cl from 'clsx/lite';
+import { useState } from 'react';
+import { CodeBlock as ReactCodeBlock } from 'react-code-block';
+import classes from './CodeBlock.module.css';
+
+export type CodeBlockProps = {
+  children: string;
+  className?: string;
+  language?: string;
+};
+
+export const CodeBlock = ({
+  children,
+  className,
+  language = 'text',
+}: CodeBlockProps) => {
+  return (
+    <ReactCodeBlock code={children} language={language}>
+      <div className={classes.codeBlockWrapper}>
+        <ReactCodeBlock.Code
+          data-color-scheme='dark'
+          className={cl(classes.codeBlock, className)}
+        >
+          <ReactCodeBlock.LineContent>
+            <ReactCodeBlock.Token />
+          </ReactCodeBlock.LineContent>
+        </ReactCodeBlock.Code>
+        <div data-color-scheme='dark' className={classes.toolbar}>
+          <CopyButton text={children} />
+        </div>
+      </div>
+    </ReactCodeBlock>
+  );
+};
+
+const CopyButton = ({ text }: { text: string }) => {
+  const [toolTipText, setToolTipText] = useState('Kopier');
+
+  const onButtonClick = () => {
+    setToolTipText('Kopiert!');
+    navigator.clipboard.writeText(text).catch((reason) => {
+      throw Error(String(reason));
+    });
+  };
+  return (
+    <>
+      {/* @ts-ignore */}
+      <Button
+        onMouseEnter={() => setToolTipText('Kopier')}
+        onClick={() => onButtonClick()}
+        className={classes.copyButton}
+        aria-label='Kopier kodesnutt'
+        data-color='neutral'
+        data-size='2xs'
+      >
+        {toolTipText}
+      </Button>
+    </>
+  );
+};
