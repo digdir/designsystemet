727e106a8f776834fb0958bb0ae9a0b81b5e166e
Michael Marszalek <mimarz@gmail.com>
2025-03-11 15:20:19 +0100

    cleaning up some more

--- packages/cli/src/scripts/update-design-tokens.ts
+++ packages/cli/src/scripts/update-design-tokens.ts
@@ -6,33 +6,36 @@ const DIRNAME: string = import.meta.dirname || __dirname;
 const TARGET = path.join(DIRNAME, '../../../../design-tokens');
 const INTERNAL = path.join(DIRNAME, '../../internal/design-tokens');
 
-async function copyFiles() {
-  // Not copying typography due to unsupported secondary typography tokens in CLI
+const options = { recursive: true };
 
+async function copyFiles() {
   console.log('📁 Copying design tokens');
   await fs.cp(
     path.join(INTERNAL, 'primitives/modes/color-scheme'),
     path.join(TARGET, 'primitives/modes/color-scheme'),
-    { recursive: true },
+    options,
   );
 
-  await fs.cp(path.join(INTERNAL, 'primitives/modes/size'), path.join(TARGET, 'primitives/modes/size'), {
-    recursive: true,
-  });
+  await fs.cp(path.join(INTERNAL, 'primitives/modes/size'), path.join(TARGET, 'primitives/modes/size'), options);
+
+  // Not copying secondary typography due to unsupported secondary typography tokens in CLI
+  await fs.cp(
+    path.join(INTERNAL, 'primitives/modes/typography/primary'),
+    path.join(TARGET, 'primitives/modes/typography/primary'),
+    options,
+  );
 
   await fs.cp(
     path.join(INTERNAL, 'primitives/modes/typography/size'),
     path.join(TARGET, 'primitives/modes/typography/size'),
-    {
-      recursive: true,
-    },
+    options,
   );
 
-  await fs.cp(path.join(INTERNAL, 'primitives/globals.json'), path.join(TARGET, 'primitives/globals.json'));
+  await fs.cp(path.join(INTERNAL, 'primitives/globals.json'), path.join(TARGET, 'primitives/globals.json'), options);
 
-  await fs.cp(path.join(INTERNAL, 'semantic'), path.join(TARGET, 'semantic'), { recursive: true });
+  await fs.cp(path.join(INTERNAL, 'semantic'), path.join(TARGET, 'semantic'), options);
 
-  await fs.cp(path.join(INTERNAL, 'themes'), path.join(TARGET, 'themes'), { recursive: true });
+  await fs.cp(path.join(INTERNAL, 'themes'), path.join(TARGET, 'themes'), options);
 
   console.log('✅ Finished copying design tokens');
 }
