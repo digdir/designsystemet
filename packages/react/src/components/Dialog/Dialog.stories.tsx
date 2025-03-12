c7992cc196c0ba71f7794c9f394bd718d9514697
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-03-12 14:22:33 +0100

    chore(apps): updates to disabled sb a11y tests (#3344)
    * storybook: re-enable aria-allowed-attr rule
    
    * updated comments

--- packages/react/src/components/Dialog/Dialog.stories.tsx
+++ packages/react/src/components/Dialog/Dialog.stories.tsx
@@ -299,15 +299,9 @@ export const DialogWithSuggestion: StoryFn<typeof Dialog> = () => {
 
 DialogWithSuggestion.parameters = {
   a11y: {
-    // TODO: these rules should be enabled after figuring out why they occur.
-    // for some reason it says `aria-expanded` is not allowed
+    // TODO: this rule should be enabled after https://github.com/dequelabs/axe-core/issues/4672 have propagated to @storybook/addon-a11y.
     config: {
       rules: [
-        {
-          id: 'aria-allowed-attr',
-          enabled: false,
-        },
-        /* It does not like role="combobox" either */
         {
           id: 'aria-allowed-role',
           enabled: false,
