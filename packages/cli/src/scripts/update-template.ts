8534dadec36458d2daf9243b492c46af91045c96
Michael Marszalek <mimarz@gmail.com>
2025-03-13 08:41:44 +0100

    change template naming

--- packages/cli/src/scripts/update-template.ts
+++ packages/cli/src/scripts/update-template.ts
@@ -44,7 +44,7 @@ export const updateTemplates = async () => {
   const typographyDir = path.join(TEMPLATE_FILES_PATH, 'primitives/modes/typography/');
   await mkdir(typographyDir);
   await writeFile(
-    path.join(typographyDir, 'typography-template.json'),
+    path.join(typographyDir, 'typography.template.json'),
     JSON.stringify(typographyTemplate, null, 2).replaceAll('digdir', '<theme>'),
   );
 
@@ -53,7 +53,7 @@ export const updateTemplates = async () => {
   const categoryDir = path.join(TEMPLATE_FILES_PATH, 'semantic/modes');
   await mkdir(categoryDir);
   await writeFile(
-    path.join(categoryDir, 'color-template.json'),
+    path.join(categoryDir, 'color.template.json'),
     JSON.stringify(categoryColorTemplate, null, 2).replaceAll('color.accent', 'color.<color>'),
   );
 
@@ -64,13 +64,13 @@ export const updateTemplates = async () => {
     color: R.omit(['accent', 'neutral', 'brand1', 'brand2', 'brand3'], semanticColorJson.color),
   };
   await writeFile(
-    path.join(TEMPLATE_FILES_PATH, `semantic/color-base-template.json`),
+    path.join(TEMPLATE_FILES_PATH, `semantic/color.base-template.json`),
     JSON.stringify(colorBaseFile, null, 2),
   );
 
   const semanticColorTemplate = semanticColorJson.color.accent;
   await writeFile(
-    path.join(TEMPLATE_FILES_PATH, `semantic/color-template.json`),
+    path.join(TEMPLATE_FILES_PATH, `semantic/color.template.json`),
     JSON.stringify(semanticColorTemplate, null, 2).replaceAll('color.accent', 'color.<color>'),
   );
 
@@ -82,13 +82,13 @@ export const updateTemplates = async () => {
 
   await mkdir(path.join(TEMPLATE_FILES_PATH, 'themes'));
   await writeFile(
-    path.join(TEMPLATE_FILES_PATH, `themes/theme-base-template.json`),
+    path.join(TEMPLATE_FILES_PATH, `themes/theme.base-template.json`),
     JSON.stringify(themeBaseFile, null, 2).replaceAll('digdir', '<theme>'),
   );
 
   const themeColorTemplate = digdirThemeJson.color.accent;
   await writeFile(
-    path.join(TEMPLATE_FILES_PATH, `themes/theme-template.json`),
+    path.join(TEMPLATE_FILES_PATH, `themes/theme.template.json`),
     JSON.stringify(themeColorTemplate, null, 2).replaceAll('digdir.accent', '<theme>.<color>'),
   );
 
