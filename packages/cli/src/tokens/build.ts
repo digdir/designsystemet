ab0e329da875ca2d996d18f0a4f60d8b8bcf3d7e
Michael Marszalek <mimarz@gmail.com>
2025-03-28 12:06:01 +0100

    refactored use of $themes and color debug

--- packages/cli/src/tokens/build.ts
+++ packages/cli/src/tokens/build.ts
@@ -1,10 +1,16 @@
+import path from 'node:path';
+import type { ThemeObject } from '@tokens-studio/types';
 import type { DesignToken } from 'style-dictionary/types';
+import { readFile } from '../utils.js';
 import { type BuildOptions, processPlatform } from './process/platform.js';
 
-export const buildTokens = async (options: Omit<BuildOptions, 'process'>) => {
+export const buildTokens = async (options: Omit<BuildOptions, 'process' | '$themes'>) => {
+  const $themes = JSON.parse(await readFile(path.resolve(`${options.tokensDir}/$themes.json`))) as ThemeObject[];
+
   const tokens = await processPlatform<DesignToken>({
-    process: 'build',
     ...options,
+    process: 'build',
+    $themes,
   });
 
   return tokens;
