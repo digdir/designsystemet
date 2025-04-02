a71a6236b24d9cda7d905773c5edccdb86430fa5
Michael Marszalek <mimarz@gmail.com>
2025-04-02 20:54:04 +0200

    cleaning up and added feat flag

--- packages/cli/src/tokens/write/generate$metadata.ts
+++ packages/cli/src/tokens/write/generate$metadata.ts
@@ -1,10 +1,10 @@
-import type { TokensSet } from '../types.js';
+import type { TokenSet } from '../types.js';
 
 type Metadata = {
   tokenSetOrder: string[];
 };
 
-export function generateMetadataJson(tokenSets: Map<string, TokensSet>): Metadata {
+export function generateMetadataJson(tokenSets: Map<string, TokenSet>): Metadata {
   return {
     tokenSetOrder: Array.from(tokenSets.keys()),
   };
