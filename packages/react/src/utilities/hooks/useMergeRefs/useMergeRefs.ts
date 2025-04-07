a2644b33c56d1250a9b1a8f5bbce8991ab900c2e
Michael Marszalek <mimarz@gmail.com>
2025-04-07 16:03:43 +0200

    updates

--- packages/react/src/utilities/hooks/useMergeRefs/useMergeRefs.ts
+++ packages/react/src/utilities/hooks/useMergeRefs/useMergeRefs.ts
@@ -1,9 +1,7 @@
 // https://github.com/floating-ui/floating-ui/blob/master/packages/react/src/hooks/useMergeRefs.ts
-import * as React from 'react';
 
 /**
  * Merges an array of refs into a single memoized callback ref or `null`.
- * @see https://floating-ui.com/docs/react-utils#usemergerefs
  */
 export function useMergeRefs<Instance>(
   refs: Array<React.Ref<Instance> | undefined>,
