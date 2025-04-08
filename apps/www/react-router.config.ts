0b146087dd80b7dc0e64984227e4039b2026cc92
Barsnes <tobias.barsnes@digdir.no>
2025-04-08 10:08:05 +0200

    remove vercel preset

--- apps/www/react-router.config.ts
+++ apps/www/react-router.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from '@react-router/dev/config';
-import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   ssr: true,
-  presets: [vercelPreset()],
+  presets: [
+    /* vercelPreset() */
+  ],
   buildDirectory: 'dist',
 } satisfies Config;
