4bcda1021e6fa67e33eed22bd4e8b7e4edd36b61
Barsnes <tobias.barsnes@digdir.no>
2025-04-08 11:05:12 +0200

    try dist dir with vercel preset

--- apps/www/react-router.config.ts
+++ apps/www/react-router.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   ssr: true,
-  presets: [
-    /* vercelPreset() */
-  ],
-  buildDirectory: 'build',
+  presets: [vercelPreset()],
+  buildDirectory: 'dist',
 } satisfies Config;
