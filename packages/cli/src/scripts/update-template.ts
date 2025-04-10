a498af1d3b600bdffe0500bc6ae639b0011c62d7
Michael Marszalek <mimarz@gmail.com>
2025-04-10 13:47:38 +0200

    removed unused template update code

--- packages/cli/src/scripts/update-template.ts
+++ packages/cli/src/scripts/update-template.ts
@@ -1,4 +1,3 @@
-import fs from 'node:fs/promises';
 import path from 'node:path';
 import chalk from 'chalk';
 import * as R from 'ramda';
@@ -14,21 +13,6 @@ const SOURCE_FILES_PATH = path.join(DIRNAME, '../../../../design-tokens');
 const TEMPLATE_FILES_PATH = path.join(DIRNAME, '../tokens/template/design-tokens');
 
 const designTokensPath = (_path: string) => path.join(DIRNAME, '../../../../design-tokens', _path);
-
-const convertJsonToEsm = async (sourceFilePath: string, outputFilePath: string) => {
-  try {
-    const jsonContent = await fs.readFile(sourceFilePath, 'utf-8');
-    const jsonObject = JSON.parse(jsonContent);
-
-    const esmContent = `export default ${JSON.stringify(jsonObject, null, 2)};`;
-
-    await fs.writeFile(outputFilePath, esmContent, 'utf-8');
-    console.log(chalk.green(`Converted ${sourceFilePath} to ${outputFilePath}`));
-  } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : String(error);
-    console.error(chalk.red(`Failed to convert ${sourceFilePath}: ${errorMessage}`));
-  }
-};
 
 const argsFromToPaths = (path_: string): [string, string] => [
   path.join(SOURCE_FILES_PATH, path_),
@@ -54,7 +38,7 @@ export const updateTemplates = async () => {
    * Create template files
    */
 
-  // primitives/modes/typeography/<theme>.json
+  // primitives/modes/typography/<theme>.json
   const digdirTypographyJson = await readFile(designTokensPath('primitives/modes/typography/primary/digdir.json'));
 
   const typographyTemplate = R.set(
@@ -68,10 +52,6 @@ export const updateTemplates = async () => {
     path.join(typographyDir, 'typography.template.json'),
     JSON.stringify(typographyTemplate, null, 2).replaceAll('digdir', '<theme>'),
   );
-  // await writeFile(
-  //   path.join(typographyDir, 'typography.template.ts'),
-  //   `export default ${JSON.stringify(typographyTemplate, null, 2).replaceAll('digdir', '<theme>')};`,
-  // );
 
   // semantic/modes/<category>-color/<color>.json
   const categoryColorTemplate = accentColorCategoryJson.color.main;
@@ -117,90 +97,7 @@ export const updateTemplates = async () => {
     JSON.stringify(themeColorTemplate, null, 2).replaceAll('digdir.accent', '<theme>.<color>'),
   );
 
-  // WIP
-  // // $themes.json
-  // const themesFile = await readFile(path.join(SOURCE_FILES_PATH, '$themes.json'), 'utf-8');
-  // const themesTemplate = (JSON.parse(themesFile) as ThemeObject[])
-  //   .filter((themeobj) => {
-  //     // Remove compact size
-  //     if (R.toLower(themeobj.name) === 'compact' && R.toLower(themeobj.group || '') === 'size') {
-  //       return false;
-  //     }
-
-  //     // Pick theme to use as template
-  //     if (R.toLower(themeobj.name) !== 'digdir' && R.toLower(themeobj.group || '') === 'theme') {
-  //       return false;
-  //     }
-
-  //     return true;
-  //   })
-  //   .map((themeobj) => {
-  //     if (R.toLower(themeobj.name) === 'digdir') {
-  //       return {
-  //         ...themeobj,
-  //         name: '<theme>',
-  //         selectedTokenSets: {
-  //           'themes/<theme>': 'enabled',
-  //         },
-  //       };
-  //     }
-  //     return themeobj;
-  //   });
-
-  // await writeFile(path.join(TEMPLATE_FILES_PATH, `$themes.json`), stringify(themesTemplate));
-
-  // // $metadata.json
-  // const metadataFile = await readFile(path.join(SOURCE_FILES_PATH, '$metadata.json'), 'utf-8');
-  // const tokenSetOrderTemplate = (JSON.parse(metadataFile) as { tokenSetOrder: string[] }).tokenSetOrder
-  //   .filter((tokenSet) => {
-  //     if (endsWithOneOf(['altinn', 'portal', 'uutilsynet'], tokenSet)) {
-  //       return false;
-  //     }
-  //     return true;
-  //   })
-  //   .map((tokenSet) => {
-  //     if (endsWithOneOf(['digdir'], tokenSet)) {
-  //       return tokenSet.replace('/digdir', '/<theme>');
-  //     }
-  //     return tokenSet;
-  //   });
-
-  // await writeFile(
-  //   path.join(TEMPLATE_FILES_PATH, `$metadata.json`),
-  //   stringify({ tokenSetOrder: tokenSetOrderTemplate }),
-  // );
-
   console.log(chalk.green('Templates updated'));
 };
 
 await updateTemplates();
-
-// const convertAllJsonToEsm = async (sourceDir: string, outputDir: string) => {
-//   try {
-//     const files = await fs.readdir(sourceDir, { withFileTypes: true });
-
-//     for (const file of files) {
-//       const sourceFilePath = path.join(sourceDir, file.name);
-//       const outputFilePath = path.join(outputDir, file.name.replace('.json', '.js'));
-
-//       if (file.isDirectory()) {
-//         await fs.mkdir(outputFilePath, { recursive: true });
-//         await convertAllJsonToEsm(sourceFilePath, outputFilePath);
-//       } else if (file.isFile() && file.name.endsWith('.json')) {
-//         await convertJsonToEsm(sourceFilePath, outputFilePath);
-//       }
-//     }
-//   } catch (error) {
-//     console.error(chalk.red(`Failed to process directory ${sourceDir}: ${error.message}`));
-//   }
-// };
-
-// const main = async () => {
-//   await fs.mkdir(OUTPUT_DIR, { recursive: true });
-//   await convertAllJsonToEsm(SOURCE_DIR, OUTPUT_DIR);
-//   console.log(chalk.green('All JSON files have been converted to ESM modules.'));
-// };
-
-// main().catch((error) => {
-//   console.error(chalk.red(`Failed to convert JSON files to ESM modules: ${error.message}`));
-// });
