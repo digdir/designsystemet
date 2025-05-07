05bea6a1f398b768cc9cada0b6de00494059e420
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-05-07 13:55:02 +0200

    feat(themebuilder): improved color modal (#3512)
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>

--- apps/_components/src/ColorModal/ColorModal.tsx
+++ apps/_components/src/ColorModal/ColorModal.tsx
@@ -1,7 +1,19 @@
-import { Dialog, Heading } from '@digdir/designsystemet-react';
-import type { Color } from '@digdir/designsystemet/color';
-import { getCssVariable, hexToHsluv } from '@digdir/designsystemet/color';
+'use client';
+import {
+  Dialog,
+  Heading,
+  Paragraph,
+  Select,
+} from '@digdir/designsystemet-react';
+import { useState } from 'react';
 
+import type { Color } from '@digdir/designsystemet/color';
+import {
+  convertColor,
+  getLuminanceFromColor,
+} from '@digdir/designsystemet/color';
+import { getCssVariable } from '@digdir/designsystemet/color';
+import type { ChangeEvent } from 'react';
 import { ClipboardButton } from '../ClipboardButton/ClipboardButton';
 import classes from './ColorModal.module.css';
 import { capitalizeFirstLetter, getColorCombinations } from './colorModalUtils';
@@ -18,12 +30,20 @@ export const ColorModal = ({
   color,
 }: ColorModalProps) => {
   const { displayName, description, number, hex } = color;
+  const [convertedColor, setConvertedColor] = useState<string>(
+    convertColor(hex, 'oklch'),
+  );
+
+  const handleColorFormat = (event: ChangeEvent<HTMLSelectElement>) => {
+    setConvertedColor(convertColor(hex, event.target.value));
+  };
+
   return (
     <Dialog
       ref={colorModalRef}
       closedby='any'
       style={{
-        maxWidth: '1100px',
+        maxWidth: '650px',
       }}
     >
       <Dialog.Block>
@@ -31,45 +51,57 @@ export const ColorModal = ({
           {`${capitalizeFirstLetter(namespace)} ${displayName}`}
         </Heading>
       </Dialog.Block>
+      <div
+        className={classes.colorPreview}
+        style={{ backgroundColor: hex }}
+      ></div>
       <Dialog.Block className={classes.modalContent}>
-        <div className={classes.description}>{description.long}</div>
-        <div className={classes.container}>
-          <table className={classes.infoTable}>
-            <tbody>
-              <tr>
-                <th scope='row'>Hexkode:</th>
-                <td>{hex}</td>
-                <td>
-                  <ClipboardButton value={hex} />
-                </td>
-              </tr>
-              <tr>
-                <th scope='row'>HSLuv:</th>
-                <td colSpan={2}>
-                  {hexToHsluv(hex)[0].toFixed(0) +
-                    '° ' +
-                    hexToHsluv(hex)[1].toFixed(0) +
-                    '% ' +
-                    hexToHsluv(hex)[2].toFixed(0) +
-                    '%'}
-                </td>
-              </tr>
-              <tr>
-                <th scope='row'>CSS variabel:</th>
-                <td>{getCssVariable(namespace, number)}</td>
-                <td>
-                  <ClipboardButton value={getCssVariable(namespace, number)} />
-                </td>
-              </tr>
-              {number !== 9 && number !== 10 && number !== 11 && (
-                <tr>
-                  <th scope='row'>Brukes mot:</th>
-                  <td colSpan={2}>{getColorCombinations(number)}</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-          <div className={classes.right} style={{ backgroundColor: hex }}></div>
+        <Paragraph data-size='sm' className={classes.description}>
+          {description.long}
+        </Paragraph>
+        <div data-size='sm' className={classes.grid}>
+          <Paragraph className={classes.key}>Hexkode</Paragraph>
+          <Paragraph asChild className={classes.value}>
+            <div>
+              {hex} <ClipboardButton value={hex} />
+            </div>
+          </Paragraph>
+          <Select defaultValue='oklch' onChange={handleColorFormat}>
+            <Select.Option value='hct'>HCT</Select.Option>
+            <Select.Option value='hsluv'>HSLUV</Select.Option>
+            <hr />
+            <Select.Option value='hsl'>HSL</Select.Option>
+            <Select.Option value='hwb'>HWB</Select.Option>
+            <Select.Option value='oklch'>OKLCH</Select.Option>
+            <Select.Option value='p3'>P3</Select.Option>
+            <Select.Option value='rgb'>RGB</Select.Option>
+            <Select.Option value='xyz-d65'>XYZ-D65</Select.Option>
+          </Select>
+          <Paragraph asChild className={classes.value}>
+            <div>
+              {convertedColor}
+              <ClipboardButton value={convertedColor} />
+            </div>
+          </Paragraph>
+          <Paragraph className={classes.key}>CSS variabel</Paragraph>
+          <Paragraph asChild className={classes.value}>
+            <div>
+              {getCssVariable(namespace, number)}
+              <ClipboardButton value={getCssVariable(namespace, number)} />
+            </div>
+          </Paragraph>
+          <Paragraph className={classes.key}>Relativ luminans</Paragraph>
+          <Paragraph className={classes.value}>
+            {getLuminanceFromColor(hex).toFixed(3)}
+          </Paragraph>
+          {number !== 9 && number !== 10 && number !== 11 && (
+            <>
+              <Paragraph className={classes.key}>Kan brukes mot</Paragraph>
+              <Paragraph className={classes.value}>
+                {getColorCombinations(number)}
+              </Paragraph>
+            </>
+          )}
         </div>
       </Dialog.Block>
     </Dialog>
