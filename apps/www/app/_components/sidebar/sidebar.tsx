3fa72316798a2e2e6a4c0a2cebc99ab9baa399c2
Tobias Barsnes <tobias.barsnes@digdir.no>
2025-05-12 10:58:36 +0200

    chore: add Designsystemet video, sidebar for mobile fix (#3567)

--- apps/www/app/_components/sidebar/sidebar.tsx
+++ apps/www/app/_components/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
-import { Paragraph } from '@digdir/designsystemet-react';
+import { Button, Paragraph } from '@digdir/designsystemet-react';
 import cl from 'clsx/lite';
-import type { HTMLAttributes } from 'react';
+import { type HTMLAttributes, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router';
 import { RRLink } from '../link';
@@ -17,51 +17,67 @@ export type SidebarProps = {
 } & HTMLAttributes<HTMLDivElement>;
 
 export const Sidebar = ({ cats, title, ...props }: SidebarProps) => {
-  const location = useLocation().pathname;
+  const { pathname } = useLocation();
   const { t } = useTranslation();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <div {...props}>
-      <Paragraph data-size='md' asChild>
-        <h2 className={classes.title}>{t(`sidebar.${title}`, title)}</h2>
-      </Paragraph>
-      <ul className={classes.list}>
-        {Object.entries(cats).map(([key, value]) => {
-          if (!value.length) {
-            return null;
-          }
-          return (
-            <li key={key} className={classes.listGroup}>
-              <Paragraph asChild data-size='md'>
-                <div className={classes.innerTitle}>
-                  {t(`sidebar.categories.${key}`, key)}
-                </div>
-              </Paragraph>
-              <ul className={classes.innerList}>
-                {value.map((item) => {
-                  const url = `${item.url}`;
+      <Button
+        className={classes.toggleBtn}
+        data-size='md'
+        data-color='neutral'
+        variant='secondary'
+        onClick={() => setShowMenu(!showMenu)}
+        aria-expanded={showMenu}
+      >
+        {showMenu ? t('sidebar.hide') : t('sidebar.show')}{' '}
+        {t(`sidebar.sidebar`)}
+      </Button>
 
-                  return (
-                    <li key={item.url} className={classes.listItem}>
-                      <Paragraph asChild data-size='sm'>
-                        <RRLink
-                          to={url}
-                          className={cl(
-                            classes.link,
-                            url === location && classes.linkActive,
-                          )}
-                        >
-                          {t(`sidebar.items.${item.title}`, item.title)}
-                        </RRLink>
-                      </Paragraph>
-                    </li>
-                  );
-                })}
-              </ul>
-            </li>
-          );
-        })}
-      </ul>
+      <div className={cl(classes.menu, showMenu && classes.activeMenu)}>
+        <Paragraph data-size='md' asChild>
+          <h2 className={classes.title}>{t(`sidebar.${title}`, title)}</h2>
+        </Paragraph>
+        <ul className={classes.list}>
+          {Object.entries(cats).map(([key, value]) => {
+            if (!value.length) {
+              return null;
+            }
+            return (
+              <li key={key} className={classes.listGroup}>
+                <Paragraph asChild data-size='md'>
+                  <div className={classes.innerTitle}>
+                    {t(`sidebar.categories.${key}`, key)}
+                  </div>
+                </Paragraph>
+                <ul className={classes.innerList}>
+                  {value.map((item) => {
+                    const url = `${item.url}`;
+
+                    return (
+                      <li key={item.url} className={classes.listItem}>
+                        <Paragraph asChild data-size='sm'>
+                          <RRLink
+                            to={url}
+                            className={cl(
+                              classes.link,
+                              url === pathname && classes.linkActive,
+                            )}
+                            onClick={() => setShowMenu(false)}
+                          >
+                            {t(`sidebar.items.${item.title}`, item.title)}
+                          </RRLink>
+                        </Paragraph>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
