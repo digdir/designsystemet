40c4ad7305521c8266c164dc9306beba48fe7da3
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-05-14 15:03:39 +0200

    Details

--- packages/react/src/components/Details/Details.stories.tsx
+++ packages/react/src/components/Details/Details.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import { useState } from 'react';
+import { ChevronDownUpIcon, ChevronUpDownIcon } from '@navikt/aksel-icons';
 
 import { Button, Card, Details, Link, Paragraph } from '../';
 
@@ -86,18 +87,30 @@ export const InCardWithColor: StoryFn<typeof Details> = () => (
 
 export const Controlled: StoryFn<typeof Details> = () => {
   const [open1, setOpen1] = useState(false);
-  const [open2, setOpen2] = useState(false);
-  const [open3, setOpen3] = useState(false);
-  const toggleOpen = () => {
+    const [open2, setOpen2] = useState(false);
+    const [open3, setOpen3] = useState(false);
     const isOpen = [open1, open2, open3].every(Boolean);
-    setOpen1(!isOpen);
-    setOpen2(!isOpen);
-    setOpen3(!isOpen);
-  };
+    const toggleOpen = () => {
+      setOpen1(!isOpen);
+      setOpen2(!isOpen);
+      setOpen3(!isOpen);
+    };
 
   return (
     <>
-      <Button onClick={toggleOpen}>Toggle Details</Button>
+      <Button variant="tertiary" onClick={toggleOpen} data-size="sm">
+          {isOpen ? (
+            <>
+              <ChevronDownUpIcon aria-hidden />
+              Lukk alle
+            </>
+          ) : (
+            <>
+              <ChevronUpDownIcon aria-hidden />
+              Åpne alle
+            </>
+          )}
+        </Button>
       <br />
       <>
         <Details open={open1} onToggle={() => setOpen1(!open1)}>
