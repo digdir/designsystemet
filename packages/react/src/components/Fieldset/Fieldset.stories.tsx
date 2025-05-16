644f0de5a20e7a58bb40364c8ce2289401a089de
Barsnes <tobias.barsnes@digdir.no>
2025-05-16 15:15:06 +0200

    add fieldset stories

--- packages/react/src/components/Fieldset/Fieldset.stories.tsx
+++ packages/react/src/components/Fieldset/Fieldset.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryFn } from '@storybook/react';
 
-import { Field, Fieldset, Input, Label, Textarea } from '../..';
+import { Checkbox, Fieldset, Radio } from '../..';
 
 type Story = StoryFn<typeof Fieldset>;
 
@@ -12,18 +12,20 @@ export default {
 export const Preview: Story = (args) => (
   <form>
     <Fieldset {...args}>
-      <Fieldset.Legend>Skriv inn dine svar</Fieldset.Legend>
+      <Fieldset.Legend>Hvilken fjordarm bor du ved?</Fieldset.Legend>
       <Fieldset.Description>
-        Gi en kort beskrivelse i begge feltene
+        Valget vil hjelpe oss å forbedre innholdet vi viser deg.
       </Fieldset.Description>
-      <Field>
-        <Label>Kort beskrivelse</Label>
-        <Input />
-      </Field>
-      <Field>
-        <Label>Lang beskrivelse</Label>
-        <Textarea />
-      </Field>
+      <Radio label='Barsnesfjorden' />
+      <Radio label='Eidsfjorden' />
+      <Radio label='Ingen av de' />
     </Fieldset>
   </form>
+);
+
+export const WithCheckbox: Story = (args) => (
+  <Fieldset {...args}>
+    <Fieldset.Legend>Godtar du vilkårene?</Fieldset.Legend>
+    <Checkbox label='Ja, jeg godtar' />
+  </Fieldset>
 );
