879fcaf3d4c613191cfcccddf5f76f4cdd9a1226
Michael Marszalek <mimarz@gmail.com>
2025-05-20 10:58:05 +0200

    make designsystemet theme like digdir

--- packages/theme/src/themes/colors.d.ts
+++ packages/theme/src/themes/colors.d.ts
@@ -2,9 +2,9 @@ import type {} from '@digdir/designsystemet-react/colors';
 
 declare module '@digdir/designsystemet-react/colors' {
   export interface MainAndSupportColors {
-    primary: never;
     accent: never;
-    extra1: never;
-    extra2: never;
+    brand1: never;
+    brand2: never;
+    brand3: never;
   }
 }
