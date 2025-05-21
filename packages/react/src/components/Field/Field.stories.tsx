9f054da32c3f9d536d3165286ca7ebdf442802b5
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-05-21 13:04:05 +0200

    docs: improved guidelines for `Field` and `usePagination` (#3619)
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>
    Co-authored-by: Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>

--- packages/react/src/components/Field/Field.stories.tsx
+++ packages/react/src/components/Field/Field.stories.tsx
@@ -1,14 +1,7 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import { Label } from '../Label';
 
-import {
-  Field,
-  Fieldset,
-  Input,
-  Select,
-  Textarea,
-  ValidationMessage,
-} from '../';
+import { Divider, Field, Input, Textarea, ValidationMessage } from '../';
 
 type Story = StoryFn<typeof Field>;
 
@@ -25,86 +18,28 @@ export default {
 
 export const Preview: Story = () => {
   return (
-    <Field data-my-field>
-      <Label>Kort beskrivelse</Label>
-      <Field.Description>Beskrivelse</Field.Description>
-      <Input />
-      <ValidationMessage>Feilmelding</ValidationMessage>
+    <Field>
+      <Label>Etternavn</Label>
+      <Field.Description>
+        Etternavn kan ikke inneholde mellomrom
+      </Field.Description>
+      <Input defaultValue='Nordmann Svenske' />
+      <ValidationMessage>
+        Du kan ikke ha mellomrom i etternavnet ditt
+      </ValidationMessage>
     </Field>
   );
 };
 
 export const Affix: Story = () => (
-  <div>
-    <Field>
-      <Label>Hvor mange kroner koster det per måned?</Label>
-      <Field.Affixes>
-        <Field.Affix>NOK</Field.Affix>
-        <Input />
-        <Field.Affix>pr. mnd.</Field.Affix>
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>Hvor mange kilo veier eplene du har valgt?</Label>
-      <Field.Affixes>
-        <Textarea rows={2} cols={4} />
-        <Field.Affix>KG</Field.Affix>
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>Hvor mange kroner koster det?</Label>
-      <Field.Affixes>
-        <Field.Affix>NOK</Field.Affix>
-        <Select>
-          <Select.Option value='-1'>Velg &hellip;</Select.Option>
-          <Select.Option value='10'>10</Select.Option>
-          <Select.Option value='20'>20</Select.Option>
-          <Select.Option value='30'>30</Select.Option>
-        </Select>
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>No affix</Label>
-      <Field.Affixes>
-        <Input />
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>No affix and small size</Label>
-      <Field.Affixes>
-        <Input size={10} />
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>No affix and huge size</Label>
-      <Field.Affixes>
-        <Input size={9999} />
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>Affix and small size</Label>
-      <Field.Affixes>
-        <Field.Affix>NOK</Field.Affix>
-        <Input size={10} />
-        <Field.Affix>pr. mnd.</Field.Affix>
-      </Field.Affixes>
-    </Field>
-
-    <Field>
-      <Label>Affix and huge size</Label>
-      <Field.Affixes>
-        <Field.Affix>NOK</Field.Affix>
-        <Input size={50} />
-        <Field.Affix>pr. mnd.</Field.Affix>
-      </Field.Affixes>
-    </Field>
-  </div>
+  <Field>
+    <Label>Hvor mange kroner koster det per måned?</Label>
+    <Field.Affixes>
+      <Field.Affix>NOK</Field.Affix>
+      <Input />
+      <Field.Affix>pr. mnd.</Field.Affix>
+    </Field.Affixes>
+  </Field>
 );
 
 export const Counter: Story = () => (
@@ -117,36 +52,27 @@ export const Counter: Story = () => (
 
 export const Position: Story = () => (
   <>
-    <Fieldset>
-      <Fieldset.Legend>Fields with position="start"</Fieldset.Legend>
-      <Field position='start'>
-        <Label>Radio</Label>
-        <Input type='radio' />
-      </Field>
-      <Field position='start'>
-        <Label>Checkbox</Label>
-        <Input type='checkbox' />
-      </Field>
-      <Field position='start'>
-        <Label>Switch</Label>
-        <Input type='checkbox' role='switch' />
-      </Field>
-    </Fieldset>
-    <br />
-    <Fieldset>
-      <Fieldset.Legend>Fields with position="end"</Fieldset.Legend>
-      <Field position='end'>
-        <Label>Radio</Label>
-        <Input type='radio' />
-      </Field>
-      <Field position='end'>
-        <Label>Checkbox</Label>
-        <Input type='checkbox' />
-      </Field>
-      <Field position='end'>
-        <Label>Switch</Label>
-        <Input type='checkbox' role='switch' />
-      </Field>
-    </Fieldset>
+    <Field position='end'>
+      <Label>Flymodus</Label>
+      <Input type='checkbox' role='switch' />
+    </Field>
+    <Divider />
+    <Field position='end'>
+      <Label>Lydløs</Label>
+      <Input type='checkbox' role='switch' />
+    </Field>
   </>
 );
+
+Position.decorators = [
+  (Story) => (
+    <div
+      style={{
+        maxWidth: 200,
+        margin: 'auto',
+      }}
+    >
+      <Story />
+    </div>
+  ),
+];
