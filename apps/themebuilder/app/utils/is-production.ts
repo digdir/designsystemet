95f3cffaef84500954d2697729d4e9abdd302d33
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-05-22 15:00:57 +0200

    fix a couple of biome blockers

--- apps/themebuilder/app/utils/is-production.ts
+++ apps/themebuilder/app/utils/is-production.ts
@@ -1,4 +1,4 @@
 export function isProduction() {
   return false;
-  return process.env.NEXT_PUBLIC_DESIGNSYSTEMET_ENV === 'production';
+  //return process.env.NEXT_PUBLIC_DESIGNSYSTEMET_ENV === 'production';
 }
