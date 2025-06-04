bfe20cad0994a92fc89bded4fbc471158a249356
Barsnes <tobias.barsnes@digdir.no>
2025-06-04 13:09:33 +0200

    lang in documentation link..

--- apps/themebuilder/app/routes/home/home.tsx
+++ apps/themebuilder/app/routes/home/home.tsx
@@ -75,7 +75,7 @@ export default function Home({ params: { lang } }: Route.ComponentProps) {
                 </Link>
               </Button>
               <Button data-color='neutral' variant='secondary' asChild>
-                <Link to='https://www.designsystemet.no/no/fundamentals/for-designers/own-theme'>
+                <Link to={`https://www.designsystemet.no/${lang}/fundamentals/for-designers/own-theme`}>
                   <BookIcon fontSize='1.5rem' aria-hidden />
                   {t('themeBuilder.documentation')}
                 </Link>
