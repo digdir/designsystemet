7e027a1a072026af572ee2badcb168fb5ff6abaa
Michael Marszalek <mimarz@gmail.com>
2025-06-06 10:59:37 +0200

    fix: update plugin to latest theme colors (#3700)
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>

--- plugins/figma/src/common/types.ts
+++ plugins/figma/src/common/types.ts
@@ -1,16 +1,3 @@
-export type JsonInput = {
-  theme: {
-    accent: object;
-    neutral: object;
-    brand1: object;
-    brand2: object;
-    brand3: object;
-  };
-};
-
-export type FigmaModeName = 'theme' | 'theme2' | 'theme3' | 'theme4';
-export type ThemeColors = 'accent' | 'neutral' | 'brand1' | 'brand2' | 'brand3';
-
 export enum Messages {
   GetThemes = 'getThemes',
   GetThemesAndRedirect = 'getThemesAndRedirect',
