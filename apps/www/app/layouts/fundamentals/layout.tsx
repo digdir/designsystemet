f1fc3d945fc6ccff43d790d5026d43641eca5f81
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-06-12 10:08:51 +0200

    restructured content

--- apps/www/app/layouts/fundamentals/layout.tsx
+++ apps/www/app/layouts/fundamentals/layout.tsx
@@ -39,7 +39,7 @@ export const loader = async ({ params: { lang } }: Route.LoaderArgs) => {
   if (lang === 'no') {
     cats.Introduksjon = [];
     cats.Designelementer = [];
-    cats['For designere'] = [];
+    cats['Kom i gang'] = [];
     cats['For utviklere'] = [];
     cats['Temabygger'] = [];
   }
@@ -47,7 +47,7 @@ export const loader = async ({ params: { lang } }: Route.LoaderArgs) => {
   if (lang === 'en') {
     cats.Introduction = [];
     cats['Design elements'] = [];
-    cats['For designers'] = [];
+    cats['Get started'] = [];
     cats['For developers'] = [];
     cats['Theme Builder'] = [];
   }
