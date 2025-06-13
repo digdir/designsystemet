81b98b3fedc34f03f14f343a9811f4e9af884bd0
Barsnes <tobias.barsnes@digdir.no>
2025-06-13 08:37:24 +0200

    undo vitest

--- vite.config.ts
+++ vite.config.ts
@@ -10,33 +10,6 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  test: {
-    projects: ['packages/*/vitest.config.mjs'],
-        reporters: [
-      'default',
-      ['junit', { suiteName: 'Unit tests', addFileAttribute: true }],
-    ],
-    outputFile: {
-      junit: './test-report.xml',
-    },
-    coverage: {
-      enabled: true,
-      provider: 'v8',
-      reporter: ['html', 'json-summary', 'json'],
-      reportOnFailure: true,
-      include: ['packages/react/src/**/*.{ts,tsx}'],
-      exclude: [
-        '**/*.{stories,e2e,docs,test}.{ts,tsx}',
-        '**/index.ts',
-        '**/types/**',
-      ],
-    },
-    css: {
-      modules: {
-        classNameStrategy: 'non-scoped',
-      },
-    },
-  },
   // Temporary "fix" https://github.com/storybookjs/storybook/issues/28542
   optimizeDeps: {
     exclude: ['./node_modules/.cache/storybook'],
---
+++ vitest.config.mjs
@@ -0,0 +1,32 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    projects: ['packages/*/vitest.config.mjs'],
+    reporters: [
+      'default',
+      ['junit', { suiteName: 'Unit tests', addFileAttribute: true }],
+    ],
+    outputFile: {
+      junit: './test-report.xml',
+    },
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['html', 'json-summary', 'json'],
+      reportOnFailure: true,
+      include: ['packages/react/src/**/*.{ts,tsx}'],
+      exclude: [
+        '**/*.{stories,e2e,docs,test}.{ts,tsx}',
+        '**/index.ts',
+        '**/types/**',
+      ],
+    },
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped',
+      },
+    },
+  },
+});
