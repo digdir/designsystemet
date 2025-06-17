426d665b773424a06756e776930a8d69bb6ed6eb
Michael Marszalek <mimarz@gmail.com>
2025-06-17 11:23:26 +0200

    move theme

--- packages/cli/src/tokens/build.ts
+++ packages/cli/src/tokens/build.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 import * as R from 'ramda';
 import type { DesignToken } from 'style-dictionary/types';
 import { mkdir, readFile, writeFile } from '../utils.js';
-import { createThemeCSSFiles, defaultFileHeader } from './process/theme.js';
+import { createThemeCSSFiles, defaultFileHeader } from './process/output/theme.js';
 
 import { generateTailwind } from './process/output/create-tailwind-files.js';
 import { type BuildOptions, processPlatform } from './process/platform.js';
