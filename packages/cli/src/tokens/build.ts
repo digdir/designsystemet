ec37daffed3223cea71f5dd6dd3393624b3bc32e
Tobias Barsnes <tobias.barsnes@digdir.no>
2025-06-18 11:37:20 +0200

    feat(cli): add tailwind option to build (#3710)
    Co-authored-by: Eirik Backer <eirik.backer@gmail.com>
    Co-authored-by: Michael Marszalek <mimarz@gmail.com>

--- packages/cli/src/tokens/build.ts
+++ packages/cli/src/tokens/build.ts
@@ -4,8 +4,9 @@ import chalk from 'chalk';
 import * as R from 'ramda';
 import type { DesignToken } from 'style-dictionary/types';
 import { mkdir, readFile, writeFile } from '../utils.js';
-import { createThemeCSSFiles, defaultFileHeader } from './process/theme.js';
+import { createThemeCSSFiles, defaultFileHeader } from './process/output/theme.js';
 
+import { createTailwindCSSFiles } from './process/output/tailwind.js';
 import { type BuildOptions, processPlatform } from './process/platform.js';
 import type { DesignsystemetObject, OutputFile } from './types.js';
 
@@ -44,8 +45,6 @@ export const buildTokens = async (options: Omit<BuildOptions, 'type' | '$themes'
     $themes,
   });
 
-  console.log(`\n💾 Writing build to ${chalk.green(outDir)}`);
-
   // https://github.com/digdir/designsystemet/issues/3434
   // Disabled for now so that we can re-enable it later if needed (under a feature flag)
   // for (const [_, buildResults] of Object.entries(processedBuilds)) {
@@ -54,17 +53,27 @@ export const buildTokens = async (options: Omit<BuildOptions, 'type' | '$themes'
   //   }
   // }
 
+  const fileHeader = R.join('')([
+    defaultFileHeader,
+    $designsystemet ? `\ndesign-tokens: v${$designsystemet.version}` : '',
+  ]);
+
+  let cssFiles = createThemeCSSFiles({ processedBuilds, fileHeader });
+
+  if (options.tailwind) {
+    const tailwindFiles = createTailwindCSSFiles(cssFiles, outDir);
+    cssFiles = cssFiles.concat(tailwindFiles.filter(Boolean) as OutputFile[]);
+  }
+
+  console.log(`\n💾 Writing build to ${chalk.green(outDir)}`);
+
   // Write types (colors.d.ts) to the output directory
   for (const { formatted } of processedBuilds.types) {
     await write(formatted, outDir, options.dry);
   }
 
-  const fileHeader = R.join('')([
-    defaultFileHeader,
-    $designsystemet ? `\ndesign-tokens: v${$designsystemet.version}` : '',
-  ]);
   // Write theme CSS files (<theme>.css) to the output directory
-  await write(createThemeCSSFiles({ processedBuilds, fileHeader }), outDir, options.dry);
+  await write(cssFiles, outDir, options.dry);
 
   console.log(`\n✅ Finished building tokens!`);
   return processedBuilds;
