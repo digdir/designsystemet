48a5f4d66f503ab6d023c0a28fb3b37a75603c0d
Barsnes <tobias.barsnes@digdir.no>
2025-06-24 09:54:17 +0200

    update plugin

--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -37,9 +37,7 @@ function postcssComposes() {
           );
 
         cache[resolvedFrom].root.walkRules((fromRule) => {
-          /* This checks for either the exact class (.selector) or a class with a pseudo-element/class (.selector:pseudo, .selector::pseudo) */
-          const classRegex = new RegExp(`^\\.${selector}($|[^-_a-zA-Z0-9])`);
-          if (classRegex.test(fromRule.selector)) {
+          if (fromRule.selector.split(/:|\s/)[0] === `.${selector}`) {
             rule.replaceWith(
               fromRule.clone({
                 selector: fromRule.selector.replace(`.${selector}`, '&'),
