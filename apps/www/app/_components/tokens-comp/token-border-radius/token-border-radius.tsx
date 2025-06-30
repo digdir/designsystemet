1790a50983905cbbdc2573d483cd9432ca8689a7
Michael Marszalek <mimarz@gmail.com>
2025-06-30 12:32:12 +0200

    updated visualization

--- apps/www/app/_components/tokens-comp/token-border-radius/token-border-radius.tsx
+++ apps/www/app/_components/tokens-comp/token-border-radius/token-border-radius.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
 import classes from './token-border-radius.module.css';
 
 const TokenBorderRadius = ({ value }: { value: string }) => {
-  return <div className={classes.bar} style={{ borderRadius: value }}></div>;
+  // This is a temp solution to get the computed value of the token. Find a better way to do this in https://github.com/digdir/designsystemet/issues/2946
+  const [computedValue, setComputedValue] = useState<string>('');
+
+  useEffect(() => {
+    if (!document) return;
+
+    const elm = document.createElement('div');
+    elm.style.cssText = `width: ${value}; height: ${value};`;
+    document.body.appendChild(elm);
+    const computedValue = getComputedStyle(elm).width;
+    document.body.removeChild(elm);
+
+    setComputedValue(computedValue);
+  }, [value]);
+
+  return (
+    <div className={classes.radius}>
+      <code>{computedValue}</code>
+      <div className={classes.bar} style={{ borderRadius: value }}></div>
+    </div>
+  );
 };
 
 export { TokenBorderRadius };
