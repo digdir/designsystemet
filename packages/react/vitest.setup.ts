458fd32851db5c0f42c76526da7de8365fb75ea2
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-07-15 11:35:31 +0200

    test(react): also run tests with react 18 (#3858)

--- packages/react/vitest.setup.ts
+++ packages/react/vitest.setup.ts
@@ -1,3 +1,19 @@
 import { configure } from '@testing-library/react';
+import { version as reactVersion } from 'react';
+import { version as reactDomVersion } from 'react-dom';
+
+const expectedVersion = process.env.REACT_VERSION;
+if (expectedVersion) {
+  if (!reactVersion.startsWith(expectedVersion)) {
+    throw new Error(
+      `Expected react v${expectedVersion}, but found v${reactVersion}`,
+    );
+  }
+  if (!reactDomVersion.startsWith(expectedVersion)) {
+    throw new Error(
+      `Expected react-dom v${expectedVersion}, but found v${reactDomVersion}`,
+    );
+  }
+}
 
 configure({ reactStrictMode: true });
