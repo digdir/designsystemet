6fd868abf5ed0580f73ea8ea37d43f1a365bae15
Michael Marszalek <mimarz@gmail.com>
2025-07-16 15:51:06 +0200

    Add note about Size export type

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,7 @@
+export type {
+  /** @deprecated This export is deprecated. Use Size from '@digdir/designsystemet/types' */
+  Size
+} from '@digdir/designsystemet/types';
 export * from './components';
-export type { LabelRequired, Size } from './types';
+export type { LabelRequired } from './types';
 export * from './utilities';
