dca12ad26edda3c45cb4560a11cb9f9e74283c40
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-16 12:56:14 +0200

    breadcrumbs updated type

--- packages/react/src/components/breadcrumbs/index.ts
+++ packages/react/src/components/breadcrumbs/index.ts
@@ -3,11 +3,56 @@ import { BreadcrumbsItem } from './breadcrumbs-item';
 import { BreadcrumbsLink } from './breadcrumbs-link';
 import { BreadcrumbsList } from './breadcrumbs-list';
 
+type Breadcrumbs = typeof BreadcrumbsParent & {
+  /**
+   * Component that provides a Breadcrumbs item.
+   *
+   * Place as a descendant of `Breadcrumbs.List`
+   *
+   * @example
+   * <Breadcrumbs.List>
+   *   <Breadcrumbs.Item>
+   *     <Breadcrumbs.Link href='#'>Nivå 1</Breadcrumbs.Link>
+   *   </Breadcrumbs.Item>
+   * </Breadcrumbs.List>
+   */
+  Item: typeof BreadcrumbsItem;
+  /**
+   * Component that provides a link for the Breadcrumbs.
+   *
+   * Place as a descendant of `Breadcrumbs`
+   *
+   * @example
+   * <Breadcrumbs>
+   *   <Breadcrumbs.Link href='#'>Nivå 1</Breadcrumbs.Link>
+   * </Breadcrumbs>
+   */
+  Link: typeof BreadcrumbsLink;
+  /**
+   * Component that provides a list of Breadcrumbs items.
+   *
+   * Place as a descendant of `Breadcrumbs`
+   *
+   * @example
+   * <Breadcrumbs>
+   *   <Breadcrumbs.List>
+   *     <Breadcrumbs.Item>
+   *       <Breadcrumbs.Link href='#'>Nivå 1</Breadcrumbs.Link>
+   *     </Breadcrumbs.Item>
+   *     <Breadcrumbs.Item>
+   *       <Breadcrumbs.Link href='#'>Nivå 2</Breadcrumbs.Link>
+   *     </Breadcrumbs.Item>
+   *   </Breadcrumbs.List>
+   * </Breadcrumbs>
+   */
+  List: typeof BreadcrumbsList;
+};
+
 /**
  * `Breadcrumbs` is a component that displays a list of breadcrumbs.
  *
  * @example
- * <Breadcrumbs aria-label='Du er her:' {...args}>
+ * <Breadcrumbs aria-label='Du er her:'>
  *  <Breadcrumbs.Link href='#' aria-label='Tilbake til Nivå 1'>
  *    Nivå 1
  *  </Breadcrumbs.Link>
@@ -21,7 +66,7 @@ import { BreadcrumbsList } from './breadcrumbs-list';
  *  </Breadcrumbs.List>
  * </Breadcrumbs>
  */
-export const Breadcrumbs = Object.assign(BreadcrumbsParent, {
+export const Breadcrumbs: Breadcrumbs = Object.assign(BreadcrumbsParent, {
   List: BreadcrumbsList,
   Item: BreadcrumbsItem,
   Link: BreadcrumbsLink,
