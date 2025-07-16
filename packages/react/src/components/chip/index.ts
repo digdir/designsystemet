73231d7cc0292c8d6f9d8755081fa1e551498a7e
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-16 13:46:45 +0200

    Chip CompondProps

--- packages/react/src/components/chip/index.ts
+++ packages/react/src/components/chip/index.ts
@@ -1,6 +1,39 @@
 import { ChipButton, ChipCheckbox, ChipRadio, ChipRemovable } from './chips';
 
-const Chip = {
+type ChipCompondProps = {
+  /**
+   * Chip.Button used for interaction
+   *
+   * @example
+   * <Chip.Button>Click me</Chip.Button>
+   */
+  Button: typeof ChipButton;
+  /**
+   * Chip.Checkbox used for multiselection
+   *
+   * @example
+   * <Chip.Checkbox name="language" value="nynorsk">Nynorsk</Chip.Checkbox>
+   * <Chip.Checkbox name="language" value="bokmål">Bokmål</Chip.Checkbox>
+   */
+  Checkbox: typeof ChipCheckbox;
+  /**
+   * Chip.Radio used for single selection
+   *
+   * @example
+   * <Chip.Radio name="language" value="nynorsk">Nynorsk</Chip.Radio>
+   * <Chip.Radio name="language" value="bokmål">Bokmål</Chip.Radio>
+   */
+  Radio: typeof ChipRadio;
+  /**
+   * Chip.Removable used for interaction
+   *
+   * @example
+   * <Chip.Removable>Click to remove me</Chip.Removable>
+   */
+  Removable: typeof ChipRemovable;
+};
+
+const Chip: ChipCompondProps = {
   Button: ChipButton,
   Checkbox: ChipCheckbox,
   Radio: ChipRadio,
