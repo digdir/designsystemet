c8f551a69a95c1c837189b6258bda5436b7b976b
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-16 15:03:26 +0200

    dropdown jsdocs

--- packages/react/src/components/dropdown/dropdown-button.tsx
+++ packages/react/src/components/dropdown/dropdown-button.tsx
@@ -4,6 +4,18 @@ import { Button, type ButtonProps } from '../button/button';
 
 export type DropdownButtonProps = Omit<ButtonProps, 'variant'>;
 
+/**
+ * DropdownButton component, used to display a button in the DropdownItem.
+ *
+ * @example
+ * <Dropdown>
+ *  <DropdownList>
+ *    <DropdownItem>
+ *      <DropdownButton>Button</DropdownButton>
+ *    </DropdownItem>
+ *  </DropdownList>
+ * </Dropdown>
+ */
 export const DropdownButton = forwardRef<
   HTMLButtonElement,
   DropdownButtonProps
--- packages/react/src/components/dropdown/dropdown-list.tsx
+++ packages/react/src/components/dropdown/dropdown-list.tsx
@@ -3,6 +3,18 @@ import { forwardRef } from 'react';
 
 export type DropdownListProps = HTMLAttributes<HTMLUListElement>;
 
+/**
+ * DropdownList component, used to display a list of items in the Dropdown.
+ *
+ * @example
+ * <Dropdown>
+ *  <DropdownList>
+ *    <DropdownItem>
+ *      <DropdownButton>Button</DropdownButton>
+ *    </DropdownItem>
+ *  </DropdownList>
+ * </Dropdown>
+ */
 export const DropdownList = forwardRef<HTMLUListElement, DropdownListProps>(
   function DropdownList({ className, ...rest }, ref) {
     return <ul ref={ref} {...rest} />;
