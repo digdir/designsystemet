55dcf718629e24d545fdbd7a9e060cf9b4e0f95f
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-07-17 13:39:09 +0200

    feat: change data-size to work like Figma size mode

---
+++ packages/cli/src/tokens/process/formats/css/size-mode.ts
@@ -0,0 +1,76 @@
+import * as R from 'ramda';
+import type { TransformedToken } from 'style-dictionary';
+import type { Format } from 'style-dictionary/types';
+import { createPropertyFormatter } from 'style-dictionary/utils';
+import { orderBySize, shortSizeName } from '../../../utils.js';
+import { basePxFontSize } from '../../configs/shared.js';
+import { buildOptions } from '../../platform.js';
+
+const formatBaseSizeToken =
+  (size: string) =>
+  (token: TransformedToken): TransformedToken & { originalName: string } => ({
+    ...token,
+    originalName: token.name,
+    name: `${token.name}--${shortSizeName(size)}`,
+    $value: token.$value / basePxFontSize,
+  });
+
+export const sizeMode: Format = {
+  name: 'ds/css-size-mode',
+  format: async ({ dictionary, file, options, platform }) => {
+    const { outputReferences, usesDtcg } = options;
+    const { selector, layer, size } = platform;
+    const destination = file.destination as string;
+
+    const format = createPropertyFormatter({
+      outputReferences,
+      dictionary,
+      format: 'css',
+      usesDtcg,
+    });
+
+    const sizeSpecificTokens = dictionary.allTokens.map(formatBaseSizeToken(size));
+    const sizeSpecificVariables = sizeSpecificTokens.map(format).join('\n');
+
+    const formattedMap = sizeSpecificTokens.map((token) => ({
+      token,
+      formatted: format({
+        ...token,
+        // Remove the `--<size>` suffix for the token listing, since that is the only token we actually use
+        name: token.originalName,
+      }),
+    }));
+
+    buildOptions.buildTokenFormats[destination] = formattedMap;
+
+    const content = `${selector} /* ${size} */ {\n${sizeSpecificVariables}\n}`;
+    const body = wrapInLayer(content, layer);
+
+    /*
+     * The following CSS is only generated once, not per mode
+     */
+    const sizes = orderBySize(buildOptions?.sizeModes ?? []).map(shortSizeName);
+    const defaultSize = shortSizeName(buildOptions?.defaultSize ?? '');
+
+    const sizingToggles = `:root, [data-size] {
+  --ds-size: var(--ds-size--${defaultSize});
+${sizes.map((size) => `  --ds-size--${size}: var(--ds-size,);`).join('\n')}
+  --ds-size-mode-font-size:
+${sizes.map((size) => `    var(--ds-size--${size}, var(--ds-size-mode-font-size--${size}))`).join('\n')};
+}`;
+    const sizingHelpers = sizes
+      .map((size) => `[data-size='${size}'] { --ds-size: var(--ds-size--${size}); }`)
+      .join('\n');
+    const sharedContent = `${sizingToggles}\n\n${sizingHelpers}`;
+    const sharedBody = shortSizeName(size) === R.last(sizes) ? `\n${wrapInLayer(sharedContent, layer)}` : '';
+    /*
+     * End of generated-once CSS
+     */
+
+    return body + sharedBody;
+  },
+};
+
+export function wrapInLayer(content: string, layer?: string) {
+  return R.isNotNil(layer) ? `@layer ${layer} {\n${content}\n}\n` : `${content}\n`;
+}
---
+++ packages/cli/src/tokens/process/formats/css/size.ts
@@ -0,0 +1,104 @@
+import * as R from 'ramda';
+import type { TransformedToken } from 'style-dictionary';
+import type { Format } from 'style-dictionary/types';
+import { createPropertyFormatter } from 'style-dictionary/utils';
+import { inlineTokens, isDigit, pathStartsWithOneOf } from '../../../utils.js';
+import { buildOptions } from '../../platform.js';
+
+const isNumericBorderRadiusToken = (t: TransformedToken) => t.path[0] === 'border-radius' && isDigit(t.path[1]);
+const isNumericSizeToken = (t: TransformedToken) => pathStartsWithOneOf(['size'], t) && isDigit(t.path[1]);
+const isSizeToken = (t: TransformedToken) => pathStartsWithOneOf(['size'], t);
+
+export const isInlineTokens = R.anyPass([isNumericBorderRadiusToken, isNumericSizeToken, isSizeToken]);
+
+/**
+ * Overrides the default sizing formula with a custom one that supports [round()](https://developer.mozilla.org/en-US/docs/Web/CSS/round) if supported.
+ *
+ * @param format - Function to format a token into a CSS property string.
+ * @param tokens - Array of transformed tokens to format.
+ * @returns Object with formatted CSS strings for calc and round.
+ */
+export const overrideSizingFormula = (format: (t: TransformedToken) => string, token: TransformedToken) => {
+  const [name, value] = format(token).split(':');
+
+  const calc = value.replace(/floor\((.*)\);/, 'calc(1rem * $1)');
+
+  const round = `round(down, ${calc}, 1px)`;
+
+  return {
+    name,
+    round,
+    calc,
+  };
+};
+
+/**
+ * Formats sizing tokens into CSS properties with support for rounding.
+ *
+ * @param format - Function to format a token into a CSS property string.
+ * @param tokens - Array of transformed tokens to format.
+ * @returns Formatted CSS string with default calc and [round()](https://developer.mozilla.org/en-US/docs/Web/CSS/round) if supported.
+ */
+const formatSizingTokens = (format: (t: TransformedToken) => string, tokens: TransformedToken[]) =>
+  R.reduce(
+    (acc, token) => {
+      const { round, calc, name } = overrideSizingFormula(format, token);
+
+      return {
+        tokens: [...acc.tokens, token],
+        round: [...acc.round, `${name}: ${round};`],
+        calc: [...acc.calc, `${name}: ${calc};`],
+      };
+    },
+    { tokens: [], round: [], calc: [] } as { tokens: TransformedToken[]; round: string[]; calc: string[] },
+    tokens,
+  );
+
+const sizingTemplate = ({ round, calc }: { round: string[]; calc: string[] }) => `
+${calc.join('\n')}\n
+  @supports (width: round(down, .1em, 1px)) {
+${round.join('\n')}
+  }`;
+
+export const size: Format = {
+  name: 'ds/css-size',
+  format: async ({ dictionary, file, options, platform }) => {
+    const { outputReferences, usesDtcg } = options;
+    const { selector, layer } = platform;
+    const destination = file.destination as string;
+
+    const format = createPropertyFormatter({
+      outputReferences,
+      dictionary,
+      format: 'css',
+      usesDtcg,
+    });
+
+    const tokens = inlineTokens(isInlineTokens, dictionary.allTokens);
+    const filteredTokens = R.reject((token) => R.equals(['_size', 'mode-font-size'], token.path), tokens);
+    const [sizingTokens, restTokens] = R.partition(
+      (t: TransformedToken) => pathStartsWithOneOf(['_size'], t) && isDigit(t.path[1]),
+      filteredTokens,
+    );
+    const formattedSizingTokens = formatSizingTokens(format, sizingTokens);
+
+    const formattedMap = restTokens.map((token) => ({
+      token,
+      formatted: format(token),
+    }));
+
+    const formattedSizingMap = formattedSizingTokens.round.map((t, i) => ({
+      token: formattedSizingTokens.tokens[i],
+      formatted: t,
+    }));
+
+    buildOptions.buildTokenFormats[destination] = [...formattedMap, ...formattedSizingMap];
+
+    const formattedTokens = [formattedMap.map(R.prop('formatted')).join('\n'), sizingTemplate(formattedSizingTokens)];
+
+    const content = `${selector} {\n${formattedTokens.join('\n')}\n}\n`;
+    const body = R.isNotNil(layer) ? `@layer ${layer} {\n${content}\n}\n` : `${content}\n`;
+
+    return body;
+  },
+};
