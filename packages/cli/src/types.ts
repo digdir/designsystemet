9a16550d9615ea9abec76c684ab61adcb4134c87
Michael Marszalek <mimarz@gmail.com>
2025-07-18 13:35:42 +0200

    add jsdoc

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -1,3 +1,5 @@
+// This file defines public types used in the Designsystemet
+
 // EmptyObject implementation from https://github.com/sindresorhus/type-fest/blob/main/source/empty-object.d.ts
 declare const emptyObjectSymbol: unique symbol;
 type EmptyObject = { [emptyObjectSymbol]?: never };
@@ -19,7 +21,35 @@ export interface SeverityColorDefinitions {}
  */
 type ColorWithFallback<T> = ColorDefinitions extends EmptyObject ? string : T;
 
+/**
+ * Represents the available severity colors for the Designsystemet variables.
+ * These are predefined colors that can be used to indicate different levels of severity.
+ *  - `'info'`: Use the info color.
+ *  - `'success'`: Use the success color.
+ *  - `'warning'`: Use the warning color.
+ *  - `'danger'`: Use the danger color.
+ */
 export type SeverityColors = ColorWithFallback<keyof SeverityColorDefinitions>;
+/**
+ * Represents the available color options for the Designsystemet variables.
+ *
+ * These are augmented based on your theme configuration.
+ *
+ * Consist of both main and support colors
+ * @link https://theme.designsystemet.no
+ */
 export type Color = ColorWithFallback<keyof ColorDefinitions>;
+/**
+ * Represents the recommended size options for the Designsystemet variables.
+ * - `'sm'`: Use the small size.
+ * - `'md'`: Use the medium size.
+ * - `'lg'`: Use the large size.
+ */
 export type Size = 'sm' | 'md' | 'lg';
+/**
+ * Represents the available color scheme options for the Designsystemet variables.
+ * - `'light'`: Use the light color scheme.
+ * - `'dark'`: Use the dark color scheme.
+ * - `'auto'`: Automatically select the color scheme based on system preferences.
+ */
 export type ColorScheme = 'light' | 'dark' | 'auto';
