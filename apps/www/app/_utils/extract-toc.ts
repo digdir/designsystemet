30e65d153d9003d25b4467fc48df9f8b6d8715e2
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-23 16:47:40 +0200

    extract table of contents logic

---
+++ apps/www/app/_utils/extract-toc.ts
@@ -0,0 +1,44 @@
+import type { Element, Root, Text } from 'hast';
+
+export type TableOfContentsItem = {
+  title: string;
+  id: string;
+  level: 2 | 3;
+};
+
+/*the VFile type has a data field*/
+export type VFile = {
+  data: {
+    toc?: TableOfContentsItem[];
+  };
+};
+
+// Custom plugin to extract headings
+export const extractToc = () => (tree: Root, file: VFile) => {
+  const toc: TableOfContentsItem[] = [];
+  const traverse = (node: Root | Element) => {
+    if ('tagName' in node && (node.tagName === 'h2' || node.tagName === 'h3')) {
+      const title = node.children
+        .filter((child) => 'type' in child && child.type === 'text')
+        .map((child: Text) => child.value)
+        .join('');
+
+      toc.push({
+        title,
+        id: node.properties?.id as string,
+        level: parseInt(node.tagName.charAt(1)) as 2 | 3,
+      });
+    }
+
+    if ('children' in node) {
+      node.children.forEach((child) => {
+        if ('type' in child) {
+          traverse(child as Element);
+        }
+      });
+    }
+  };
+
+  traverse(tree);
+  file.data.toc = toc;
+};
--- apps/www/app/_utils/generate-from-mdx.ts
+++ apps/www/app/_utils/generate-from-mdx.ts
@@ -1,12 +1,22 @@
+import type { Root } from 'hast';
 import { bundleMDX } from 'mdx-bundler';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
+import type { TableOfContentsItem, VFile } from './extract-toc';
+import { extractToc } from './extract-toc';
 
 export const generateFromMdx = async (
   fileContent: string,
-): ReturnType<typeof bundleMDX> => {
-  return await bundleMDX({
+): Promise<{
+  code: string;
+  // biome-ignore lint/suspicious/noExplicitAny: this is how frontmatter is typed in mdx-bundler
+  frontmatter: { [key: string]: any };
+  toc: TableOfContentsItem[];
+}> => {
+  let tocData: TableOfContentsItem[] = [];
+
+  const result = await bundleMDX({
     source: fileContent,
     mdxOptions(options) {
       options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
@@ -14,9 +24,18 @@ export const generateFromMdx = async (
         ...(options.rehypePlugins ?? []),
         rehypeSlug,
         rehypeAutolinkHeadings,
+        () => (tree: Root, file: VFile) => {
+          extractToc()(tree, file);
+          tocData = file.data.toc || [];
+        },
       ];
 
       return options;
     },
   });
+
+  return {
+    ...result,
+    toc: tocData,
+  };
 };
