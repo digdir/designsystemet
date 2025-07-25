5422fa9ab2e4fed1673c4c2e8755e10141a4ab54
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-25 13:49:43 +0200

    note

--- apps/storybook/.storybook/customTheme.ts
+++ apps/storybook/.storybook/customTheme.ts
@@ -1,5 +1,9 @@
 import { create } from 'storybook/theming';
 
+/* Styling will become significantly easier if this ever gets done:
+ * https://github.com/storybookjs/storybook/discussions/24344
+ * Currently most of these properties only support hex/rgba and not var() or light-dark()
+ * */
 export default create({
   brandTitle: 'Komponenter',
   // brandImage: 'logo_digdir.svg',
