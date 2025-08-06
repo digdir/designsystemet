adc56b3336974ed01b02607a82ed3fa14a4ee875
Barsnes <tobias.barsnes@digdir.no>
2025-08-06 11:47:32 +0200

    work on header

--- apps/storybook/.storybook/manager.ts
+++ apps/storybook/.storybook/manager.ts
@@ -1,6 +1,7 @@
 import { addons } from 'storybook/manager-api';
-
 import customTheme from './customThemeLight';
+import '@digdir/designsystemet-css';
+import '@digdir/designsystemet-theme';
 
 addons.setConfig({
   theme: customTheme,
