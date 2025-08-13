33838b1195d1344b51990baa1c14bd72b871e8af
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-08-13 11:48:27 +0200

    whoops, span is needed anyway

--- packages/react/src/components/link/link.tsx
+++ packages/react/src/components/link/link.tsx
@@ -29,12 +29,12 @@ export type LinkProps = MergeRight<
 export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
   ({ asChild, className, children, ...rest }, ref) => {
     const Component = asChild ? Slot : 'a';
-    // Ensure bare strings are trimmed if there are 2 or more children.
-    // This removes underline between icon and text. We add spacing to the icon using css instead.
+    // Ensure bare strings are wrapped in <span> if there are 2 or more children.
+    // This is necessary for styling which removes underline between icon and text.
     const fixedChildren =
       Children.count(children) > 1
         ? Children.map(children, (child) =>
-            typeof child === 'string' ? child.trim() : child,
+            typeof child === 'string' ? <span>{child.trim()}</span> : child,
           )
         : children;
     return (
