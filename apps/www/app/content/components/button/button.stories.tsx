8ed0c16f0a845f510652520409b6a60b6c0f83dc
Barsnes <tobias.barsnes@digdir.no>
2025-08-19 09:25:36 +0200

    docs

--- apps/www/app/content/components/button/button.stories.tsx
+++ apps/www/app/content/components/button/button.stories.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@digdir/designsystemet-react';
+import { Button, type ButtonProps } from '@digdir/designsystemet-react';
 
 export const Preview = () => {
   return <Button>min knapp!</Button>;
@@ -10,6 +10,31 @@ export const Variants = () => {
       <Button variant='primary'>Primary</Button>
       <Button variant='secondary'>Secondary</Button>
       <Button variant='tertiary'>Teritiary</Button>
+    </>
+  );
+};
+
+export const ColorVariants = () => {
+  const colorVariants = [
+    'accent',
+    'brand1',
+    'brand2',
+    'brand3',
+    'neutral',
+    'danger',
+  ];
+
+  return (
+    <>
+      {colorVariants.map((color) => (
+        <Button
+          key={color}
+          data-color={color as ButtonProps['data-color']}
+          variant='primary'
+        >
+          {color}
+        </Button>
+      ))}
     </>
   );
 };
