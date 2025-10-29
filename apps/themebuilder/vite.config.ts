e00f4decd81b63ad4b51a4e352efad618e50a4ac
Barsnes <tobias.barsnes@digdir.no>
2025-10-29 11:29:49 +0100

    chore(themebuilder): use react-compiler

--- apps/themebuilder/vite.config.ts
+++ apps/themebuilder/vite.config.ts
@@ -1,9 +1,20 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ['@babel/preset-typescript'], // if you use TypeScript
+        plugins: [['babel-plugin-react-compiler', {}]],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   ssr: {
     noExternal: ['ramda'],
   },
