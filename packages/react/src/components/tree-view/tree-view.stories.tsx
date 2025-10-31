824022c3cae6a5660d0ebe616b5a2b16746372d0
Barsnes <tobias.barsnes@digdir.no>
2025-10-31 14:54:16 +0100

    make padding work

--- packages/react/src/components/tree-view/tree-view.stories.tsx
+++ packages/react/src/components/tree-view/tree-view.stories.tsx
@@ -16,8 +16,13 @@ export const Preview: StoryFn<typeof TreeView> = () => {
       <TreeView.Collapse>
         <TreeView.Summary>Item 3</TreeView.Summary>
         <TreeView.Item>Item 4</TreeView.Item>
-        <TreeView.Item>Item 5</TreeView.Item>
+        <TreeView.Collapse>
+          <TreeView.Summary>Item 5</TreeView.Summary>
+          <TreeView.Item>Item 6</TreeView.Item>
+          <TreeView.Item>Item 7</TreeView.Item>
+        </TreeView.Collapse>
       </TreeView.Collapse>
+      <TreeView.Item>Item 8</TreeView.Item>
     </TreeView>
   );
 };
