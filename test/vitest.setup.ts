edf3d3b919cc4f3a5d75e4ab7b7006456384abb1
Barsnes <tobias.barsnes@digdir.no>
2025-11-07 14:39:53 +0100

    chore: monorepo hygene

--- test/vitest.setup.ts
+++ test/vitest.setup.ts
@@ -2,25 +2,11 @@ import { expect, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
-import { mockMediaQuery } from './testUtils';
-
 expect.extend(matchers);
 
 /**
  * Mock of Dialog element for testing purposes
  */
-
-HTMLDialogElement.prototype.show = vi.fn(function mock(
-  this: HTMLDialogElement,
-) {
-  this.open = true;
-});
-
-HTMLDialogElement.prototype.showModal = vi.fn(function mock(
-  this: HTMLDialogElement,
-) {
-  this.open = true;
-});
 
 HTMLDialogElement.prototype.close = vi.fn(function mock(
   this: HTMLDialogElement,
@@ -37,15 +23,3 @@ document.addEventListener('keydown', (event) => {
     dialog?.close();
   }
 });
-
-// Add support for dialog form method
-document.addEventListener('click', ({ target }) => {
-  if (target instanceof HTMLElement)
-    target
-      .closest('form[method="dialog"] button[type="submit"]')
-      ?.closest('dialog')
-      ?.close();
-});
-
-const { setScreenWidth } = mockMediaQuery(800);
-setScreenWidth(800);
