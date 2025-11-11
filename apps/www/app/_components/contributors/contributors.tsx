cb856addcf57095d7c15453c9b110dc40a9ef9f3
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-11-11 14:49:08 +0100

    contibutors new design

--- apps/www/app/_components/contributors/contributors.tsx
+++ apps/www/app/_components/contributors/contributors.tsx
@@ -16,24 +16,27 @@ export const Contributors = ({
   const { t } = useTranslation();
   return (
     <section className={classes.container}>
-      <div className={classes.logo}>
-        <img
-          src='/img/diamond-logo.svg'
-          alt='Designsystemet logo'
-          className={classes.logoImage}
-        />
+      <img
+        src='/img/diamond-logo.svg'
+        alt='Designsystemet logo'
+        className={classes.logo}
+      />
+
+      <div>
+        <Heading level={headingLevel} data-size='2xs'>
+          {t('contributors')}
+        </Heading>
+        <Paragraph data-size='sm' className={classes.meta}>
+          {authors?.map((author, index) => (
+            <Fragment key={index}>
+              {index !== 0 && (
+                <span aria-hidden className={classes.metaSquare} />
+              )}
+              <span>{author}</span>
+            </Fragment>
+          ))}
+        </Paragraph>
       </div>
-      <Heading level={headingLevel} data-size='2xs'>
-        {t('contributors')}
-      </Heading>
-      <Paragraph data-size='sm' className={classes.meta}>
-        {authors?.map((author, index) => (
-          <Fragment key={index}>
-            {index !== 0 && <span aria-hidden className={classes.metaSquare} />}
-            <span>{author}</span>
-          </Fragment>
-        ))}
-      </Paragraph>
     </section>
   );
 };
