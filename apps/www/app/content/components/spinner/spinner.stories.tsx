ada83ae30ff1d1bb76218b8a94f95f134ea696e5
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-11-25 17:44:29 +0100

    spinner

--- apps/www/app/content/components/spinner/spinner.stories.tsx
+++ apps/www/app/content/components/spinner/spinner.stories.tsx
@@ -1,4 +1,4 @@
-import { Spinner } from '@digdir/designsystemet-react';
+import { Paragraph, Spinner } from '@digdir/designsystemet-react';
 
 export const Preview = () => {
   return <Spinner aria-label='Henter kaffi' />;
@@ -13,6 +13,24 @@ export const Sizes = () => {
       <Spinner aria-label='Henter kaffi' data-size='md' />
       <Spinner aria-label='Henter kaffi' data-size='lg' />
       <Spinner aria-label='Henter kaffi' data-size='xl' />
+    </>
+  );
+};
+
+export const Text = () => {
+  return (
+    <>
+      <Spinner aria-label='Preparing' data-size='sm' />
+      <Paragraph>Forbereder filen din</Paragraph>
+    </>
+  );
+};
+
+export const TextEn = () => {
+  return (
+    <>
+      <Spinner aria-label='Preparing' data-size='sm' />
+      <Paragraph>Preparing your file</Paragraph>
     </>
   );
 };
