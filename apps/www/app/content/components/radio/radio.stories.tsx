fffdff3bc1d150f536e65cff9974ac575caed252
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-11-25 14:57:58 +0100

    apparently Radio deserves all my time today

--- apps/www/app/content/components/radio/radio.stories.tsx
+++ apps/www/app/content/components/radio/radio.stories.tsx
@@ -38,6 +38,36 @@ export const Group = () => {
   );
 };
 
+export const GroupEn = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>How would you like us to contact you?</Fieldset.Legend>
+      <Fieldset.Description>
+        Choose the method that works best for you. We use this only to send
+        important updates about your case.
+      </Fieldset.Description>
+      <Radio
+        label='Email'
+        description='We will use the email address you provided earlier (name@example.com)'
+        value='email'
+        name='contact'
+      />
+      <Radio
+        label='SMS'
+        description='We will use the phone number you provided earlier (99 99 99 99)'
+        value='sms'
+        name='contact'
+      />
+      <Radio
+        label='Letter'
+        description='Delivery may take 3–5 working days, depending on the postal service.'
+        value='letter'
+        name='contact'
+      />
+    </Fieldset>
+  );
+};
+
 export const WithError = () => {
   return (
     <Fieldset>
@@ -66,12 +96,55 @@ export const WithError = () => {
   );
 };
 
+export const WithErrorEn = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Which district do you live in?</Fieldset.Legend>
+      <Fieldset.Description>
+        Trondheim is divided into four districts
+      </Fieldset.Description>
+      <Radio label='Østbyen' value='ostbyen' name='city' aria-invalid='true' />
+      <Radio
+        label='Lerkendal'
+        value='lerkendal'
+        name='city'
+        aria-invalid='true'
+      />
+      <Radio label='Heimdal' value='heimdal' name='city' aria-invalid='true' />
+      <Radio
+        label='Midtbyen'
+        value='midtbyen'
+        name='city'
+        aria-invalid='true'
+      />
+      <ValidationMessage data-color='danger'>
+        You must choose a district before you can continue.
+      </ValidationMessage>
+    </Fieldset>
+  );
+};
+
 export const ReadOnly = () => {
   return (
     <Fieldset>
       <Fieldset.Legend>Hvilken bydel bor du i?</Fieldset.Legend>
       <Fieldset.Description>
         Trondheim er delt inn i fire bydeler
+      </Fieldset.Description>
+      <Radio label='Østbyen' value='ostbyen' name='city' readOnly />
+      <Radio label='Lerkendal' value='lerkendal' name='city' readOnly />
+      <Radio label='Heimdal' value='heimdal' name='city' readOnly checked />
+      <Radio label='Midtbyen' value='midtbyen' name='city' readOnly />
+    </Fieldset>
+  );
+};
+
+export const ReadOnlyEn = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Which district do you live in?</Fieldset.Legend>
+      <Fieldset.Description>
+        Trondheim is divided into four districts
       </Fieldset.Description>
       <Radio label='Østbyen' value='ostbyen' name='city' readOnly />
       <Radio label='Lerkendal' value='lerkendal' name='city' readOnly />
@@ -93,6 +166,23 @@ export const Inline = () => {
       >
         <Radio name='my-inline' label='Ja' value='ja' />
         <Radio name='my-inline' label='Nei' value='nei' />
+      </div>
+    </Fieldset>
+  );
+};
+
+export const InlineEn = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Contact by email?</Fieldset.Legend>
+      <Fieldset.Description>
+        Confirm whether you would like to be contacted by email.
+      </Fieldset.Description>
+      <div
+        style={{ display: 'flex', flexWrap: 'wrap', gap: 'var(--ds-size-6)' }}
+      >
+        <Radio name='my-inline' label='Yes' value='yes' />
+        <Radio name='my-inline' label='No' value='no' />
       </div>
     </Fieldset>
   );
