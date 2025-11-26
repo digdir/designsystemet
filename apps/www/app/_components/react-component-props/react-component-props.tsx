c72445b859e912c05b929f5a31dd07a144d02a26
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-11-26 15:34:05 +0100

    propstable wip

--- apps/www/app/_components/react-component-props/react-component-props.tsx
+++ apps/www/app/_components/react-component-props/react-component-props.tsx
@@ -1,4 +1,4 @@
-import { Tag } from '@digdir/designsystemet-react';
+import { Table, Tag } from '@digdir/designsystemet-react';
 import cl from 'clsx/lite';
 import { forwardRef, type HTMLAttributes } from 'react';
 import type { ComponentDoc } from 'react-docgen-typescript';
@@ -28,7 +28,7 @@ export const ReactComponentDocs = forwardRef<
         .map((doc, index) => (
           <div key={doc.displayName} className={classes.component}>
             {index > 0 && <h3 className='ds-heading'>{doc.displayName}</h3>}
-            <div className={classes.props}>
+            {/* <div className={classes.props}>
               {Object.entries(doc.props).map(([name, prop]) => (
                 <dl key={name}>
                   <dt className={classes.propName}>
@@ -63,7 +63,47 @@ export const ReactComponentDocs = forwardRef<
                   )}
                 </dl>
               ))}
-            </div>
+            </div> */}
+            <Table
+              data-color='neutral'
+              data-size='sm'
+              border
+              className={classes.table}
+            >
+              <Table.Head>
+                <Table.Row>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Type</Table.HeaderCell>
+                  <Table.HeaderCell>Default</Table.HeaderCell>
+                  <Table.HeaderCell>Description</Table.HeaderCell>
+                </Table.Row>
+              </Table.Head>
+              <Table.Body>
+                {Object.entries(doc.props).map(([name, prop]) => (
+                  <Table.Row key={name}>
+                    <Table.Cell>
+                      <Tag data-color='accent'>{name}</Tag>
+                      {prop.required && (
+                        <span className={classes.required}>Required</span>
+                      )}
+                    </Table.Cell>
+                    <Table.Cell data-color='accent' className={classes.type}>
+                      <code>
+                        {prop.type?.raw ? prop.type.raw : prop.type?.name}
+                      </code>
+                    </Table.Cell>
+                    <Table.Cell className={classes.default}>
+                      {prop.defaultValue ? (
+                        <Tag>{prop.defaultValue.value}</Tag>
+                      ) : (
+                        '-'
+                      )}
+                    </Table.Cell>
+                    <Table.Cell>{prop.description || '-'}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
           </div>
         ))}
     </div>
