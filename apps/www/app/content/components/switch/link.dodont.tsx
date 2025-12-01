a49ab69c2d4755511a51b3ce36e2ad9292405add
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-12-01 14:46:34 +0100

    switch

---
+++ apps/www/app/content/components/switch/link.dodont.tsx
@@ -0,0 +1,16 @@
+import { Switch } from '@digdir/designsystemet-react';
+
+export const DoIcon = () => {
+  return <Switch label='Flymodus' />;
+};
+export const DontIcon = () => {
+  return <Switch label='Skru av flymodus' />;
+};
+
+export const DoIconEn = () => {
+  return <Switch label='Flight mode' />;
+};
+
+export const DontIconEn = () => {
+  return <Switch label='Turn off flight mode' />;
+};
