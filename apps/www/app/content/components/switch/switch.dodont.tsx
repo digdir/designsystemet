e9aae9c3fc7ddada7fdd3da966295b2eb2176e60
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-12-02 09:07:34 +0100

    alert: add link to System notifications pattern for clearer guidance

--- apps/www/app/content/components/switch/switch.dodont.tsx
+++ apps/www/app/content/components/switch/switch.dodont.tsx
@@ -1,16 +1,16 @@
 import { Switch } from '@digdir/designsystemet-react';
 
 export const DoIcon = () => {
-  return <Switch label='Flymodus' />;
+  return <Switch label='Flymodus' data-color='accent' />;
 };
 export const DontIcon = () => {
-  return <Switch label='Skru av flymodus' />;
+  return <Switch label='Skru av flymodus' data-color='accent' />;
 };
 
 export const DoIconEn = () => {
-  return <Switch label='Flight mode' />;
+  return <Switch label='Flight mode' data-color='accent' />;
 };
 
 export const DontIconEn = () => {
-  return <Switch label='Turn off flight mode' />;
+  return <Switch label='Turn off flight mode' data-color='accent' />;
 };
