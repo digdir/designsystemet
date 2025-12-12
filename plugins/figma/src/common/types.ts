557107c9d881b2ada490b39319a63398e6983e26
Tobias Barsnes <tobias.barsnes@digdir.no>
2025-12-12 12:14:41 +0100

    chore: delete figma plugin (#4316)

--- plugins/figma/src/common/types.ts
+++
@@ -1,7 +0,0 @@
-export enum Messages {
-  GetThemes = 'getThemes',
-  GetThemesAndRedirect = 'getThemesAndRedirect',
-  GetVariables = 'getVariables',
-  SetValueForModeError = 'setValueForModeError',
-  UpdateVariables = 'updateVariables',
-}
