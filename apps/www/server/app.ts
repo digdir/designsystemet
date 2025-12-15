654cd7e97b9fe9527a4d42988a24929c329bafdf
Barsnes <tobias.barsnes@digdir.no>
2025-12-15 14:26:51 +0100

    two more headers

--- apps/www/server/app.ts
+++ apps/www/server/app.ts
@@ -25,6 +25,11 @@ app.use((req, res, next) => {
     'Content-Security-Policy-Report-Only',
     `default-src 'none';base-uri 'self';script-src 'self' 'unsafe-inline' 'unsafe-eval';style-src 'self' https://altinncdn.no https://siteimproveanalytics.com 'unsafe-inline';font-src 'self' https://altinncdn.no;img-src 'self' data:;connect-src ${connectSrc};frame-ancestors 'self';form-action 'self';manifest-src 'self';`,
   );
+  res.setHeader(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains',
+  );
+  res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('Cache-Control', 'max-age');
   /* Stop TRACE request */
   if (req.method === 'TRACE') {
