1c621ead9d53ef4de425c0e30fe3d5c30fa348c5
Barsnes <tobias.barsnes@digdir.no>
2026-01-05 14:37:35 +0100

    usable calc

--- apps/www/app/_components/roi-calculator/roi-calcuator.tsx
+++ apps/www/app/_components/roi-calculator/roi-calcuator.tsx
@@ -5,11 +5,27 @@ import {
   Radio,
   Textfield,
 } from '@digdir/designsystemet-react';
+import { useState } from 'react';
 import classes from './roi-calculator.module.css';
 
 export default function RoiCalculator() {
+  const [newSolutions, setNewSolutions] = useState(4);
+  const [numberOfDevs, setNumberOfDevs] = useState(2);
+  const [usageFactor, setUsageFactor] = useState(0.25);
+
+  // formel: antall årsverk (lagt inn 2)* 1695 * antall løsninger *0,25 (0,35)
+  const savingsFormula = (devsPerSolution: number, numberOfSolutions: number) =>
+    devsPerSolution * 1695 * numberOfSolutions * usageFactor;
+
+  const savedHours = savingsFormula(numberOfDevs, newSolutions).toLocaleString(
+    'no-NO',
+    {
+      maximumFractionDigits: 0,
+    },
+  );
+
   return (
-    <div className={classes.roiCalculator}>
+    <div className={classes.roiCalculator} data-wide-content>
       <Heading>Hva kan din virksomhet spare?</Heading>
       <Paragraph>
         Generell tekst om at du kan spare ulikt basert på om du skal bruke det
@@ -17,10 +33,18 @@ export default function RoiCalculator() {
       </Paragraph>
 
       <Fieldset>
-        <Radio name='usage' label='Jeg skal bruke det direkte i løsninger' />
+        <Radio
+          name='usage'
+          label='Jeg skal bruke det direkte i løsninger'
+          value='0.25'
+          onChange={() => setUsageFactor(0.25)}
+          defaultChecked
+        />
         <Radio
           name='usage'
           label='Jeg skal bygge eget Designsystem på toppen av Designsystemet'
+          value='0.35'
+          onChange={() => setUsageFactor(0.35)}
         />
       </Fieldset>
 
@@ -35,14 +59,29 @@ export default function RoiCalculator() {
           label='Antall nye løsninger per år'
           suffix='nye løsninger per år'
           type='number'
-          defaultValue={10}
+          defaultValue={4}
+          value={newSolutions}
+          onChange={(e) => setNewSolutions(Number(e.target.value))}
+          size={3}
+          name='number-of-solutions'
         />
         <Textfield
           label='Antall årsverk på design og frontend per løsning'
           suffix='årsverk design/frontend per løsning'
           type='number'
           defaultValue={2}
+          value={numberOfDevs}
+          onChange={(e) => setNumberOfDevs(Number(e.target.value))}
+          size={3}
+          name='number-of-devs-per-solution'
         />
+      </div>
+
+      <div className={classes.result}>
+        <Heading level={3}>
+          Virksomheten får frigjort {savedHours} timer per år ({numberOfDevs}{' '}
+          årsverk)
+        </Heading>
       </div>
     </div>
   );
