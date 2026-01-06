b467fda261d6ef74490d40195e89e3741c297aa3
stianmorsund <stian.morsund@ksdigital.no>
2026-01-06 11:31:03 +0100

    fix(card): prevent nested cards with heading-link affecting parent hover-style

--- packages/react/src/components/card/card.chromatic.tsx
+++ packages/react/src/components/card/card.chromatic.tsx
@@ -13,4 +13,27 @@ const meta: Meta = {
 };
 export default meta;
 
-export const Snapshots = createSingleStory(CardStories, meta);
+type Story = StoryFn<typeof Card>;
+
+/* Visual-only Chromatic story.
+  Verifies rendering of nested card with link for visual
+  regression snapshots. This story is intentionally kept out of the main
+  `card.stories`
+*/
+export const Cardception: Story = (args) => (
+  <Card>
+    <Card {...args}>
+      <Card>
+        <h2>
+          <a href="#">Nested Card Link</a>
+        </h2>
+      </Card>
+    </Card>
+    </Card>
+);
+
+
+export const Snapshots = createSingleStory(
+  { ...CardStories, Cardception },
+  meta
+);
