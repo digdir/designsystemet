6cd70291851cf9755cef106c67204ea383a6c168
Barsnes <tobias.barsnes@digdir.no>
2026-01-08 13:12:33 +0100

    prerender /intro

--- apps/www/app/_utils/config/generate-prerender-paths.ts
+++ apps/www/app/_utils/config/generate-prerender-paths.ts
@@ -8,6 +8,7 @@ const CONTENT_BASE_PATH = join(APP_ROOT, './app/content');
 const COMPONENTS_BASE_PATH = join(CONTENT_BASE_PATH, 'components');
 
 const SECTIONS_WITH_INDEX_ROUTES = [
+  'intro',
   'patterns',
   'components',
   'blog',
