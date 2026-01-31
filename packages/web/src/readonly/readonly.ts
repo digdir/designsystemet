b5051c941b9b5b090cfa27d5169f7d2408e630d4
eirikbacker <eirik.backer@mattilsynet.no>
2026-01-31 12:54:16 +0100

    fix: simplify and speed up readonly check

--- packages/web/src/readonly/readonly.ts
+++ packages/web/src/readonly/readonly.ts
@@ -1,19 +1,13 @@
 import { attr, on, onHotReload } from '../utils/utils';
 
-const isReadOnlyFix = (
-  el: unknown,
-): el is HTMLInputElement | HTMLSelectElement =>
-  (el instanceof HTMLSelectElement ||
-    (el instanceof HTMLInputElement &&
-      el.matches(
-        `[type="range"],[type="color"],[type="checkbox"],[type="radio"],[type="file"]`,
-      ))) &&
-  el.matches(`[readonly],[aria-readonly="true"]`);
+const isReadOnly = (el: unknown): el is HTMLInputElement | HTMLSelectElement =>
+  (el instanceof HTMLSelectElement || el instanceof HTMLInputElement) &&
+  (el.hasAttribute('readonly') || attr(el, 'aria-readonly') === 'true');
 
 // Allow tabbing when readonly, and only fix readonly input/select elements (since type select and non-text-inputs do not support readonly)
 // If radio buttons, move focus without changing checked state
 const handleKeyDown = (e: Event & Partial<KeyboardEvent>) => {
-  if (e.key !== 'Tab' && isReadOnlyFix(e.target)) {
+  if (e.key !== 'Tab' && isReadOnly(e.target)) {
     e.preventDefault();
     if (e.key?.startsWith('Arrow') && attr(e.target, 'type') === 'radio') {
       const all = document.querySelectorAll(`input[name="${e.target.name}"]`);
@@ -24,15 +18,21 @@ const handleKeyDown = (e: Event & Partial<KeyboardEvent>) => {
   }
 };
 
-const handleMouse = (e: Event) => {
+const handleClick = (e: Event) => {
   const input = (e.target as Element)?.closest?.('label')?.control || e.target;
-  if (isReadOnlyFix(input)) {
+  if (isReadOnly(input)) {
     e.preventDefault();
     input.focus();
   }
 };
 
+const handleMouseDown = (e: Event) => {
+  if (e.target instanceof HTMLSelectElement && isReadOnly(e.target))
+    e.preventDefault();
+};
+
 onHotReload('readonly', () => [
   on(document, 'keydown', handleKeyDown),
-  on(document, 'click mousedown', handleMouse), // mousedown needed for <select>, click needed for <label> and <input>
+  on(document, 'click', handleClick), // click needed for <label> and <input>
+  on(document, 'mousedown', handleMouseDown), // mousedown needed for <select>
 ]);
