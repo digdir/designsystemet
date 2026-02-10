88982346544c0854ab8aaae4f471a4d44880a310
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2026-02-10 13:59:06 +0100

    chore(docs): add story for type=

--- apps/www/app/content/components/textfield/textfield.stories.tsx
+++ apps/www/app/content/components/textfield/textfield.stories.tsx
@@ -1,4 +1,6 @@
-import { Tag, Textfield } from '@digdir/designsystemet-react';
+import { Tag, Textfield, Field, Label, Select, type TextfieldProps } from '@digdir/designsystemet-react';
+import { useState } from 'react';
+import { ArrowRightIcon } from '@navikt/aksel-icons';
 
 export const Preview = () => {
   return <Textfield label='Label' />;
@@ -73,3 +75,35 @@ export const RequiredEn = () => (
     required
   />
 );
+
+
+export const InputType = () => {
+  const [type, setType] = useState<TextfieldProps['type'] >('text');
+
+  return (
+    <div style={{ display: 'flex', gap: 'var(--ds-size-4)', alignItems: 'end', flexWrap: 'wrap' }}>
+      <Field>
+        <Label><span data-lang='no'>Velg</span><span data-lang='en'>Choose</span> type</Label>
+        <Select defaultValue='text' onChange={(e) => setType(e.target.value as TextfieldProps['type'])}>
+          <Select.Option value='text'>text</Select.Option>
+          <Select.Option value='color'>color</Select.Option>
+          <Select.Option value='date'>date</Select.Option>
+          <Select.Option value='datetime-local'>datetime-local</Select.Option>
+          <Select.Option value='email'>email</Select.Option>
+          <Select.Option value='file'>file</Select.Option>
+          <Select.Option value='month'>month</Select.Option>
+          <Select.Option value='hidden'>hidden</Select.Option>
+          <Select.Option value='number'>number</Select.Option>
+          <Select.Option value='password'>password</Select.Option>
+          <Select.Option value='search'>search</Select.Option>
+          <Select.Option value='tel'>tel</Select.Option>
+          <Select.Option value='time'>time</Select.Option>
+          <Select.Option value='url'>url</Select.Option>
+          <Select.Option value='week'>week</Select.Option>
+        </Select>
+      </Field>
+      <ArrowRightIcon aria-hidden fontSize={48}/>
+      <Textfield label={`type="${type}"`} type={type} />
+    </div>
+  );
+};
