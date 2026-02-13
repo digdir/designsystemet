2e6997874978990a1da9d8bbe756fcd6fb3c2a80
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2026-02-13 14:29:37 +0100

    Update apps/themebuilder/app/locales/no.ts
    Co-authored-by: Tobias Barsnes <tobias.barsnes@digdir.no>

--- apps/themebuilder/app/locales/no.ts
+++ apps/themebuilder/app/locales/no.ts
@@ -147,7 +147,7 @@ export default {
     'set-to': 'Sett til',
     view: 'visning',
   },
-  'color_tokens': {
+  'color-tokens': {
     title: 'Fargetokens',
     description:
       'Her ser du hvilke tokens som er brukt for å lage kortene i seksjonen over.',
