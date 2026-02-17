60ee1c264b9c8705fdf65b036ce974dccac9f2e7
Marianne Røsvik <marianne.rosvik@digdir.no>
2026-02-17 15:33:24 +0100

    add story for known dates

---
+++ apps/www/app/content/patterns/no/date-and-time.stories.tsx
@@ -0,0 +1,28 @@
+import { Fieldset, Textfield } from '@digdir/designsystemet-react';
+
+export const KnownDates = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Når ble du født?</Fieldset.Legend>
+      <Fieldset.Description>For eksempel 24 7 1987</Fieldset.Description>
+
+      <div style={{ display: 'flex', gap: 'var(--ds-size-4)' }}>
+        <Textfield
+          label='Dag'
+          style={{ maxWidth: '3.5em' }}
+          autoComplete='bday-day'
+        />
+        <Textfield
+          label='Måned'
+          style={{ maxWidth: '3.5em' }}
+          autoComplete='bday-month'
+        />
+        <Textfield
+          label='År'
+          style={{ maxWidth: '6.5em' }}
+          autoComplete='bday-year'
+        />
+      </div>
+    </Fieldset>
+  );
+};
