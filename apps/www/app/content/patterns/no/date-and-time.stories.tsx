84a2bb6478d47c2029878492f0fc7c26b7dab6c0
Marianne Røsvik <marianne.rosvik@digdir.no>
2026-02-18 07:38:20 +0100

    add stories

--- apps/www/app/content/patterns/no/date-and-time.stories.tsx
+++ apps/www/app/content/patterns/no/date-and-time.stories.tsx
@@ -6,7 +6,7 @@ export const KnownDates = () => {
       <Fieldset.Legend data-size='lg'>Når ble du født?</Fieldset.Legend>
       <Fieldset.Description>For eksempel 24 7 1987</Fieldset.Description>
 
-      <div style={{ display: 'flex', gap: 'var(--ds-size-4)' }}>
+      <div style={{ display: 'flex', gap: 'var(--ds-size-6)' }}>
         <Textfield
           label='Dag'
           style={{ maxWidth: '3.5em' }}
@@ -22,6 +22,40 @@ export const KnownDates = () => {
           style={{ maxWidth: '6.5em' }}
           autoComplete='bday-year'
         />
+      </div>
+    </Fieldset>
+  );
+};
+
+export const NearFuturePast = () => {
+  return (
+    <Textfield
+      label='Når skal fristen for innlevering være?'
+      type='date'
+      style={{ maxWidth: '14em' }}
+    />
+  );
+};
+
+export const StartEndDate = () => {
+  return (
+    <div style={{ display: 'flex', gap: 'var(--ds-size-6)' }}>
+      <Textfield label='Startdato' type='date' />
+      <Textfield label='Sluttdato' type='date' />
+    </div>
+  );
+};
+
+export const StartEndTime = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend data-size='lg'>
+        Hvor lenge varer skoledagen?
+      </Fieldset.Legend>
+      <Fieldset.Description>Oppgi start- og sluttid</Fieldset.Description>
+      <div style={{ display: 'flex', gap: 'var(--ds-size-6)' }}>
+        <Textfield label='Fra kl' type='time' />
+        <Textfield label='Til kl' type='time' />
       </div>
     </Fieldset>
   );
