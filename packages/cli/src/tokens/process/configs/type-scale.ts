b3087e90d5678f468b934e7d0a49ca03358015de
Michael Marszalek <mimarz@gmail.com>
2026-03-20 13:56:29 +0100

    wip

--- packages/cli/src/tokens/process/configs/type-scale.ts
+++ packages/cli/src/tokens/process/configs/type-scale.ts
@@ -3,53 +3,57 @@ import { formats } from '../formats/css.js';
 import { sizeRem, typographyName } from '../transformers.js';
 import { basePxFontSize, type GetStyleDictionaryConfig, prefix } from './shared.js';
 
-export const typeScaleVariables: GetStyleDictionaryConfig = ({ theme }) => {
-  const selector = ':root, [data-size]';
-  const layer = `ds.theme.type-scale`;
+export const typeScaleVariables =
+  (scaleValues: 'static' | 'modular'): GetStyleDictionaryConfig =>
+  ({ theme, size }) => {
+    const selector = ':root';
+    const layer = `ds.theme.type-scale`;
 
-  return {
-    usesDtcg: true,
-    preprocessors: ['tokens-studio'],
-    expand: {
-      include: ['typography'],
-    },
-    platforms: {
-      css: {
-        prefix,
-        selector,
-        layer,
-        buildPath: `${theme}/`,
-        basePxFontSize,
-        transforms: [
-          'name/kebab',
-          'ts/size/px',
-          sizeRem.name,
-          'ts/size/lineheight',
-          'ts/typography/fontWeight',
-          typographyName.name,
-        ],
-        files: [
-          {
-            destination: `type-scale.css`,
-            format: formats.typeScale.name,
-            filter: (token) => {
-              const included = typeEquals(['typography', 'dimension', 'fontsize'], token);
+    return {
+      usesDtcg: true,
+      preprocessors: ['tokens-studio'],
+      expand: {
+        include: ['typography'],
+      },
+      platforms: {
+        css: {
+          size: scaleValues === 'static' ? size : undefined,
+          prefix,
+          selector,
+          layer,
+          buildPath: `${theme}/`,
+          basePxFontSize,
+          transforms: [
+            'name/kebab',
+            ...(scaleValues === 'static' ? ['ts/resolveMath'] : []),
+            'ts/size/px',
+            sizeRem.name,
+            'ts/size/lineheight',
+            'ts/typography/fontWeight',
+            typographyName.name,
+          ],
+          files: [
+            {
+              destination: scaleValues === 'static' ? `type-scale/${size}.css` : `type-scale.css`,
+              format: formats.typeScale.name,
+              filter: (token) => {
+                const included = typeEquals(['typography', 'dimension', 'fontsize', 'number'], token);
 
-              // Remove primitive typgography tokens
-              if (/primitives\/modes\/typography\/(primary|secondary)/.test(token.filePath)) return false;
+                // Remove primitive typgography tokens
+                if (/primitives\/modes\/typography\/(primary|secondary)/.test(token.filePath)) return false;
 
-              return (
-                included &&
-                !pathStartsWithOneOf(['spacing', 'sizing', 'size', 'border-width', 'border-radius'], token) &&
-                (pathStartsWithOneOf(['font-size'], token) || token.path.includes('fontSize'))
-              );
+                return (
+                  included &&
+                  !pathStartsWithOneOf(['spacing', 'sizing', 'size', 'border-width', 'border-radius'], token) &&
+                  (pathStartsWithOneOf(['font-size', 'font-scale'], token) || token.path.includes('fontSize'))
+                );
+              },
             },
+          ],
+          options: {
+            outputReferences: (token) => pathStartsWithOneOf(['typography'], token) && token.path.includes('fontSize'),
           },
-        ],
-        options: {
-          outputReferences: (token) => pathStartsWithOneOf(['typography'], token) && token.path.includes('fontSize'),
         },
       },
-    },
+    };
   };
-};
