a82226d59bd427d666f57af20ed1f96bf828adfc
eirikbacker <eirik.backer@mattilsynet.no>
2026-03-31 13:55:59 +0200

    fix(togglegroup): support aria-labelledby

--- packages/web/src/toggle-group/toggle-group.ts
+++ packages/web/src/toggle-group/toggle-group.ts
@@ -1,11 +1,15 @@
 import { attr, getCSSProp, on, onHotReload, warn } from '../utils/utils';
 
+const ARIA_LABELLEDBY = 'aria-labelledby';
+const ARIA_LABEL = 'aria-label';
+
 const handleKeydown = (event: Partial<KeyboardEvent>) => {
   const { key, target: el } = event;
   const group = el instanceof HTMLInputElement && el.closest('fieldset');
 
   if (!group || !getCSSProp(el, '--_ds-toggle-group')) return;
-  if (!attr(group, 'aria-label')) warn('Missing aria-label on:', group);
+  if (!attr(group, ARIA_LABEL) && !attr(group, ARIA_LABELLEDBY))
+    warn(`Missing ${ARIA_LABEL} or ${ARIA_LABELLEDBY} on:`, group);
   if (key === 'Enter') el.click(); // Forward Enter, but no need to listen for space key, as this is handled by the browser
   if (key?.startsWith('Arrow')) {
     event.preventDefault?.();
