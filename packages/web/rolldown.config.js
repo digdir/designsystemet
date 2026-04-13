aff8800eb9cdde786aceb50da68cff3557f16bf6
Michael Marszalek <mimarz@gmail.com>
2026-04-13 08:23:40 +0200

    revert some changes

--- packages/web/rolldown.config.js
+++ packages/web/rolldown.config.js
@@ -50,7 +50,7 @@ export default defineConfig([
       format: 'cjs',
       sourcemap: true,
       // See https://github.com/rollup/rollup/issues/3684#issuecomment-1535836196
-      entryFileNames: entryFileNames('js'),
+      entryFileNames: entryFileNames('cjs'),
       // Needed to truly enable being treeshakable when Vite is in lib mode
       // https://stackoverflow.com/questions/74362685/tree-shaking-does-not-work-in-vite-library-mode
       preserveModules: true,
