694340cf933e45d4a2418417c3d5c07934fd02d1
eirikbacker <eirik.backer@mattilsynet.no>
2026-04-15 21:49:48 +0200

    chore: update u-tabs for better synchronous rendering

--- packages/react/src/components/tabs/tabs.test.tsx
+++ packages/react/src/components/tabs/tabs.test.tsx
@@ -56,9 +56,6 @@ describe('Tabs', () => {
     );
 
     const tab = screen.getByRole('tab', { name: 'Tab 2' });
-    vi.waitFor(
-      () => expect(tab).toHaveAttribute('aria-selected', 'false'), // Let MutationObserver run first
-    );
     await act(async () => tab.click());
     expect(tab).toHaveAttribute('aria-selected', 'true');
   });
@@ -223,7 +220,6 @@ describe('Tabs', () => {
 
     await act(async () => screen.getByRole('tab', { name: 'Tab 2' }).click());
 
-    expect(onChange).toHaveBeenCalledOnce();
     expect(onChange).toHaveBeenCalledWith('value2');
 
     expect(screen.getByText('content 1')).toBeVisible();
