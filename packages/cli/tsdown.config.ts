225fe85164d3ca5751a4c88a258417c04e58c533
Barsnes <tobias.barsnes@digdir.no>
2026-04-20 09:55:00 +0200

    cli tsdown config

---
+++ packages/cli/tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['./src/**/*.ts', '!./src/scripts/**', './bin/**/*.ts'],
+  unbundle: true,
+  sourcemap: false,
+  format: 'esm',
+  platform: 'node',
+  target: 'esnext',
+  root: './',
+  dts: false,
+  fixedExtension: false,
+});
