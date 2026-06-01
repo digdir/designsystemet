2ee0958e59170111187a46da84b3ea4160d7b7d1
Marianne Røsvik <marianne.rosvik@digdir.no>
2026-06-01 11:54:47 +0200

    fix story

--- apps/www/app/content/fundamentals/no/theme/colorsvg.stories.tsx
+++ apps/www/app/content/fundamentals/no/theme/colorsvg.stories.tsx
@@ -1,15 +1,6 @@
-import {
-  Textfield,
-} from '@digdir/designsystemet-react';
+import { Textfield } from '@digdir/designsystemet-react';
 import { useState } from 'react';
 
 export const WithCurrentColor = () => {
-  return (
-    <Textfield
-      label='Etternavn (Dette er en ledetekst)'
-      description='Skriv bare det siste etternavnet. Hvis det er etternavn med bindestrek kan du skrive hele navnet. (Dette er en beskrivelse)'
-      multiline
-      rows={4}
-    />
-  );
+  return <Textfield label='Bytt ut dette eksempelet med SVG-eksempel' />;
 };
