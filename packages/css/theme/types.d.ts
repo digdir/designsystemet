8265c1ae3c713b8fd8fb37f07c490bcb84650fe3
Michael Marszalek <mimarz@gmail.com>
2026-06-02 08:41:01 +0200

    update designsystemet theme

--- packages/css/theme/types.d.ts
+++ packages/css/theme/types.d.ts
@@ -4,9 +4,6 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
-    accent: never;
-    brand1: never;
-    brand2: never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
