a117223ecd930ba951a9b2863831a0d155bf20f6
Michael Marszalek <mimarz@gmail.com>
2026-06-03 08:54:05 +0200

    revert tokens

--- packages/css/theme/colors.d.ts
+++ packages/css/theme/colors.d.ts
@@ -6,7 +6,6 @@ import type {} from '@digdir/designsystemet-types';
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
     accent: never;
-    inverted: never;
     brand1: never;
     brand2: never;
     neutral: never;
