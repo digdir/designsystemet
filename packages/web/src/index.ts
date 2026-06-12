ba32b275fad9f6e98f0ab0cb789cf879b6b430fa
eirikbacker <eirik.backer@mattilsynet.no>
2026-06-12 08:30:41 +0200

    chore: readme and import cleanup

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,9 +1,8 @@
-import { isSupported, apply as polyfillInvokers } from 'invokers-polyfill/fn';
-import { isBrowser } from './utils/utils';
-import '@u-elements/u-details/polyfill'; // Polyfill for <details> element for Android Firefox + Talkback
 import './clickdelegatefor/clickdelegatefor';
+import './details/details';
 import './dialog/dialog';
 import './fieldset/fieldset';
+import './invokers/invokers';
 import './popover/popover';
 import './readonly/readonly';
 import './toggle-group/toggle-group';
@@ -17,5 +16,3 @@ export * from './pagination/pagination';
 export * from './suggestion/suggestion';
 export * from './tabs/tabs';
 export * from './tooltip/tooltip';
-
-if (isBrowser() && !isSupported()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
