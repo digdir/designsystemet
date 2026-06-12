38b897a06714a2f3954a5490b76c57da06652857
Marianne Røsvik <marianne.rosvik@digdir.no>
2026-06-12 09:28:33 +0200

    add code example with checkboxes

--- apps/www/app/content/patterns/no/consent-banner.stories.tsx
+++ apps/www/app/content/patterns/no/consent-banner.stories.tsx
@@ -1,5 +1,7 @@
 import {
   Button,
+  Checkbox,
+  Fieldset,
   Heading,
   Link,
   Paragraph,
@@ -13,7 +15,7 @@ export const ConsentBanner = () => {
         <Heading
           id='samtykkebanner-tittel'
           data-size='md'
-          style={{ marginBottom: 'var(--ds-size-4)' }}
+          style={{ marginBottom: 'var(--ds-size-2)' }}
         >
           Får vi samle informasjon om hvordan nettsiden brukes?
         </Heading>
@@ -40,6 +42,98 @@ export const ConsentBanner = () => {
           <Button className='consent-banner-button' name='action' type='submit'>
             Nei
           </Button>
+        </form>
+        <Paragraph
+          data-size='sm'
+          style={{
+            marginTop: 'var(--ds-size-8)',
+          }}
+        >
+          <Link href='#nodvendig-informasjon' style={{ color: 'inherit' }}>
+            Vi lagrer også nødvendig informasjon
+          </Link>{' '}
+          som ikke kan velges bort. Dette gjør at nettsiden fungerer og er
+          trygg.
+        </Paragraph>
+      </section>
+      <SkipLink href='#main'>Hopp til hovedinnhold</SkipLink>
+    </>
+  );
+};
+
+export const ConsentBannerCheckboxes = () => {
+  return (
+    <>
+      <section aria-labelledby='samtykkebanner-flere-valg-tittel'>
+        <form method='post' action='/api/consent'>
+          <Fieldset>
+            <Fieldset.Legend>
+              <Heading
+                id='samtykkebanner-flere-valg-tittel'
+                data-size='md'
+                style={{ marginBottom: 'var(--ds-size-2)' }}
+              >
+                Hva får vi samle informasjon om?
+              </Heading>
+            </Fieldset.Legend>
+            <Fieldset.Description>
+              Informasjonen hjelper oss å forbedre nettsiden og løse problemer
+              raskere.
+            </Fieldset.Description>
+            <Checkbox
+              label='Hvordan nettsiden brukes'
+              name='consent'
+              value='usage'
+            />
+            <Checkbox
+              label='Tekniske feil som oppstår'
+              name='consent'
+              value='technical-errors'
+            />
+          </Fieldset>
+          <Paragraph
+            style={{
+              marginTop: 'var(--ds-size-5)',
+            }}
+          >
+            Du kan når som helst endre valget ditt nederst på siden.{' '}
+            <Link href='#mer-om-hva-vi-lagrer' style={{ color: 'inherit' }}>
+              Mer om hva vi lagrer og hvorfor.
+            </Link>
+          </Paragraph>
+          <div
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              gap: 'var(--ds-size-4)',
+              marginTop: 'var(--ds-size-5)',
+            }}
+          >
+            <Button
+              className='consent-banner-button'
+              name='action'
+              type='submit'
+              value='save'
+            >
+              Lagre valg
+            </Button>
+            <Button
+              className='consent-banner-button'
+              name='action'
+              type='submit'
+              value='approve-all'
+            >
+              Godta alle
+            </Button>
+            <Button
+              className='consent-banner-button'
+              name='action'
+              type='submit'
+              value='decline-all'
+            >
+              Avslå alle
+            </Button>
+          </div>
         </form>
         <Paragraph
           data-size='sm'
