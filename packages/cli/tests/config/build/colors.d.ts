12018ca3d99d8696c642660669078e53fc901c6a
Michael Marszalek <mimarz@gmail.com>
2026-06-29 08:28:54 +0200

    test: :white_check_mark: add hyphen color to cli config test

--- packages/cli/tests/config/build/colors.d.ts
+++ packages/cli/tests/config/build/colors.d.ts
@@ -1,5 +1,5 @@
 /* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
-/* build: v1.14.0 */
+/* build: v1.18.0 */
 import type {} from '@digdir/designsystemet-types';
 
 // Augment types based on theme
@@ -7,10 +7,8 @@ declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
     dominant: never;
     complimentary: never;
-    first: never;
-    second: never;
-    third: never;
-    fourth: never;
+    'hyphen-color': never;
+    'double-hyphen-color': never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
