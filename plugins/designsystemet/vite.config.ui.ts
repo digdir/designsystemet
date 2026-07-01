d007f8ac7c519dd4af1a864af827f55a166e9992
Michael Marszalek <mimarz@gmail.com>
2026-07-01 13:56:39 +0200

    wip

---
+++ plugins/designsystemet/vite.config.ui.ts
@@ -0,0 +1,32 @@
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import postcssUrl from 'postcss-url';
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), viteSingleFile()],
+  root: path.resolve('src/ui'),
+  build: {
+    minify: mode === 'production',
+    cssMinify: mode === 'production',
+    sourcemap: mode !== 'production' ? 'inline' : false,
+    emptyOutDir: false,
+    outDir: path.resolve('dist'),
+    rollupOptions: {
+      input: path.resolve('src/ui/index.html'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [postcssUrl({ url: 'inline' })],
+    },
+  },
+  resolve: {
+    alias: {
+      '@common': path.resolve('src/common'),
+      '@ui': path.resolve('src/ui'),
+    },
+  },
+}));
