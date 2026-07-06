1098f3286ef399dd95c02fa4566dc4f4c1d638f5
Michael Marszalek <mimarz@gmail.com>
2026-07-06 12:22:00 +0200

    does this change chromatic?

--- packages/web/src/invokers/invokers.ts
+++ packages/web/src/invokers/invokers.ts
@@ -1,8 +1,4 @@
-import {
-  isPolyfilled,
-  isSupported,
-  apply as polyfillInvokers,
-} from 'invokers-polyfill/fn';
+import { isSupported, apply as polyfillInvokers } from 'invokers-polyfill/fn';
 import { isBrowser } from '../utils/utils';
 
-if (isBrowser() && !isSupported() && !isPolyfilled()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
+if (isBrowser() && !isSupported()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
