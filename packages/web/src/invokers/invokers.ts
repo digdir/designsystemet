fa71ba3c3758b5d9982098b41a248588cc7fa3ab
Michael Marszalek <mimarz@gmail.com>
2026-07-06 12:26:53 +0200

    re-introduce isPolyfilled

--- packages/web/src/invokers/invokers.ts
+++ packages/web/src/invokers/invokers.ts
@@ -1,4 +1,8 @@
-import { isSupported, apply as polyfillInvokers } from 'invokers-polyfill/fn';
+import {
+  isPolyfilled,
+  isSupported,
+  apply as polyfillInvokers,
+} from 'invokers-polyfill/fn';
 import { isBrowser } from '../utils/utils';
 
-if (isBrowser() && !isSupported()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
+if (isBrowser() && !isSupported() && !isPolyfilled()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
