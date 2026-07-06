2b12edbf965cfd4bbcab7d3321e72733ac678768
Michael Marszalek <mimarz@gmail.com>
2026-07-06 12:39:38 +0200

    remove isPolyfilled again

--- packages/web/src/invokers/invokers.ts
+++ packages/web/src/invokers/invokers.ts
@@ -1,8 +1,5 @@
-import {
-  isPolyfilled,
-  isSupported,
-  apply as polyfillInvokers,
-} from 'invokers-polyfill/fn';
+import { isSupported, apply as polyfillInvokers } from 'invokers-polyfill/fn';
 import { isBrowser } from '../utils/utils';
 
-if (isBrowser() && !isSupported() && !isPolyfilled()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
+// isPolyfilled is not used here as isSupported is enough to determine if the polyfill needs to be applied as it checks for the presence of the invoker commands in the environment. If they are not present, it means the polyfill is not applied and needs to be applied.
+if (isBrowser() && !isSupported()) polyfillInvokers(); // Ensure invoker commands polyfill is loaded in browser environment only
