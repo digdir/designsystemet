a5e8bfca99171b8f1a27230c87989b94917f4708
Michael Marszalek <mimarz@gmail.com>
2026-07-16 16:16:27 +0200

    maybe not have default true to clean folder

--- packages/cli/src/schemas/next/schema.ts
+++ packages/cli/src/schemas/next/schema.ts
@@ -4,13 +4,13 @@ import { configFileCreateSchema } from '../v1.1/schema.ts';
 const designTokensOutputSchema = z.object({
   type: z.literal('design-tokens').describe('The type of output file'),
   dir: z.string().default('design-tokens').describe('The output directory'),
-  cleanDir: z.boolean().default(true).describe('Whether to clean the output directory before generating files'),
+  cleanDir: z.boolean().default(false).describe('Whether to clean the output directory before generating files'),
 });
 
 const cssOutputSchema = z.object({
   type: z.literal('css').describe('The type of output file'),
   dir: z.string().default('design-tokens-build').describe('The output directory'),
-  cleanDir: z.boolean().default(true).describe('Whether to clean the output directory before generating files'),
+  cleanDir: z.boolean().default(false).describe('Whether to clean the output directory before generating files'),
   tokenDir: z.string().default('design-tokens').describe('The directory containing the design tokens'),
   banner: z.string().default('my banner').describe('A banner to include at the top of the CSS file'),
   experimental_tailwind: z.boolean().default(true).describe('Whether to enable experimental Tailwind support'),
