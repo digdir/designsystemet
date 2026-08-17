984d0ce9d3a09e1942abcad0127b8297ec4e025d
eirikbacker <eirik.backer@gmail.com>
2026-08-17 12:15:31 +0200

    chore: cleanup

--- packages/web/vitest.setup.ts
+++ packages/web/vitest.setup.ts
@@ -2,18 +2,6 @@
 import '@testing-library/jest-dom/vitest';
 import { beforeAll } from 'vitest';
 
-// Polyfill missing HTMLDocument to Document inheritance in HappyDOM
-// TODO: This should be document in a "testing" section for consumers who use HappyDOM to run tests
-if (
-  typeof HTMLDocument !== 'undefined' &&
-  !(HTMLDocument.prototype instanceof Document)
-) {
-  // Use try/catch to avoid cyclic dependency
-  try {
-    Object.setPrototypeOf(HTMLDocument.prototype, Document.prototype);
-  } catch (_) {}
-}
-
 // Polyfill for HTMLDialogElement methods in JSDOM
 beforeAll(() => {
   HTMLDialogElement.prototype.show = function mock(this: HTMLDialogElement) {
