850d49eac78cbf179ec3bad5f2b818257018bc31
eirikbacker <eirik.backer@gmail.com>
2026-08-20 14:55:14 +0200

    chore: better deprecation warning avatar-stack

--- packages/react/src/components/avatar-stack/avatar-stack.tsx
+++ packages/react/src/components/avatar-stack/avatar-stack.tsx
@@ -7,21 +7,21 @@ export type AvatarStackProps = {
   /**
    *  Adjusts gap-mask between avatars in the stack. Must be a valid css length value (px, em, rem, var(--ds-size-1) etc.)
    *  @default 2px
-   *  @deprecated Please use `style={{ --dsc-avatar-stack-gap: VALUE }}` instead
+   *  @deprecated Please use `style={{ '--dsc-avatar-stack-gap': VALUE }}` instead
    */
   gap?: string;
   /**
    *  Control the size of the avatars. Must be a valid css length value (px, em, rem, var(--ds-size-12) etc.)
    *  @default 'var(--ds-size-12)'
-   *  @deprecated Please use `style={{ --dsc-avatar-stack-size: VALUE }}` instead
+   *  @deprecated Please use `style={{ '--dsc-avatar-stack-size': VALUE }}` instead
    */
   avatarSize?: string;
   /**
    *  A number which represents the percentage value of how much avatars should overlap.
    *  @default 50
-   *  @deprecated Please use `style={{ --dsc-avatar-stack-overlap: VALUE }}` instead
+   *  @deprecated Please use `style={{ '--dsc-avatar-stack-overlap': VALUE }}` instead
    */
-  overlap?: number;
+  overlap?: number | string;
   /**
    *  Text to the right of the avatars to show a number representing additional avatars not shown such as '+5'".
    */
@@ -52,33 +52,35 @@ export const EXPERIMENTAL_AvatarStack = forwardRef<
   HTMLDivElement,
   AvatarStackProps
 >(function AvatarStack(
-  { className, gap, suffix, avatarSize, overlap, expandable, ...rest },
+  { className, gap, suffix, avatarSize, overlap, style, expandable, ...rest },
   ref,
 ) {
+  if (typeof overlap === 'number')
+    overlap = `calc(var(--dsc-avatar-stack-size) / 100 * ${overlap})`; // Support backwards compatible integer overlap
+
   useEffect(() => {
     for (const [key, value] of Object.entries({ gap, avatarSize, overlap }))
       if (value)
         warn(
-          `AvatarStack: "${key}" property is deprecated. Please use style={{ '--dsc-avatar-stack-${key}:' '${value}' } as React.CSSProperties} instead`,
+          `AvatarStack: "${key}" property is deprecated. Please use style={{ '--dsc-avatar-stack-${key.replace('avatarSize', 'size')}': '${value}' } as React.CSSProperties} instead`,
         );
   }, [gap, avatarSize, overlap]);
 
   return (
     <div
       className={cl(`ds-avatar-stack`, className)}
+      data-expandable={expandable}
+      data-suffix={suffix}
       tabIndex={rest.tabIndex ?? (expandable ? 0 : undefined)}
       ref={ref}
       style={
         {
-          ...(rest.style || {}),
-          '--dsc-avatar-stack-gap': gap !== undefined ? `${gap}` : undefined,
-          '--dsc-avatar-stack-size': avatarSize ? `${avatarSize}` : undefined,
-          '--dsc-avatar-stack-overlap':
-            overlap !== undefined ? `${overlap}` : undefined,
+          '--dsc-avatar-stack-gap': gap,
+          '--dsc-avatar-stack-size': avatarSize,
+          '--dsc-avatar-stack-overlap': overlap,
+          ...style,
         } as CSSProperties
       }
-      data-expandable={expandable}
-      data-suffix={suffix}
       {...rest}
     />
   );
