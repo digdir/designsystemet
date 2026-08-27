f8344c39e1a17fdcca8d86b2b80e253fdb67f7e7
Michael Marszalek <mimarz@gmail.com>
2026-08-27 09:45:03 +0200

    rename folder

--- packages/cli/src/scripts/create-json-schema-next.ts
+++ packages/cli/src/scripts/create-json-schema-next.ts
@@ -1,7 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { z } from 'zod';
-import { nextConfigObjectSchema } from '../schemas/next/schema.ts';
+import { nextConfigObjectSchema } from '../schemas/v1.2/schema.ts';
 
 const schema = z
   .object({
