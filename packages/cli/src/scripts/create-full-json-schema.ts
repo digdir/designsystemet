b69204bbfdfd30717cfb3c7142048ee041207f8b
Michael Marszalek <mimarz@gmail.com>
2026-09-15 14:22:58 +0200

    simplify

--- packages/cli/src/scripts/create-full-json-schema.ts
+++ packages/cli/src/scripts/create-full-json-schema.ts
@@ -1,13 +1,13 @@
 import { writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { z } from 'zod';
-import { configObjectSchema } from '../schemas/schema.ts';
+import { configSchema } from '../schemas/schema.ts';
 
 const schema = z
   .object({
     $schema: z.string().optional(),
   })
-  .extend(configObjectSchema.shape);
+  .extend(configSchema.shape);
 
 writeFile(
   resolve(import.meta.dirname, '../../dist/config-full.schema.json'),
--- packages/cli/src/scripts/createJsonSchema.ts
+++ packages/cli/src/scripts/createJsonSchema.ts
@@ -1,13 +1,13 @@
 import { writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { z } from 'zod';
-import { externalConfigObjectSchema } from '../schemas/schema.ts';
+import { externalConfigSchema } from '../schemas/schema.ts';
 
 const schema = z
   .object({
     $schema: z.string().optional(),
   })
-  .extend(externalConfigObjectSchema.shape);
+  .extend(externalConfigSchema.shape);
 
 writeFile(
   resolve(import.meta.dirname, '../../dist/config.schema.json'),
