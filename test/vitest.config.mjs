b75e9e62ed77dc4450601737c40c514bb08f6958
Michael Marszalek <mimarz@gmail.com>
2026-09-16 14:59:21 +0200

    drop internal

--- test/vitest.config.mjs
+++ test/vitest.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    projects: ['{packages,internal,test}/*/vitest.config.mjs'],
+    projects: ['{packages,test}/*/vitest.config.mjs'],
     css: {
       modules: {
         classNameStrategy: 'non-scoped',
