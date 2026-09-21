ad977af85e62156a9012ff6d539de49d6e937487
Michael Marszalek <mimarz@gmail.com>
2026-09-21 14:01:42 +0200

    move scope handling to cli

--- packages/cli/src/internal.ts
+++ packages/cli/src/internal.ts
@@ -10,5 +10,13 @@ export {
   toFigmaCollections,
   UNGROUPED,
 } from './figma/collections.ts';
+export {
+  type FigmaVariableScope,
+  type FigmaVariableToken,
+  type FigmaVariableType,
+  figmaVariableScopes,
+  figmaVariableType,
+  isPrivateTokenPath,
+} from './figma/scopes.ts';
 export { getThemeColorScales } from './tokens/create/generators/primitives/color-scheme.ts';
 export { cssVariableName } from './tokens/css-variables.ts';
