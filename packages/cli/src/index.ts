c72e9f5ac5a2ee7f5e1f31c47de157dc23e2c620
Michael Marszalek <mimarz@gmail.com>
2026-09-23 08:17:33 +0200

    feat: cli internal export

--- packages/cli/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './colors/index.ts';
-export { severityColorNames, severityColors } from './schemas/defaults.ts';
-export {
-  type ExternalConfigSchema as ConfigSchema,
-  externalConfigSchema as configSchema,
-} from './schemas/schema.ts';
-export * from './tokens/index.ts';
