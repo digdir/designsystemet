7bd32121d7165eb53b9eb3233db709ad381ec09d
Michael Marszalek <mimarz@gmail.com>
2026-09-23 12:14:58 +0200

    Revert "Refactor exports and types in internal.ts"
    This reverts commit 23e193e27900d24eefb1b14ab1bb9ca1c4e5fe30.

--- packages/cli/src/internal.ts
+++ packages/cli/src/internal.ts
@@ -40,6 +40,4 @@ export {
 } from './schemas/schema.ts';
 export { getThemeColorScales } from './tokens/create/generators/primitives/color-scheme.ts';
 export { createSystemTokens, createTokens, getTokenSetDimensions } from './tokens/create.ts';
-export { formatThemeCSS, formatTokens } from './tokens/format.ts';
-export { deprecatedCLIOptions as cliOptions } from './schemas/helpers.ts';
-export type { BorderRadiusConfig, Theme as CreateTokensOptions, TokenSets } from './tokens/types.ts';
+export type { BorderRadiusConfig, TokenSets } from './tokens/types.ts';
